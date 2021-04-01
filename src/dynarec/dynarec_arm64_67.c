34a82f7bfc328f6bbac30b59ad7a9f58f44bd204
ptitSeb <sebastien.chev@gmail.com>
2021-04-01 14:38:13 +0200

    [DYNAREC] Handling 67 REX.W as just REX.W

--- src/dynarec/dynarec_arm64_67.c
+++ src/dynarec/dynarec_arm64_67.c
@@ -41,6 +41,9 @@ uintptr_t dynarec64_67(dynarec_arm_t* dyn, uintptr_t addr, uintptr_t ip, int nin
         opcode = F8;
     }
 
+    if(rex.w)   // rex.w cancels "67"
+        return dynarec64_00(dyn, addr-1, ip, ninst, rex, rep, ok, need_epilog);
+
     switch(opcode) {
 
         case 0x8D:
