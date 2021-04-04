7b099f9c6405da5e23e00b1dbdd06e025990a60d
rajdakin <rajdakin@gmail.com>
2021-04-04 18:36:18 +0200

    [WRAPPER] Added a new feature in the wrapper, forced all i32 to i64

--- src/wrapped/generated/wrapper.h
+++ src/wrapped/generated/wrapper.h
@@ -1137,4 +1137,6 @@ void iFEpvvppp(x64emu_t *emu, uintptr_t fnc);
 void iFEpLvvpp(x64emu_t *emu, uintptr_t fnc);
 void iFEpuvvppp(x64emu_t *emu, uintptr_t fnc);
 
+int isSimpleWrapper(wrapper_t fun);
+
 #endif //__WRAPPER_H_
