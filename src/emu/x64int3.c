a20b8303790c07582fc2d05bc1fa657af198ea76
ptitSeb <sebastien.chev@gmail.com>
2021-04-24 11:23:55 +0200

    Better trace for getenv

--- src/emu/x64int3.c
+++ src/emu/x64int3.c
@@ -115,6 +115,9 @@ void x64Int3(x64emu_t* emu)
                 } else  if(strstr(s, "strcmp")==s) {
                     tmp = (char*)(R_RDI);
                     snprintf(buff, 255, "%04d|%p: Calling %s(\"%s\", \"%s\")", tid, *(void**)(R_RSP), s, (tmp)?tmp:"(nil)", (char*)R_RSI);
+                } else  if(strstr(s, "getenv")==s) {
+                    tmp = (char*)(R_RDI);
+                    snprintf(buff, 255, "%04d|%p: Calling %s(\"%s\")", tid, *(void**)(R_RSP), s, (tmp)?tmp:"(nil)");
                 } else  if(!strcmp(s, "poll")) {
                     struct pollfd* pfd = (struct pollfd*)(R_RDI);
                     snprintf(buff, 255, "%04d|%p: Calling %s(%p[%d/%d/%d, ...], %d, %d)", tid, *(void**)(R_RSP), s, pfd, pfd->fd, pfd->events, pfd->revents, R_ESI, R_EDX);
