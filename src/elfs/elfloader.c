a8a45aede350c76611955adb4161686c7f2863bc
ptitSeb <sebastien.chev@gmail.com>
2021-07-05 11:55:10 +0200

    Try to speedup a bit FindR64COPYRel

--- src/elfs/elfloader.c
+++ src/elfs/elfloader.c
@@ -371,14 +371,16 @@ int FindR64COPYRel(elfheader_t* h, const char* name, uintptr_t *offs, uint64_t**
         int t = ELF64_R_TYPE(rel[i].r_info);
         Elf64_Sym *sym = &h->DynSym[ELF64_R_SYM(rel[i].r_info)];
         const char* symname = SymName(h, sym);
-        int version2 = h->VerSym?((Elf64_Half*)((uintptr_t)h->VerSym+h->delta))[ELF64_R_SYM(rel[i].r_info)]:-1;
-        if(version2!=-1) version2 &= 0x7fff;
-        if(version && !version2) version2=-1;   // match a versionned symbol against a global "local" symbol
-        const char* vername2 = GetSymbolVersion(h, version2);
-        if(SameVersionnedSymbol(name, version, vername, symname, version2, vername2) && t==R_X86_64_COPY) {
-            *offs = sym->st_value + h->delta;
-            *p = (uint64_t*)(rel[i].r_offset + h->delta + rel[i].r_addend);
-            return 1;
+        if(t==R_X86_64_COPY && symname && !strcmp(symname, name)) {
+            int version2 = h->VerSym?((Elf64_Half*)((uintptr_t)h->VerSym+h->delta))[ELF64_R_SYM(rel[i].r_info)]:-1;
+            if(version2!=-1) version2 &= 0x7fff;
+            if(version && !version2) version2=-1;   // match a versionned symbol against a global "local" symbol
+            const char* vername2 = GetSymbolVersion(h, version2);
+            if(SameVersionnedSymbol(name, version, vername, symname, version2, vername2)) {
+                *offs = sym->st_value + h->delta;
+                *p = (uint64_t*)(rel[i].r_offset + h->delta + rel[i].r_addend);
+                return 1;
+            }
         }
     }
     return 0;
