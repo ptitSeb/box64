b6ce8efa2019a58c7c43c9056230e242b3239f00
ptitSeb <sebastien.chev@gmail.com>
2021-07-09 17:07:56 +0200

    Removed an out-of-place comment

--- src/emu/x64run66.c
+++ src/emu/x64run66.c
@@ -47,7 +47,7 @@ int Run66(x64emu_t *emu, rex_t rex, int rep)
         rep = opcode-0xF1;
         opcode = F8;
     }
-    // REX prefix before the F0 are ignored
+
     rex.rex = 0;
     while(opcode>=0x40 && opcode<=0x4f) {
         rex.rex = opcode;
