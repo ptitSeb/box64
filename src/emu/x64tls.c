445d232ee74a8a8475c73ab4bed5e1e5a1885942
ptitSeb <sebastien.chev@gmail.com>
2021-07-17 18:01:59 +0200

    Added the macro, for later use (maybe)

--- src/emu/x64tls.c
+++ src/emu/x64tls.c
@@ -136,6 +136,8 @@ uint32_t my_modify_ldt(x64emu_t* emu, int op, thread_area_t* td, int size)
 int my_arch_prctl(x64emu_t *emu, int code, void* addr)
 {
     #define ARCH_SET_GS          0x1001
+    #define ARCH_SET_FS          0x1002
+    #define ARCH_GET_FS          0x1003
     #define ARCH_GET_GS          0x1004
     switch(code) {
         case ARCH_GET_GS:
