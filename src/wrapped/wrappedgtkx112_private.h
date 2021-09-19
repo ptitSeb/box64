6df1b65249e2a85d58d835e519d99e0237706c3d
ptitSeb <sebastien.chev@gmail.com>
2021-09-19 16:29:10 +0200

    Added 1 more gtk function

--- src/wrapped/wrappedgtkx112_private.h
+++ src/wrapped/wrappedgtkx112_private.h
@@ -349,7 +349,7 @@ GO(gtk_cell_layout_get_type, LFv)
 GO(gtk_cell_layout_pack_end, vFppi)
 GO(gtk_cell_layout_pack_start, vFppi)
 GO(gtk_cell_layout_reorder, vFppi)
-//GO(gtk_cell_layout_set_attributes, vFpppppppppp)  // vaarg
+GO(gtk_cell_layout_set_attributes, vFpppppppppp)  // vaarg
 GOM(gtk_cell_layout_set_cell_data_func, vFEppppp)
 GO(gtk_cell_renderer_accel_get_type, LFv)
 GO(gtk_cell_renderer_accel_mode_get_type, LFv)
