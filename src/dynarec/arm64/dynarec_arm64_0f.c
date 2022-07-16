fda8b9606310d5bb0e68b3d32859346de243d86d
ptitSeb <sebastien.chev@gmail.com>
2022-07-16 21:35:34 +0200

    [DYNAREC] Fixed 0F 09 Opcode

--- src/dynarec/arm64/dynarec_arm64_0f.c
+++ src/dynarec/arm64/dynarec_arm64_0f.c
@@ -122,6 +122,14 @@ uintptr_t dynarec64_0F(dynarec_arm_t* dyn, uintptr_t addr, uintptr_t ip, int nin
 
         case 0x09:
             INST_NAME("WBINVD");
+            SETFLAGS(X_ALL, SF_SET);    // Hack to set flags in "don't care" state
+            GETIP(ip);
+            STORE_XEMU_CALL(xRIP);
+            CALL(arm_ud, -1);
+            LOAD_XEMU_CALL(xRIP);
+            jump_to_epilog(dyn, 0, xRIP, ninst);
+            *need_epilog = 0;
+            *ok = 0;
             break;
 
         case 0x0B:
