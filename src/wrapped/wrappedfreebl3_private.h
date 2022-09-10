511446846f40999ec5ae426c65f14f56252d9f86
ptitSeb <sebastien.chev@gmail.com>
2022-09-10 12:41:53 +0200

    Added empty wrapped libfreebl3

---
+++ src/wrapped/wrappedfreebl3_private.h
@@ -0,0 +1,13 @@
+#if !(defined(GO) && defined(GOM) && defined(GO2) && defined(DATA))
+#error Meh....
+#endif
+
+//GO(FREEBL_GetVector, 
+//GO(NSSLOWHASH_Begin, 
+//GO(NSSLOWHASH_Destroy, 
+//GO(NSSLOWHASH_End, 
+//GO(NSSLOWHASH_Length, 
+//GO(NSSLOWHASH_NewContext, 
+//GO(NSSLOWHASH_Update, 
+//GO(NSSLOW_Init, 
+//GO(NSSLOW_Shutdown, 
