f2a0a1b62dd6b3935dcf4ab0420612c58c05e38e
ptitSeb <sebastien.chev@gmail.com>
2022-09-19 19:29:39 +0200

    Small fix to protection tracking on signal handling

--- src/libtools/signals.c
+++ src/libtools/signals.c
@@ -791,7 +791,7 @@ void my_box64signalhandler(int32_t sig, siginfo_t* info, void * ucntx)
     }
     dynablock_t* db = NULL;
     int db_searched = 0;
-    if ((sig==SIGSEGV) && (addr) && (info->si_code == SEGV_ACCERR) && (prot&PROT_DYNAREC)) {
+    if ((sig==SIGSEGV) && (addr) && (info->si_code == SEGV_ACCERR) && (prot&PROT_CUSTOM)) {
         pthread_mutex_lock(&mutex_dynarec_prot);
         // check if SMC inside block
         db = FindDynablockFromNativeAddress(pc);
