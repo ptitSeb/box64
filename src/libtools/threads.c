d0d18441cfdedac53b0ed8a772a88a1c26be7984
ptitSeb <sebastien.chev@gmail.com>
2022-12-03 13:21:52 +0100

    Use DynaCall instead of EmuCall for pthread_once callback (speedup some init code=

--- src/libtools/threads.c
+++ src/libtools/threads.c
@@ -686,7 +686,7 @@ int EXPORT my_pthread_once(x64emu_t* emu, int* once, void* cb)
 	#endif
 	if(old)
 		return 0;
-	EmuCall(emu, (uintptr_t)cb);  // avoid DynaCall for now, functions are only used once after all
+	DynaCall(emu, (uintptr_t)cb);  // using DynaCall, speedup wine 7.21 initialisation
 	return 0;
 }
 EXPORT int my___pthread_once(x64emu_t* emu, void* once, void* cb) __attribute__((alias("my_pthread_once")));
