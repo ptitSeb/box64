e9d6735e52067c8fea167eca8fdbdaa2887a0d9a
ptitSeb <sebastien.chev@gmail.com>
2022-12-06 17:41:38 +0100

    Added wrapped libxcb-render-util.so (helps #468)

---
+++ src/wrapped/wrappedlibxcbrenderutil_private.h
@@ -0,0 +1,19 @@
+#if !(defined(GO) && defined(GOM) && defined(GO2) && defined(DATA))
+#error meh!
+#endif
+
+//GO(xcb_render_util_change_glyphset, 
+//GO(xcb_render_util_composite_text, 
+//GO(xcb_render_util_composite_text_checked, 
+//GO(xcb_render_util_composite_text_free, 
+//GO(xcb_render_util_composite_text_stream, 
+//GO(xcb_render_util_disconnect, 
+//GO(xcb_render_util_find_format, 
+GO(xcb_render_util_find_standard_format, pFpi)
+GO(xcb_render_util_find_visual_format, pFpp)
+//GO(xcb_render_util_glyphs_16, 
+//GO(xcb_render_util_glyphs_32, 
+//GO(xcb_render_util_glyphs_8, 
+//GO(xcb_render_util_query_formats, 
+//GO(xcb_render_util_query_version, 
+
