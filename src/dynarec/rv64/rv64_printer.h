57ed841d6564a8e409194a8e497eff83a54851d5
Yang Liu <liuyang22@iscas.ac.cn>
2023-03-12 14:15:55 +0800

    [RV64] Add a disassembler for RV64 instructions

--- src/dynarec/rv64/rv64_printer.h
+++ src/dynarec/rv64/rv64_printer.h
@@ -1,7 +1,8 @@
 #ifndef _RV64_PRINTER_H_
 #define _RV64_PRINTER_H_
 #include <stdint.h>
+#include<stdbool.h>
 
-const char* rv64_print(uint32_t opcode, uint64_t addr);
+const char* rv64_print(uint32_t data, uint64_t addr);
 
 #endif //_RV64_PRINTER_H_
