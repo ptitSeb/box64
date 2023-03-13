d70b2ab832f54c6d9f1d3a07407eea909456065d
Yang Liu <liuyang22@iscas.ac.cn>
2023-03-13 17:07:38 +0800

    [RV64_DYNAREC] Added (81/83) SUB opcode

--- src/dynarec/rv64/dynarec_rv64_00.c
+++ src/dynarec/rv64/dynarec_rv64_00.c
@@ -76,6 +76,23 @@ uintptr_t dynarec64_00(dynarec_rv64_t* dyn, uintptr_t addr, uintptr_t ip, int ni
             SUBI(xRSP, xRSP, 8);
             break;
 
+        case 0x81:
+        case 0x83:
+            nextop = F8;
+            switch((nextop>>3)&7) {
+                case 5:
+                    if(opcode==0x81) {INST_NAME("SUB Ed, Id");} else {INST_NAME("SUB Ed, Ib");}
+                    SETFLAGS(X_ALL, SF_SET_PENDING);
+                    GETED((opcode==0x81)?4:1);
+                    if(opcode==0x81) i64 = F32S; else i64 = F8S;
+                    emit_sub32c(dyn, ninst, rex, ed, i64, x2, x3, x4, x5);
+                    WBACK;
+                    break;
+                default:
+                    DEFAULT;
+            }
+            break;
+
         case 0x89:
             INST_NAME("MOV Ed, Gd");
             nextop=F8;
