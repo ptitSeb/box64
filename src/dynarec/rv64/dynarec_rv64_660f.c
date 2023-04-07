aa7ee8e65db1e07a875a882b0b43edf5b413152a
Yang Liu <liuyang22@iscas.ac.cn>
2023-04-07 19:30:19 +0800

    [RV64_DYNAREC] Added more opcodes for Stardew Valley and some fixes (#673)
    * [RV64_DYNAREC] Added 66 0F D6 MOVQ opcode
    
    * [RV64_DYNAREC] Added 66 0F 56 ORPD opcode
    
    * [RV64_DYNAREC] Fixed emit_or32c
    
    * [RV64_DYNAREC] Added 0F BA /7 BTC opcode

--- src/dynarec/rv64/dynarec_rv64_660f.c
+++ src/dynarec/rv64/dynarec_rv64_660f.c
@@ -141,6 +141,13 @@ uintptr_t dynarec64_660F(dynarec_rv64_t* dyn, uintptr_t addr, uintptr_t ip, int 
             GETGX(x2);
             SSE_LOOP_Q(x3, x4, NOT(x3, x3); AND(x3, x3, x4));
             break;
+        case 0x56:
+            INST_NAME("ORPD Gx, Ex");
+            nextop = F8;
+            GETEX(x1, 0);
+            GETGX(x2);
+            SSE_LOOP_Q(x3, x4, OR(x3, x3, x4));
+            break;
         case 0x57:
             INST_NAME("XORPD Gx, Ex");
             nextop = F8;
@@ -432,6 +439,18 @@ uintptr_t dynarec64_660F(dynarec_rv64_t* dyn, uintptr_t addr, uintptr_t ip, int 
             GETGX(x1);
             GETEX(x2, 0);
             SSE_LOOP_Q(x3, x4, ADD(x3, x3, x4));
+            break;
+        case 0xD6:
+            INST_NAME("MOVQ Ex, Gx");
+            nextop = F8;
+            GETGXSD(d0);
+            GETEX(x2, 0);
+            FSD(d0, wback, fixedaddress+0);
+            if (MODREG) {
+                SD(xZR, wback, fixedaddress+8);
+            } else {
+                SMWRITE2();
+            }
             break;
         case 0xDB:
             INST_NAME("PAND Gx,Ex");
