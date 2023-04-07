66e217a7ddf169b06ad7ee58c71bedfc23c4fd7a
Yang Liu <liuyang22@iscas.ac.cn>
2023-04-07 16:19:58 +0800

    [RV64_DYNAREC] Added 66 0F 72 /6 PSLLD opcode

--- src/dynarec/rv64/dynarec_rv64_660f.c
+++ src/dynarec/rv64/dynarec_rv64_660f.c
@@ -249,6 +249,20 @@ uintptr_t dynarec64_660F(dynarec_rv64_t* dyn, uintptr_t addr, uintptr_t ip, int 
                         }
                     }
                     break;
+                case 6:
+                    INST_NAME("PSLLD Ex, Ib");
+                    GETEX(x1, 1);
+                    u8 = F8;
+                    if(u8) {
+                        if (u8>31) {
+                            // just zero dest
+                            SD(xZR, x1, fixedaddress+0);
+                            SD(xZR, x1, fixedaddress+8);
+                        } else if(u8) {
+                            SSE_LOOP_DS(x3, SLLI(x3, x3, u8));
+                        }
+                    }
+                    break;
                 default:
                     DEFAULT;
             }
