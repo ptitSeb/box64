86ea40995913ef4cbe96c7d95cfdc5ae24e9d250
Yang Liu <liuyang22@iscas.ac.cn>
2023-04-07 18:54:15 +0800

    [RV64_DYNAREC] Added 0F BA /7 BTC opcode

--- src/dynarec/rv64/dynarec_rv64_0f.c
+++ src/dynarec/rv64/dynarec_rv64_0f.c
@@ -521,6 +521,24 @@ uintptr_t dynarec64_0F(dynarec_rv64_t* dyn, uintptr_t addr, uintptr_t ip, int ni
                     ANDI(xFlags, xFlags, ~1);
                     OR(xFlags, xFlags, x3);
                     break;
+                case 7:
+                    INST_NAME("BTC Ed, Ib");
+                    SETFLAGS(X_CF, SF_SUBSET);
+                    SET_DFNONE();
+                    GETED(1);
+                    u8 = F8;
+                    u8&=rex.w?0x3f:0x1f;
+                    SRLIxw(x3, ed, u8);
+                    ANDI(x3, x3, 1); // F_CF is 1
+                    ANDI(xFlags, xFlags, ~1);
+                    OR(xFlags, xFlags, x3);
+                    if (u8 <= 10) {
+                        XORI(ed, ed, (1LL << u8));
+                    } else {
+                        MOV64xw(x3, (1LL << u8));
+                        XOR(ed, ed, x3);
+                    }
+                    break;
                 default:
                     DEFAULT;
             }
