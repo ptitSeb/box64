9f9831ac5620ef8c4073005fe1de11707d993bca
ptitSeb <sebastien.chev@gmail.com>
2023-04-08 10:08:44 +0200

    [ARM64_DYNAREC] Added 66 0F 38 14/15 opcodes

--- src/dynarec/arm64/dynarec_arm64_660f.c
+++ src/dynarec/arm64/dynarec_arm64_660f.c
@@ -329,6 +329,35 @@ uintptr_t dynarec64_660F(dynarec_arm_t* dyn, uintptr_t addr, uintptr_t ip, int n
                     }
                     break;
 
+                case 0x14:
+                    INST_NAME("PBLENDVPS Gx,Ex");
+                    nextop = F8;
+                    GETGX(q0, 1);
+                    GETEX(q1, 0, 0);
+                    v0 = sse_get_reg(dyn, ninst, x1, 0, 0);
+                    v1 = fpu_get_scratch(dyn);
+                    if(q0!=q1) {
+                        VSSHRQ_32(v1, v0, 31);    // bit[31]-> bit[31..0]
+                        VBICQ(q0, q0, v1);
+                        VANDQ(v1, q1, v1);
+                        VORRQ(q0, q0, v1);
+                    }
+                    break;
+                case 0x15:
+                    INST_NAME("PBLENDVPD Gx,Ex");
+                    nextop = F8;
+                    GETGX(q0, 1);
+                    GETEX(q1, 0, 0);
+                    v0 = sse_get_reg(dyn, ninst, x1, 0, 0);
+                    v1 = fpu_get_scratch(dyn);
+                    if(q0!=q1) {
+                        VSSHRQ_64(v1, v0, 63);    // bit[63]-> bit[63..0]
+                        VBICQ(q0, q0, v1);
+                        VANDQ(v1, q1, v1);
+                        VORRQ(q0, q0, v1);
+                    }
+                    break;
+
                 case 0x17:
                     INST_NAME("PTEST Gx, Ex");
                     nextop = F8;
