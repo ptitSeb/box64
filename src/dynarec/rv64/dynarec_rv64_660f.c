56e0b7db66f1ee3de944b57e2883331fbffa597d
Yang Liu <liuyang22@iscas.ac.cn>
2023-04-08 20:42:29 +0800

    [RV64_DYNAREC] Fixed 66 0F 72 /4 PSRAD opcode

--- src/dynarec/rv64/dynarec_rv64_660f.c
+++ src/dynarec/rv64/dynarec_rv64_660f.c
@@ -286,7 +286,7 @@ uintptr_t dynarec64_660F(dynarec_rv64_t* dyn, uintptr_t addr, uintptr_t ip, int 
                     u8 = F8;
                     if(u8>31) u8=31;
                     if (u8) {
-                        SSE_LOOP_DS(x3, SRAI(x3, x3, u8));
+                        SSE_LOOP_DS(x3, SRAIW(x3, x3, u8));
                     }
                     break;
                 case 6:
