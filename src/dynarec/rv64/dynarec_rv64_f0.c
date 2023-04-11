b8ecbcc529bb5afd47d4b3313499f07d1d2764c9
xctan <xctan@cirno.icu>
2023-04-11 22:16:10 +0800

    [RV64_DYNAREC] Fixed F0 09 LOCK OR opcode

--- src/dynarec/rv64/dynarec_rv64_f0.c
+++ src/dynarec/rv64/dynarec_rv64_f0.c
@@ -63,22 +63,22 @@ uintptr_t dynarec64_F0(dynarec_rv64_t* dyn, uintptr_t addr, uintptr_t ip, int ni
             nextop = F8;
             GETGD;
             SMDMB();
-            if(MODREG) {
+            if (MODREG) {
                 ed = xRAX+(nextop&7)+(rex.b<<3);
                 emit_or32(dyn, ninst, rex, ed, gd, x3, x4);
             } else {
                 addr = geted(dyn, addr, ninst, nextop, &wback, x2, x1, &fixedaddress, rex, LOCK_LOCK, 0, 0);
                 MARKLOCK;
                 LRxw(x1, wback, 1, 1);
-                OR(x1, x1, gd);
-                SCxw(x3, x1, wback, 1, 1);
+                OR(x4, x1, gd);
+                SCxw(x3, x4, wback, 1, 1);
                 BNEZ_MARKLOCK(x3);
-                IFX(X_ALL|X_PEND) {
+                IFX(X_ALL|X_PEND)
                     emit_or32(dyn, ninst, rex, x1, gd, x3, x4);
-                }
             }
             SMDMB();
             break;
+
         case 0x0F:
             nextop = F8;
             switch(nextop) {
