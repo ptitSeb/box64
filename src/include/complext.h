766049afbba15c97b9291b9193b5039e3c8f3330
ptitSeb <sebastien.chev@gmail.com>
2023-04-24 15:32:49 +0200

    Made the to/from_complex function static inline (might help #738)

--- src/include/complext.h
+++ src/include/complext.h
@@ -9,23 +9,23 @@ typedef struct complexf_s { float r; float i;} complexf_t;
 #ifdef COMPLEX_IMPL
 #ifndef __COMPLEX_T_IMPL_H_
 #define __COMPLEX_T_IMPL_H_
-inline complexf_t to_complexf(x64emu_t* emu, int i) {
+static inline complexf_t to_complexf(x64emu_t* emu, int i) {
     complexf_t ret; 
     ret.r = emu->xmm[i].f[0];
     ret.i = emu->xmm[i].f[1];
     return ret;
 }
-inline complex_t to_complex(x64emu_t* emu, int i) {
+static inline complex_t to_complex(x64emu_t* emu, int i) {
     complex_t ret;
     ret.r = emu->xmm[i].d[0];
     ret.i = emu->xmm[i+1].d[0];
     return ret;
 }
-inline void from_complexf(x64emu_t* emu, complexf_t v) {
+static inline void from_complexf(x64emu_t* emu, complexf_t v) {
     emu->xmm[0].f[0]=v.r;
     emu->xmm[0].f[1]=v.i;
 }
-inline void from_complex(x64emu_t* emu, complex_t v) {
+static inline void from_complex(x64emu_t* emu, complex_t v) {
     emu->xmm[0].d[0]=v.r; 
     emu->xmm[1].d[0]=v.i;
 }
