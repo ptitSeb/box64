40f0b7dc08c22c46bab2d6e086d20ab8748cb459
Yang Liu <liuyang22@iscas.ac.cn>
2023-05-05 11:53:26 +0800

    Added 66 0F 3A DF AESKEYGENASSIST opcode

--- src/dynarec/rv64/dynarec_rv64_660f.c
+++ src/dynarec/rv64/dynarec_rv64_660f.c
@@ -516,6 +516,28 @@ uintptr_t dynarec64_660F(dynarec_rv64_t* dyn, uintptr_t addr, uintptr_t ip, int 
                     MOV32w(x4, u8);
                     CALL(native_pclmul, -1);
                     break;
+                case 0xDF:
+                    INST_NAME("AESKEYGENASSIST Gx, Ex, Ib");  // AES-NI
+                    nextop = F8;
+                    GETG;
+                    sse_forget_reg(dyn, ninst, gd);
+                    MOV32w(x1, gd); // gx
+                    if(MODREG) {
+                        ed = (nextop&7)+(rex.b<<3); 
+                        sse_forget_reg(dyn, ninst, ed);
+                        MOV32w(x2, ed);
+                        MOV32w(x3, 0);  //p = NULL
+                    } else {
+                        MOV32w(x2, 0);
+                        addr = geted(dyn, addr, ninst, nextop, &ed, x3, x2, &fixedaddress, rex, NULL, 0, 1);
+                        if(ed!=x3) {
+                            MV(x3, ed);
+                        }
+                    }
+                    u8 = F8;
+                    MOV32w(x4, u8);
+                    CALL(native_aeskeygenassist, -1);
+                    break;
             default:
                     DEFAULT;
             }
