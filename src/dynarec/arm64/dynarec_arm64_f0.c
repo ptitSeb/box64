08e48239175ab06edba5753a3d4ee478c3263d06
ptitSeb <sebastien.chev@gmail.com>
2023-06-24 13:06:56 +0200

    [32BITS][ARM64_DYNAREC] Added F0 prefixed opcode (except 66 prefix)

--- src/dynarec/arm64/dynarec_arm64_f0.c
+++ src/dynarec/arm64/dynarec_arm64_f0.c
@@ -43,11 +43,6 @@ uintptr_t dynarec64_F0(dynarec_arm_t* dyn, uintptr_t addr, uintptr_t ip, int nin
     MAYUSE(wb2);
     MAYUSE(j64);
 
-    if(rex.is32bits) {
-        DEFAULT;
-        return addr;
-    }
-
     while((opcode==0xF2) || (opcode==0xF3)) {
         rep = opcode-0xF1;
         opcode = F8;
