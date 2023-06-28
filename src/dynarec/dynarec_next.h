1f65d3add7cc97ee5276470ff47ff591fb903034
Alexandre Julliard <julliard@winehq.org>
2023-06-28 17:50:14 +0200

    [DYNAREC] Fix push rsp instruction (#875)
    * [DYNAREC] Add one more missing void to a function prototype.
    
    * [DYNAREC] Add a missing return in arm64_print().
    
    * [DYNAREC] Use an intermediate register for PUSH RSP.
    
    To avoid undefined behavior, similar to POP RSP.

--- src/dynarec/dynarec_next.h
+++ src/dynarec/dynarec_next.h
@@ -4,21 +4,21 @@
 #ifdef ARM64
 void arm64_next(void) EXPORTDYN;
 void arm64_prolog(x64emu_t* emu, void* addr) EXPORTDYN;
-void arm64_epilog() EXPORTDYN;
+void arm64_epilog(void) EXPORTDYN;
 #define native_next         arm64_next
 #define native_prolog       arm64_prolog
 #define native_epilog       arm64_epilog
 #elif defined(LA464)
 void la464_next(void) EXPORTDYN;
 void la464_prolog(x64emu_t* emu, void* addr) EXPORTDYN;
-void la464_epilog() EXPORTDYN;
+void la464_epilog(void) EXPORTDYN;
 #define native_next         la464_next
 #define native_prolog       la464_prolog
 #define native_epilog       la464_epilog
 #elif defined(RV64)
 void rv64_next(void) EXPORTDYN;
 void rv64_prolog(x64emu_t* emu, void* addr) EXPORTDYN;
-void rv64_epilog() EXPORTDYN;
+void rv64_epilog(void) EXPORTDYN;
 #define native_next         rv64_next
 #define native_prolog       rv64_prolog
 #define native_epilog       rv64_epilog
