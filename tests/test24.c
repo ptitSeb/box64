f7e99017a69740bda4c7fd20f518cfd9bd7de202
Yang Liu <liuyang22@iscas.ac.cn>
2023-09-24 17:32:17 +0000

    Refined test24

--- tests/test24.c
+++ tests/test24.c
@@ -7,11 +7,13 @@
 
 uint64_t a = 0x12345678abcdefed;
 uint32_t b = 0x12345678;
+uint16_t c = 0x1234;
 
 int main()
 {
     uint64_t ret1;
     uint32_t ret2;
+    uint16_t ret3;
 
     asm volatile(
         "movbe %1, %0\n"
@@ -20,13 +22,19 @@ int main()
         : "memory");
     printf("ret = 0x%lx\n", ret1);
 
-
     asm volatile(
         "movbe %1, %0\n"
         : "=r"(ret2)
         : "m"(b)
         : "memory");
     printf("ret = 0x%x\n", ret2);
+
+    asm volatile(
+        "movbe %1, %0\n"
+        : "=r"(ret3)
+        : "m"(c)
+        : "memory");
+    printf("ret = 0x%x\n", ret3);
 
     asm volatile(
         "movbe %1, %0\n"
@@ -40,6 +48,27 @@ int main()
         : "=m"(ret2)
         : "r"(b)
         : "memory");
+    printf("ret = 0x%x\n", ret2);
+
+    asm volatile(
+        "movbe %1, %0\n"
+        : "=m"(ret3)
+        : "r"(b)
+        : "memory");
+    printf("ret = 0x%x\n", ret3);
+
+    asm volatile(
+        "bswap %0\n"
+        : "+r"(ret1)
+        :
+        :);
+    printf("ret = 0x%lx\n", ret1);
+
+    asm volatile(
+        "bswap %0\n"
+        : "+r"(ret2)
+        :
+        :);
     printf("ret = 0x%x\n", ret2);
     return 0;
 }
