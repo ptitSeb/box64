5fb76da301b7632a5cdf53ac3261b44f1fed4535
factfinding <www.wxmqq@gmail.com>
2024-02-29 14:17:52 +0800

    [LONGAARCH] More renamed arch to LA64

--- src/dynarec/la64/la64_printer.h
+++ src/dynarec/la64/la64_printer.h
@@ -1,6 +1,6 @@
 #ifndef _LA64_PRINTER_H_
 #define _LA64_PRINTER_H_
 
-const char* la464_print(uint32_t opcode, uint64_t addr);
+const char* la64_print(uint32_t opcode, uint64_t addr);
 
 #endif //_LA64_PRINTER_H_
