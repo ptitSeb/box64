6ed50f145733c27ad3faef46c9ce06fc0b78b6d5
howard <howardzz@foxmail.com>
2024-03-28 09:33:46 +0800

    Add build commands

--- tests/x64_functions_1.c
+++ tests/x64_functions_1.c
@@ -4,6 +4,8 @@
 
 #define EXPORT __attribute__((visibility("default")))
 
+// Build with `x86_64-linux-gnu-gcc -g -shared -fPIC x64_functions_1.c -o libx64functions1.so`
+
 EXPORT char* hello_word_str(void) {
     char* buff = (char*)malloc(30);
     memset(buff, 0, 30);
--- tests/x64_functions_2.c
+++ tests/x64_functions_2.c
@@ -4,6 +4,8 @@
 
 #define EXPORT __attribute__((visibility("default")))
 
+// Build with `x86_64-linux-gnu-gcc -g -shared -fPIC x64_functions_2.c -o libx64functions2.so`
+
 EXPORT int x64_tolower(int c) {
     return tolower(c);
 }
