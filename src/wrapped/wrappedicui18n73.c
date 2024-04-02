e18e532846bda3462daf3458c5e528086348f3c8
ptitSeb <sebastien.chev@gmail.com>
2024-04-02 15:16:26 +0200

    Added wrapped libicuuc73 and libicui18n73

---
+++ src/wrapped/wrappedicui18n73.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define _GNU_SOURCE         /* See feature_test_macros(7) */
+#include <dlfcn.h>
+
+#include "wrappedlibs.h"
+
+#include "debug.h"
+#include "wrapper.h"
+#include "bridge.h"
+#include "librarian/library_private.h"
+#include "x64emu.h"
+#include "emu/x64emu_private.h"
+#include "callback.h"
+#include "librarian.h"
+#include "box64context.h"
+#include "emu/x64emu_private.h"
+#include "myalign.h"
+
+const char* icui18n73Name = "libicui18n.so.73";
+#define LIBNAME icui18n73
+
+#define ALTMY my73_
+
+#define NEEDED_LIBS "libicuuc.so.73"
+
+#include "wrappedlib_init.h"
