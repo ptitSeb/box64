2adeec8fb9512049d1a79feeaa17c3c24cd533b7
xctan <xctan@cirno.icu>
2024-05-30 16:33:25 +0800

    [RV64_DYNAREC] Added 0F 76 PCMPEQD opcode

--- src/dynarec/rv64/dynarec_rv64_0f.c
+++ src/dynarec/rv64/dynarec_rv64_0f.c
@@ -1301,6 +1301,13 @@ uintptr_t dynarec64_0F(dynarec_rv64_t* dyn, uintptr_t addr, uintptr_t ip, int ni
             GETEM(x2, 0);
             MMX_LOOP_W(x3, x4, SUB(x3, x3, x4); SEQZ(x3, x3); NEG(x3, x3));
             break;
+        case 0x76:
+            INST_NAME("PCMPEQD Gm,Em");
+            nextop = F8;
+            GETGM();
+            GETEM(x2, 0);
+            MMX_LOOP_D(x3, x4, SUB(x3, x3, x4); SEQZ(x3, x3); NEG(x3, x3));
+            break;
         case 0x77:
             INST_NAME("EMMS");
             // empty MMX, FPU now usable
