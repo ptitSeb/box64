ccd57f310b916d82812de2639c8367b3eac85c2d
xctan <xctan@cirno.icu>
2024-06-03 16:58:07 +0800

    [RV64_DYNAREC] Optimized 66 0F 38 08 PSIGNB opcode

--- src/dynarec/rv64/dynarec_rv64_660f.c
+++ src/dynarec/rv64/dynarec_rv64_660f.c
@@ -454,10 +454,10 @@ uintptr_t dynarec64_660F(dynarec_rv64_t* dyn, uintptr_t addr, uintptr_t ip, int 
                     for (int i = 0; i < 16; ++i) {
                         LB(x3, gback, gdoffset + i);
                         LB(x4, wback, fixedaddress + i);
-                        BGE(x4, xZR, 4 + 4);
-                        NEG(x3, x3);
-                        BNE(x4, xZR, 4 + 4);
-                        MOV_U12(x3, 0);
+                        SLT(x1, xZR, x4);
+                        SRAI(x5, x4, 63);
+                        OR(x1, x1, x5);
+                        MUL(x3, x1, x3);
                         SB(x3, gback, gdoffset + i);
                     }
                     break;
