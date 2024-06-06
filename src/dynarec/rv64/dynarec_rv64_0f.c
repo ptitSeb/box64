1807ecd486e4cb2e019b6f84eb9b3e1174314d12
xctan <xctan@cirno.icu>
2024-06-06 19:19:18 +0800

    [RV64_DYNAREC] Fixed XADD Eb, Gb again for AH, BH, CH, DH

--- src/dynarec/rv64/dynarec_rv64_0f.c
+++ src/dynarec/rv64/dynarec_rv64_0f.c
@@ -2219,13 +2219,13 @@ uintptr_t dynarec64_0F(dynarec_rv64_t* dyn, uintptr_t addr, uintptr_t ip, int ni
             nextop = F8;
             GETEB(x1, 0);
             GETGB(x2);
-            if (!(MODREG && wback == gb1 && wb2 == gb2))
+            if (!(MODREG && wback == gb1 && !!(wb2) == !!(gb2)))
                 MV(x9, ed);
             emit_add8(dyn, ninst, ed, gd, x4, x5);
-            if (!(MODREG && wback == gb1 && wb2 == gb2))
+            if (!(MODREG && wback == gb1 && !!(wb2) == !!(gb2)))
                 MV(gd, x9);
             EBBACK(x5, 0);
-            if (!(MODREG && wback == gb1 && wb2 == gb2))
+            if (!(MODREG && wback == gb1 && !!(wb2) == !!(gb2)))
                 GBBACK(x5);
             break;
         case 0xC1:
