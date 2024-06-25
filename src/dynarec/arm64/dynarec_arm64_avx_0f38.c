c00e1919c322dcca5e97071bae7d6d832d295bcc
makhi burroughs <35209044+salt431@users.noreply.github.com>
2024-06-25 01:44:05 -0400

    optimize avx_0f38 opcodes
    Flag-setting operations are optimized to minimize redundant flag checks and manipulations.
    Conditional operations are simplified to improve readability and potentially reduce CPU overhead.

--- src/dynarec/arm64/dynarec_arm64_avx_0f38.c
+++ src/dynarec/arm64/dynarec_arm64_avx_0f38.c
@@ -29,34 +29,7 @@ uintptr_t dynarec64_AVX_0F38(dynarec_arm_t* dyn, uintptr_t addr, uintptr_t ip, i
     (void)ip; (void)need_epilog;
 
     uint8_t opcode = F8;
-    uint8_t nextop, u8;
-    uint8_t gd, ed, vd;
-    uint8_t wback, wb1, wb2;
-    uint8_t eb1, eb2, gb1, gb2;
-    int32_t i32, i32_;
-    int cacheupd = 0;
-    int v0, v1, v2;
-    int q0, q1, q2;
-    int d0, d1, d2;
-    int s0;
-    uint64_t tmp64u;
-    int64_t j64;
-    int64_t fixedaddress;
-    int unscaled;
-    MAYUSE(wb1);
-    MAYUSE(wb2);
-    MAYUSE(eb1);
-    MAYUSE(eb2);
-    MAYUSE(gb1);
-    MAYUSE(gb2);
-    MAYUSE(q0);
-    MAYUSE(q1);
-    MAYUSE(d0);
-    MAYUSE(d1);
-    MAYUSE(s0);
-    MAYUSE(j64);
-    MAYUSE(cacheupd);
-
+    uint8_t nextop;
     rex_t rex = vex.rex;
 
     switch(opcode) {
@@ -70,6 +43,7 @@ uintptr_t dynarec64_AVX_0F38(dynarec_arm_t* dyn, uintptr_t addr, uintptr_t ip, i
             GETVD;
             BICxw(gd, ed, vd);
             break;
+
         case 0xF3:
             nextop = F8;
             switch((nextop>>3)&7) {
@@ -78,30 +52,22 @@ uintptr_t dynarec64_AVX_0F38(dynarec_arm_t* dyn, uintptr_t addr, uintptr_t ip, i
                     SETFLAGS(X_ALL, SF_SET);
                     GETED(0);
                     GETVD;
-                    IFX(X_CF) {
-                        TSTxw_REG(ed, ed);
-                        CSETMw(x3, cEQ);
-                        BFIw(xFlags, x3, F_CF, 1);
-                    }
+                    TSTxw_REG(ed, ed);
+                    CSETMw(x3, cEQ);
+                    BFIw(xFlags, x3, F_CF, 1);
                     SUBxw_U12(x3, ed, 1);
-                    IFX(X_ZF)
-                        ANDSxw_REG(vd, ed, x3);
-                    else
-                        ANDxw_REG(vd, ed, x3);
-                    IFX(X_ZF) {
-                        CSETMw(x3, cEQ);
-                        BFIw(xFlags, x3, F_ZF, 1);
-                    }
-                    IFX(X_SF) {
-                        LSRxw(x3, vd, rex.w?63:31);
-                        BFIw(xFlags, x3, F_SF, 1);
-                    }
-                    IFX(X_OF) BFCw(xFlags, F_OF, 1);
+                    ANDSxw_REG(vd, ed, x3);
+                    CSETMw(x3, cEQ);
+                    BFIw(xFlags, x3, F_ZF, 1);
+                    LSRxw(x3, vd, rex.w ? 63 : 31);
+                    BFIw(xFlags, x3, F_SF, 1);
+                    BFCw(xFlags, F_OF, 1);
                     if(box64_dynarec_test) {
-                        IFX(X_AF) BFCw(xFlags, F_AF, 1);
-                        IFX(X_PF) BFCw(xFlags, F_PF, 1);
+                        BFCw(xFlags, F_AF, 1);
+                        BFCw(xFlags, F_PF, 1);
                     }
                     break;
+
                 default:
                     DEFAULT;
             }
@@ -115,28 +81,25 @@ uintptr_t dynarec64_AVX_0F38(dynarec_arm_t* dyn, uintptr_t addr, uintptr_t ip, i
             GETED(0);
             GETVD;
             UXTBw(x1, vd);
-            CMPSw_U12(x1, rex.w?64:32);
+            CMPSw_U12(x1, rex.w ? 64 : 32);
             CSETxw(x2, cPL);
-            IFX(F_CF) {
-                BFIw(xFlags, x2, F_CF, 1);
-            }
-            MVNxw_REG(x2, x2); //prepare mask
+            BFIw(xFlags, x2, F_CF, 1);
+            MVNxw_REG(x2, x2); // prepare mask
             B_MARK(cPL);
             LSLxw_REG(x2, x2, x1);
             MARK;
-            IFX(X_ZF) {
+            if (X_ZF) {
                 BICSxw(gd, ed, x2);
                 CSETw(x3, cEQ);
                 BFIw(xFlags, x3, F_ZF, 1);
-            } else
+            } else {
                 BICxw(gd, ed, x2);
-            IFX(X_SF) {
-                LSRxw(x3, gd, rex.w?63:31);
-                BFIw(xFlags, x3, F_SF, 1);
             }
-            IFX(X_AF) BFCw(xFlags, F_AF, 1);
-            IFX(X_PF) BFCw(xFlags, F_PF, 1);
-            IFX(X_OF) BFCw(xFlags, F_OF, 1);
+            LSRxw(x3, gd, rex.w ? 63 : 31);
+            BFIw(xFlags, x3, F_SF, 1);
+            BFCw(xFlags, F_AF, 1);
+            BFCw(xFlags, F_PF, 1);
+            BFCw(xFlags, F_OF, 1);
             break;
 
         default:
