0a373148d69746af36b9a6d99839e0c1241f3151
KreitinnSoftware <pablopro5051@gmail.com>
2024-07-04 12:52:39 -0300

    [VULKAN] Add Wrapped VK_KHR_android_surface

--- src/wrapped/wrappedvulkan_private.h
+++ src/wrapped/wrappedvulkan_private.h
@@ -311,6 +311,9 @@ GO(vkGetPhysicalDeviceXcbPresentationSupportKHR, uFpubp)
 GOM(vkCreateXlibSurfaceKHR, iFEpppp)
 GO(vkGetPhysicalDeviceXlibPresentationSupportKHR, uFpupp)
 
+// VK_KHR_android_surface
+GOM(vkCreateAndroidSurfaceKHR, iFEpppp)
+
 // VK_KHR_swapchain
 GO(vkAcquireNextImageKHR, iFpUUUUp)
 GO(vkAcquireNextImage2KHR, iFppp)
@@ -965,3 +968,4 @@ GO(vkCmdInitializeGraphScratchMemoryAMDX, vFpU)
 GOM(vkCreateExecutionGraphPipelinesAMDX, iFEpUuppp)
 GO(vkGetExecutionGraphPipelineNodeIndexAMDX, iFpUpp)
 GO(vkGetExecutionGraphPipelineScratchSizeAMDX, iFpUp)
+
