d5526bc8fb64301c8cebdf249063241ffcda1531
makhi burroughs <35209044+salt431@users.noreply.github.com>
2024-07-09 17:07:35 -0400

    Update x64runavxf20f38.c

--- src/emu/x64runavxf20f38.c
+++ src/emu/x64runavxf20f38.c
@@ -1,4 +1,4 @@
-#define _GNU_SOURCE
+#define *GNU*SOURCE
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,7 +8,6 @@
 #include <signal.h>
 #include <sys/types.h>
 #include <unistd.h>
-
 #include "debug.h"
 #include "box64stack.h"
 #include "x64emu.h"
@@ -26,25 +25,7 @@
 #include "custommem.h"
 #include "../dynarec/native_lock.h"
 #endif
-
 #include "modrm.h"
-
-#ifdef TEST_INTERPRETER
-x64emu_t *emu = test->emu;
-#else
-x64emu_t *emu = NULL;
-#endif
-
-uint8_t opcode, nextop;
-reg64_t *oped, *opgd, *opvd;
-sse_regs_t *opex, *opgx, *opvx, eax1, eax2;
-sse_regs_t *opey, *opgy, *opvy, eay1, eay2;
-
-uint8_t tmp8u, u8, tmp8s;
-int16_t tmp16s;
-uint16_t tmp16u, tmp32s, tmp32u, tmp64u, tmp64u2;
-int64_t tmp64s;
-int32_t tmp32s2, tmp32u2, tmp64u3;
 
 #ifdef TEST_INTERPRETER
 uintptr_t TestAVX_F20F38(x64test_t *test, vex_t vex, uintptr_t addr, int *step)
@@ -52,65 +33,76 @@ uintptr_t TestAVX_F20F38(x64test_t *test, vex_t vex, uintptr_t addr, int *step)
 uintptr_t RunAVX_F20F38(x64emu_t *emu, vex_t vex, uintptr_t addr, int *step)
 #endif
 {
-    opcode = F8;
-
+    uint8_t opcode = F8;
+    uint8_t nextop;
+    uint8_t u8;
+    uint32_t tmp32u;
+    uint64_t tmp64u;
+    reg64_t *oped, *opgd, *opvd;
+    sse_regs_t *opex, *opgx, *opvx;
+#ifdef TEST_INTERPRETER
+    x64emu_t *emu = test->emu;
+#endif
     rex_t rex = vex.rex;
 
     switch(opcode) {
-#define MASK64 0xFFFFFFFFFFFFFFFFLL
-
-case 0xF5:  /* PDEP Gd, Ed, Vd */
-    nextop = F8;
-    GETED(0);
-    GETGD;
-    GETVD;
-
-    if(rex.w) {
-        if(ED->q[0]&MASK64)
-            ED->q[0] ^ depx(ED->q[0], dexp, ED->q[0]);
-        if(VD->q[0]&MASK64)
-            VD->q[0] ^= ED->q[0];
-    } else {
-        if(ED->dword[0])
-            VD->dword[0] = ED->dword[0] & ~(R_EDX-1);
-        if(VD->dword[0])
-            GD->dword[0] = VD->dword[0] & ~(R_EDX-1);
-    }
-    break;
-
-case 0xF6:  /* MULX Gd, Vd, Ed (,RDX) */
-    nextop = F8;
-    GETED(0);
-    GETGD;
-    GETVD;
-
-    if(rex.w) {
-        tmp64u = ED->q[0] * R_EDX64LL;
-        VD->q[0] ^= tmp64u;
-        GD->q[0] ^= tmp64u >> 63;
-    } else {
-        if(ED->dword[0])
-            VD->dword[0] = ED->dword[0] * R_EDX;
-        GD->dword[0] = VD->dword[0];
-    }
-    break;
-
-case 0xF7:  /* SHRX Gd, Ed, Vd */
-    nextop = F8;
-    GETED(0);
-    GETGD;
-    GETVD;
-
-    if(rex.w) {
-        u8 = VD->q[0] & 0x3f;
-        GD->q[0] = ED->q[0] >> u8;
-    } else {
-        u8 = VD->dword[0] & 0x1f;
-        GD->dword[0] = ED->dword[0] >> u8;
-    }
-    break;
-     default:
-        return 0;
+        case 0xF5:  /* PDEP Gd, Ed, Vd */
+            nextop = F8;
+            GETED(0);
+            GETGD;
+            GETVD;
+            if(rex.w) {
+                tmp64u = 0;
+                u8 = 0;
+                for(int i=0; i<64; ++i) {
+                    if(ED->q[0] & (1ULL << i)) {
+                        tmp64u |= ((VD->q[0] >> u8) & 1ULL) << i;
+                        ++u8;
+                    }
+                }
+                GD->q[0] = tmp64u;
+            } else {
+                tmp32u = 0;
+                u8 = 0;
+                for(int i=0; i<32; ++i) {
+                    if(ED->dword[0] & (1U << i)) {
+                        tmp32u |= ((VD->dword[0] >> u8) & 1U) << i;
+                        ++u8;
+                    }
+                }
+                GD->q[0] = tmp32u;
+            }
+            break;
+        case 0xF6:  /* MULX Gd, Vd, Ed (,RDX) */
+            nextop = F8;
+            GETED(0);
+            GETGD;
+            GETVD;
+            if(rex.w) {
+                __uint128_t res = (__uint128_t)R_RDX * ED->q[0];
+                VD->q[0] = (uint64_t)res;
+                GD->q[0] = (uint64_t)(res >> 64);
+            } else {
+                tmp64u = (uint64_t)R_EDX * ED->dword[0];
+                VD->q[0] = (uint32_t)tmp64u;
+                GD->q[0] = (uint32_t)(tmp64u >> 32);
+            }
+            break;
+        case 0xF7:  /* SHRX Gd, Ed, Vd */
+            nextop = F8;
+            GETED(0);
+            GETGD;
+            GETVD;
+            if(rex.w) {
+                u8 = VD->q[0] & 0x3f;
+                GD->q[0] = ED->q[0] >> u8;
+            } else {
+                u8 = VD->dword[0] & 0x1f;
+                GD->q[0] = ED->dword[0] >> u8;
+            }
+            break;
+        default:
+            return 0;
     }
     return addr;
 }
