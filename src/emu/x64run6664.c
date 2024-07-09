b2f139d08a42d752276ef47eb790c95a46a2afa2
makhi burroughs <35209044+salt431@users.noreply.github.com>
2024-07-09 15:30:01 -0400

    Update x64run6664.c

--- src/emu/x64run6664.c
+++ src/emu/x64run6664.c
@@ -1,6 +1,26 @@
+#define _GNU_SOURCE
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 #include <string.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-// ...
+#include "debug.h"
+#include "box64stack.h"
+#include "x64emu.h"
+#include "x64run.h"
+#include "x64emu_private.h"
+#include "x64run_private.h"
+#include "x64primop.h"
+#include "x64trace.h"
+#include "x87emu_private.h"
+#include "box64context.h"
+#include "bridge.h"
+
+#include "modrm.h"
 
 uint8_t opcode;
 uint8_t nextop;
