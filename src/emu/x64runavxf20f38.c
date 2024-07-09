bfaa3bd341f154c881c6cfe26fab4e648c2d3664
makhi burroughs <35209044+salt431@users.noreply.github.com>
2024-07-09 15:57:18 -0400

    Update x64runavxf20f38.c

--- src/emu/x64runavxf20f38.c
+++ src/emu/x64runavxf20f38.c
@@ -30,29 +30,28 @@
 #include "modrm.h"
 
 #ifdef TEST_INTERPRETER
+x64emu_t *emu = test->emu;
+#else
+x64emu_t *emu = NULL;
+#endif
+
+uint8_t opcode, nextop;
+reg64_t *oped, *opgd, *opvd;
+sse_regs_t *opex, *opgx, *opvx, eax1, eax2;
+sse_regs_t *opey, *opgy, *opvy, eay1, eay2;
+
+uint8_t tmp8u, u8, tmp8s;
+int16_t tmp16s;
+uint16_t tmp16u, tmp32s, tmp32u, tmp64u, tmp64u2;
+int64_t tmp64s;
+int32_t tmp32s2, tmp32u2, tmp64u3;
+
+#ifdef TEST_INTERPRETER
 uintptr_t TestAVX_F20F38(x64test_t *test, vex_t vex, uintptr_t addr, int *step)
 #else
 uintptr_t RunAVX_F20F38(x64emu_t *emu, vex_t vex, uintptr_t addr, int *step)
 #endif
 {
-    uint8_t opcode;
-    uint8_t nextop;
-    uint8_t tmp8u, u8;
-    int8_t tmp8s;
-    int16_t tmp16s;
-    uint16_t tmp16u;
-    int32_t tmp32s, tmp32s2;
-    uint32_t tmp32u, tmp32u2;
-    uint64_t tmp64u, tmp64u2;
-    int64_t tmp64s;
-    reg64_t *oped, *opgd, *opvd;
-    sse_regs_t *opex, *opgx, *opvx, eax1, eax2;
-    sse_regs_t *opey, *opgy, *opvy, eay1, eay2;
-
-
-#ifdef TEST_INTERPRETER
-    x64emu_t *emu = test->emu;
-#endif
     opcode = F8;
 
     rex_t rex = vex.rex;
@@ -65,52 +64,62 @@ uintptr_t RunAVX_F20F38(x64emu_t *emu, vex_t vex, uintptr_t addr, int *step)
             GETGD;
             GETVD;
             if(rex.w) {
-                tmp64u = 0;
-                u8 = 0;
-                for(int i=0; i<64; ++i)
-                    if(ED->q[0]&(1LL<<i))
-                        tmp64u |= ((VD->q[0]>>(u8++))&1LL)<<i;
-                GD->q[0] = tmp64u;
-            } else {
-                tmp32u = 0;
-                u8 = 0;
-                for(int i=0; i<32; ++i)
-                    if(ED->dword[0]&(1<<i))
-                        tmp32u |= ((VD->dword[0]>>(u8++))&1)<<i;
-                GD->q[0] = tmp32u;
+                reg64_t deped = ED[0];
+                reg64_t devd = VD[0];
+                reg64_t gqd = GD[0];
+
+                if(deped&MASK64)
+                    gqx(gqd, gqx, dex(gqd), 0) ^= depx(deped, dexp, dex(deped));
+                if(devd&MASK64)
+                    gvx(gvd, gxv, devx(devd, dexp, dex(devd)));
+                } else {
+                    ED[0] = 0;
+                    VD[0] = 0;
+                    GD[0] = 0;
             }
             break;
+
         case 0xF6:  /* MULX Gd, Vd, Ed (,RDX) */
             nextop = F8;
             GETED(0);
             GETGD;
             GETVD;
             if(rex.w) {
-                unsigned __int128 res = (unsigned __int128)R_RDX * ED->q[0];
-                VD->q[0] = res&0xFFFFFFFFFFFFFFFFLL;
-                GD->q[0] = res>>64;
+                reg64_t ed = ED[0];
+                reg64_t gvd = VD[0];
+                reg64_t gqd = GD[0];
+
+                if(ed&MASK64)
+                    tmp64u = (ed ^ gvx(ed, gxv, dex(ed))) | ((ed ^ gqx(ed, gxq, dex(ed)))>>63);
+                    gvd ^= tmp64u;
+                    gqd ^= tmp64u >> 1;
             } else {
-                tmp64u = (uint64_t)R_EDX * ED->dword[0];
-                VD->q[0] = tmp64u&0xFFFFFFFF;
-                GD->q[0] = tmp64u>>32;
+                reg32_t ed = ED[0];
+                reg32_t gvd = VD[0];
+                reg32_t gqd = GD[0];
+
+                if(ed)
+                    gvd ^= ed & R_EDX;
+                    gqd ^= (ed&R_EDX)>>31;
             }
             break;
+
         case 0xF7:  /* SHRX Gd, Ed, Vd */
             nextop = F8;
             GETED(0);
             GETGD;
             GETVD;
             if(rex.w) {
-                u8 = VD->q[0] & 0x3f;
-                GD->q[0] = ED->q[0] >> u8;
+                u8 = VD[0] & 0x3f;
+                GD[0] = ED[0] >> u8;
             } else {
-                u8 = VD->dword[0] & 0x1f;
-                GD->q[0] = ED->dword[0] >> u8;
+                u8 = VD[0] & 0x1f;
+                GD[0] = ED[0] >> u8;
             }
             break;
 
         default:
-            return 0;
+            return addr;
     }
     return addr;
 }
