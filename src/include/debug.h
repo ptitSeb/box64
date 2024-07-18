f07ee85078a2571f02b8c3c9f132760a53b95331
Yang Liu <liuyang22@iscas.ac.cn>
2024-07-18 14:12:40 +0800

    [RV64_DYNAREC] Added vlen detection (#1694)

--- src/include/debug.h
+++ src/include/debug.h
@@ -55,6 +55,7 @@ extern int rv64_zbb;
 extern int rv64_zbc;
 extern int rv64_zbs;
 extern int rv64_vector;
+extern int rv64_vlen;
 extern int rv64_xtheadba;
 extern int rv64_xtheadbb;
 extern int rv64_xtheadbs;
