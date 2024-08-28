0450371e282bd5c4dbbceaeca590626304609036
Yang Liu <liuyang22@iscas.ac.cn>
2024-08-29 05:06:29 +0800

    [RV64_DYNAREC] Fixed a falsy optimization, better EMIT macro (#1769)

--- src/dynarec/arm64/dynarec_arm64_jmpnext.c
+++ src/dynarec/arm64/dynarec_arm64_jmpnext.c
@@ -2,7 +2,11 @@
 
 #include "arm64_emitter.h"
 
-#define EMIT(A) *block = (A); ++block
+#define EMIT(A)       \
+    do {              \
+        *block = (A); \
+        ++block;      \
+    } while (0)
 void CreateJmpNext(void* addr, void* next)
 {
     uint32_t* block = (uint32_t*)addr;
