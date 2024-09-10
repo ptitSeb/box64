f3af885dffc40fe83fb6c61479b2b957ac625433
Yang Liu <liuyang22@iscas.ac.cn>
2024-09-10 15:27:19 +0800

    [RV64_DYNAREC] Added 66 0F D4 PADDQ opcode for vector

--- src/dynarec/rv64/dynarec_rv64_660f_vector.c
+++ src/dynarec/rv64/dynarec_rv64_660f_vector.c
@@ -194,6 +194,14 @@ uintptr_t dynarec64_660F_vector(dynarec_rv64_t* dyn, uintptr_t addr, uintptr_t i
                 VXOR_VV(q0, q0, q1, VECTOR_UNMASKED);
             }
             break;
+        case 0xD4:
+            INST_NAME("PADDQ Gx,Ex");
+            nextop = F8;
+            SET_ELEMENT_WIDTH(x1, VECTOR_SEW64, 1);
+            GETGX_vector(q0, 1, VECTOR_SEW64);
+            GETEX_vector(q1, 0, 0, VECTOR_SEW64);
+            VADD_VV(q0, q0, q1, VECTOR_UNMASKED);
+            break;
         default:
             DEFAULT_VECTOR;
     }
