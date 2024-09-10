8cf44448d65b2aa609998c9dae3190fa286f2843
Yang Liu <liuyang22@iscas.ac.cn>
2024-09-10 15:28:16 +0800

    [RV64_DYNAREC] Transform SEW cache after fpu cache

--- src/dynarec/rv64/dynarec_rv64_helper.c
+++ src/dynarec/rv64/dynarec_rv64_helper.c
@@ -2437,13 +2437,14 @@ static void sewTransform(dynarec_rv64_t* dyn, int ninst, int s1)
     vector_vsetvl_emul1(dyn, ninst, s1, dyn->insts[jmp].vector_sew);
 }
 
-void CacheTransform(dynarec_rv64_t* dyn, int ninst, int cacheupd, int s1, int s2, int s3) {
-    if (cacheupd & 4)
-        sewTransform(dyn, ninst, s1);
+void CacheTransform(dynarec_rv64_t* dyn, int ninst, int cacheupd, int s1, int s2, int s3)
+{
     if (cacheupd & 2)
         fpuCacheTransform(dyn, ninst, s1, s2, s3);
     if (cacheupd & 1)
         flagsCacheTransform(dyn, ninst, s1);
+    if (cacheupd & 4)
+        sewTransform(dyn, ninst, s1);
 }
 
 
