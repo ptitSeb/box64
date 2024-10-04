e6f56bf5faa09c0de38b59c2fb6ddbc6cbbdb484
ptitSeb <sebastien.chev@gmail.com>
2024-10-04 14:24:02 +0200

    Fixed x86_64 build

--- src/wrapped/wrappedlibc.c
+++ src/wrapped/wrappedlibc.c
@@ -2923,8 +2923,8 @@ EXPORT void* my_mmap64(x64emu_t* emu, void *addr, size_t length, int prot, int f
         prot|=PROT_READ;    // PROT_READ is implicit with PROT_WRITE on i386
     if((emu || box64_is32bits) && (box64_log>=LOG_DEBUG || box64_dynarec_log>=LOG_DEBUG)) {printf_log(LOG_NONE, "mmap64(%p, 0x%lx, 0x%x, 0x%x, %d, %ld) => ", addr, length, prot, flags, fd, offset);}
     int new_flags = flags;
-    #ifndef NOALIGN
     void* old_addr = addr;
+    #ifndef NOALIGN
     new_flags&=~MAP_32BIT;   // remove MAP_32BIT
     if((flags&MAP_32BIT) && !(flags&MAP_FIXED)) {
         // MAP_32BIT only exist on x86_64!
