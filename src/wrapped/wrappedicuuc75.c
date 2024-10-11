2a4ecc1d28193bb21d9301610645040947520b73
Yang Liu <liuyang22@iscas.ac.cn>
2024-10-12 03:27:17 +0800

    [RV64_DYNAREC] Wrapped icuuc75 (#1926)

---
+++ src/wrapped/wrappedicuuc75.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define _GNU_SOURCE         /* See feature_test_macros(7) */
+#include <dlfcn.h>
+
+#include "wrappedlibs.h"
+
+#include "debug.h"
+#include "wrapper.h"
+#include "bridge.h"
+#include "librarian/library_private.h"
+#include "x64emu.h"
+#include "emu/x64emu_private.h"
+#include "callback.h"
+#include "librarian.h"
+#include "box64context.h"
+#include "emu/x64emu_private.h"
+#include "myalign.h"
+
+const char* icuuc75Name = "libicuuc.so.75";
+#define LIBNAME icuuc75
+
+#define ALTMY my75_
+
+#include "wrappedlib_init.h"
