37ab762a6927781f936a7c7a7e3cfa0ff7c8a965
zhaixiang <zhaixiang@loongson.cn>
2024-11-26 13:44:18 +0800

    [LA64_DYNAREC] Fix LOCK ADC wrong ed (#2069)

--- src/dynarec/la64/dynarec_la64_f0.c
+++ src/dynarec/la64/dynarec_la64_f0.c
@@ -410,7 +410,7 @@ uintptr_t dynarec64_F0(dynarec_la64_t* dyn, uintptr_t addr, uintptr_t ip, int ni
             GETGD;
             SMDMB();
             if (MODREG) {
-                ed = xRAX + (nextop & 7) + (rex.b << 3);
+                ed = TO_LA64((nextop & 7) + (rex.b << 3));
                 emit_adc32(dyn, ninst, rex, ed, gd, x3, x4, x5, x6);
             } else {
                 addr = geted(dyn, addr, ninst, nextop, &wback, x2, x1, &fixedaddress, rex, LOCK_LOCK, 0, 0);
