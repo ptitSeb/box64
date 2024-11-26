b18efe4e407140d6744e3e533521c9fee3da261c
ptitSeb <sebastien.chev@gmail.com>
2024-11-26 11:55:05 +0100

    [WRAPPER] Reworked gstfft with new wrapperhelper

--- src/wrapped/wrappedgstfft_private.h
+++ src/wrapped/wrappedgstfft_private.h
@@ -1,25 +1,25 @@
 #if !(defined(GO) && defined(GOM) && defined(GO2) && defined(DATA))
-#error meh!
+#error Meh...
 #endif
 
-//GO(gst_fft_f32_fft, 
-//GO(gst_fft_f32_free, 
-//GO(gst_fft_f32_inverse_fft, 
-//GO(gst_fft_f32_new, 
-//GO(gst_fft_f32_window, 
-//GO(gst_fft_f64_fft, 
-//GO(gst_fft_f64_free, 
-//GO(gst_fft_f64_inverse_fft, 
-//GO(gst_fft_f64_new, 
-//GO(gst_fft_f64_window, 
-//GO(gst_fft_next_fast_length, 
-//GO(gst_fft_s16_fft, 
-//GO(gst_fft_s16_free, 
-//GO(gst_fft_s16_inverse_fft, 
-//GO(gst_fft_s16_new, 
-//GO(gst_fft_s16_window, 
-//GO(gst_fft_s32_fft, 
-//GO(gst_fft_s32_free, 
-//GO(gst_fft_s32_inverse_fft, 
-//GO(gst_fft_s32_new, 
-//GO(gst_fft_s32_window, 
+GO(gst_fft_f32_fft, vFppp)
+GO(gst_fft_f32_free, vFp)
+GO(gst_fft_f32_inverse_fft, vFppp)
+GO(gst_fft_f32_new, pFii)
+GO(gst_fft_f32_window, vFppu)
+GO(gst_fft_f64_fft, vFppp)
+GO(gst_fft_f64_free, vFp)
+GO(gst_fft_f64_inverse_fft, vFppp)
+GO(gst_fft_f64_new, pFii)
+GO(gst_fft_f64_window, vFppu)
+GO(gst_fft_next_fast_length, iFi)
+GO(gst_fft_s16_fft, vFppp)
+GO(gst_fft_s16_free, vFp)
+GO(gst_fft_s16_inverse_fft, vFppp)
+GO(gst_fft_s16_new, pFii)
+GO(gst_fft_s16_window, vFppu)
+GO(gst_fft_s32_fft, vFppp)
+GO(gst_fft_s32_free, vFp)
+GO(gst_fft_s32_inverse_fft, vFppp)
+GO(gst_fft_s32_new, pFii)
+GO(gst_fft_s32_window, vFppu)
