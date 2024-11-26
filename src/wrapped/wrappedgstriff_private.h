f78d0ae471edcb464d608eb23c607b5947d6992a
ptitSeb <sebastien.chev@gmail.com>
2024-11-26 16:27:53 +0100

    [WRAPPER] Reworked gstriff with new wrapperhelper

--- src/wrapped/wrappedgstriff_private.h
+++ src/wrapped/wrappedgstriff_private.h
@@ -1,19 +1,19 @@
 #if !(defined(GO) && defined(GOM) && defined(GO2) && defined(DATA))
-#error meh!
+#error Meh...
 #endif
 
 GO(gst_riff_create_audio_caps, pFWpppppp)
 GO(gst_riff_create_audio_template_caps, pFv)
-//GO(gst_riff_create_iavs_caps, 
-//GO(gst_riff_create_iavs_template_caps, 
+GO(gst_riff_create_iavs_caps, pFuppppp)
+GO(gst_riff_create_iavs_template_caps, pFv)
 GO(gst_riff_create_video_caps, pFuppppp)
 GO(gst_riff_create_video_template_caps, pFv)
 GO(gst_riff_init, vFv)
-//GO(gst_riff_parse_chunk, 
-//GO(gst_riff_parse_file_header, 
-//GO(gst_riff_parse_info, 
-//GO(gst_riff_parse_strf_auds, 
-//GO(gst_riff_parse_strf_iavs, 
-//GO(gst_riff_parse_strf_vids, 
-//GO(gst_riff_parse_strh, 
-//GO(gst_riff_read_chunk, 
+GO(gst_riff_parse_chunk, iFppppp)
+GO(gst_riff_parse_file_header, iFppp)
+GO(gst_riff_parse_info, vFppp)
+GO(gst_riff_parse_strf_auds, iFpppp)
+GO(gst_riff_parse_strf_iavs, iFpppp)
+GO(gst_riff_parse_strf_vids, iFpppp)
+GO(gst_riff_parse_strh, iFppp)
+GO(gst_riff_read_chunk, iFppppp)
