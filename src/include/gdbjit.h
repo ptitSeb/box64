6907d15e08b4d61a4a4782c0a029d26907e53d3b
Yang Liu <numbksco@gmail.com>
2024-12-18 14:29:37 +0800

    [GDBJIT] Show x64pc in gdb

--- src/include/gdbjit.h
+++ src/include/gdbjit.h
@@ -11,13 +11,14 @@ typedef struct gdbjit_block_s {
     FILE* file;
     GDB_CORE_ADDR start;
     GDB_CORE_ADDR end;
-    uintptr_t alloced;
+    uintptr_t x64start;
+    size_t alloced;
     size_t nlines;
     struct gdb_line_mapping lines[0];
 } gdbjit_block_t;
 
 
-void GdbJITNewBlock(gdbjit_block_t* block, GDB_CORE_ADDR start, GDB_CORE_ADDR end);
+void GdbJITNewBlock(gdbjit_block_t* block, GDB_CORE_ADDR start, GDB_CORE_ADDR end, uintptr_t x64start);
 gdbjit_block_t* GdbJITBlockAddLine(gdbjit_block_t* block, GDB_CORE_ADDR addr, const char* line);
 void GdbJITBlockReady(gdbjit_block_t* block);
 
