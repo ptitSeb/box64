8712345ea55c1908deb0a404986a658316c76cc6
Yang Liu <numbksco@gmail.com>
2025-02-17 22:46:14 +0800

    [INTERP] Added 0F 19 NOP opcode ([DYNAREC] too)

--- src/dynarec/la64/dynarec_la64_0f.c
+++ src/dynarec/la64/dynarec_la64_0f.c
@@ -248,6 +248,7 @@ uintptr_t dynarec64_0F(dynarec_la64_t* dyn, uintptr_t addr, uintptr_t ip, int ni
                         FAKEED;
                 }
             break;
+        case 0x19:
         case 0x1F:
             INST_NAME("NOP (multibyte)");
             nextop = F8;
--- src/emu/x64run0f.c
+++ src/emu/x64run0f.c
@@ -283,6 +283,7 @@ uintptr_t Run0F(x64emu_t *emu, rex_t rex, uintptr_t addr, int *step)
             }
             break;
 
+        case 0x19:
         case 0x1F:                      /* NOP (multi-byte) */
             nextop = F8;
             FAKEED(0);
