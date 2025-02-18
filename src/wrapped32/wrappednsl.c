611ac3933c655690b09f14cc7b25fa48af54f64a
ptitSeb <sebastien.chev@gmail.com>
2025-02-18 18:12:28 +0100

    [LA64][WRAPPING][BOX32] Added alt name for libnsl

--- src/wrapped32/wrappednsl.c
+++ src/wrapped32/wrappednsl.c
@@ -11,7 +11,11 @@
 #include "librarian/library_private.h"
 #include "x64emu.h"
 
+#ifdef LA64
+static const char* nslName = "libnsl.so.2";
+#else
 static const char* nslName = "libnsl.so.1";
+#endif
 #define LIBNAME nsl
 
 #include "wrappedlib_init32.h"
