73b5311acff1ec97412e7d705b31c1ef95d994ba
airidosas252 <airidaslideikis@gmail.com>
2025-03-10 18:33:15 +0200

    [WRAPPER] add missing Android specific shmem symbols
    It can't create X server without these symbols, it works after adding them.

--- src/wrapped/wrappedandroidshmem_private.h
+++ src/wrapped/wrappedandroidshmem_private.h
@@ -2,6 +2,10 @@
 #error Meh....
 #endif
 
+GO(libandroid_shmdt, iFp)
+GO(libandroid_shmctl, iFiip)
+GO(libandroid_shmat, pFipi)
+GO(libandroid_shmget, iFiLi)
 GO(shmctl, iFiip)
 GO(shmget, iFiLi)
 GO(shmat, pFipi)
