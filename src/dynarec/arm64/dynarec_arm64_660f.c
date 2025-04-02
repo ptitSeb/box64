3f90221b18f4da5dbecdf60f7d7deb7fa6e60e39
ptitSeb <sebastien.chev@gmail.com>
2025-04-02 14:21:46 +0200

    [ARM64_DYNAREC] Fixed an optim in BLENDPS opcode

--- src/dynarec/arm64/dynarec_arm64_660f.c
+++ src/dynarec/arm64/dynarec_arm64_660f.c
@@ -1023,11 +1023,11 @@ uintptr_t dynarec64_660F(dynarec_arm_t* dyn, uintptr_t addr, uintptr_t ip, int n
                     GETGX(q0, 1);
                     GETEX(q1, 0, 1);
                     u8 = F8&0b1111;
-                    if(u8==0b0011) {
+                    if((u8&0b0011)==0b0011) {
                         VMOVeD(q0, 0, q1, 0);
                         u8&=~0b0011;
                     }
-                    if(u8==0b1100) {
+                    if((u8&0b1100)==0b1100) {
                         VMOVeD(q0, 1, q1, 1);
                         u8&=~0b1100;
                     }
