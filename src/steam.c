70770db8bec892e66fa8b6834f1ea4a8f27325b6
ptitSeb <sebastien.chev@gmail.com>
2025-04-09 14:28:04 +0200

    Small change on Steam game launch handling

--- src/steam.c
+++ src/steam.c
@@ -101,7 +101,7 @@ void pressure_vessel(int argc, const char** argv, int nextarg, const char* prog)
                 ldcmd = usrsbinldconfig;
             else if(FileExist(sbinldconfig, IS_FILE))
                 ldcmd = sbinldconfig;
-            char tmp[MAX_PATH*4] = {0};
+            char tmp[MAX_PATH*5] = {0};
             // prepare folders, using ldconfig
             snprintf(tmp, sizeof(tmp), "%s -i -n %s/lib/x86_64-linux-gnu", ldcmd, sniper);
             if(system(tmp)<0) printf_log(LOG_INFO, "%s failed\n", tmp);
@@ -111,9 +111,11 @@ void pressure_vessel(int argc, const char** argv, int nextarg, const char* prog)
             if(system(tmp)<0) printf_log(LOG_INFO, "%s failed\n", tmp);
             snprintf(tmp, sizeof(tmp), "%s -i -n %s/lib64", ldcmd, sniper);
             if(system(tmp)<0) printf_log(LOG_INFO, "%s failed\n", tmp);
+            snprintf(tmp, sizeof(tmp), "%s -i -n %s/lib32", ldcmd, sniper);
+            if(system(tmp)<0) printf_log(LOG_INFO, "%s failed\n", tmp);
             // setup LD_LIBRARY_PATH
             const char* ld = getenv("LD_LIBRARY_PATH");
-            snprintf(tmp, sizeof(tmp), "%s/lib/x86_64-linux-gnu:%s/lib/i386-linux-gnu:%s/lib:%s/lib64:%s", sniper, sniper, sniper, sniper, ld?ld:"");
+            snprintf(tmp, sizeof(tmp), "%s/lib/x86_64-linux-gnu:%s/lib/i386-linux-gnu:%s/lib:%s/lib64:%s/lib32:%s", sniper, sniper, sniper, sniper, ld?ld:"");
             setenv("LD_LIBRARY_PATH", tmp, 1);
             printf_log(LOG_DEBUG, "setenv(%s, %s, 1)\n", "LD_LIBRARY_PATH", tmp);
         }
