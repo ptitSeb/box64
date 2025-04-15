990bd81481abf8f7fd44fc821834cbe2094f5c96
Yang Liu <liuyang22@iscas.ac.cn>
2025-04-15 22:31:58 +0800

    [WOW64] Added PE build to the build system

---
+++ wow64/toolchain_mingw.cmake
@@ -0,0 +1,5 @@
+set(CMAKE_SYSTEM_NAME Windows)
+set(CMAKE_SYSTEM_PROCESSOR aarch64)
+
+set(CMAKE_C_COMPILER /home/ksco/llvm-mingw/bin/aarch64-w64-mingw32-clang)
+set(CMAKE_ASM_COMPILER /home/ksco/llvm-mingw/bin/aarch64-w64-mingw32-as)
