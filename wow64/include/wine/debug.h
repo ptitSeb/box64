46a91e0ad92e3d8de6e6fc04b10dcc453a8d69cf
Yang Liu <liuyang22@iscas.ac.cn>
2025-04-16 21:12:02 +0800

    [WOW64] More tweaks to CMake PE build (#2541)

---
+++ wow64/include/wine/debug.h
@@ -0,0 +1,8 @@
+#ifndef __WINE_DEBUG_H__
+#define __WINE_DEBUG_H__
+
+#include <windef.h>
+
+int __cdecl __wine_dbg_output( const char *str );
+
+#endif // __WINE_DEBUG_H__
--- wow64/toolchain_mingw.cmake
+++ wow64/toolchain_mingw.cmake
@@ -1,5 +1,5 @@
 set(CMAKE_SYSTEM_NAME Windows)
 set(CMAKE_SYSTEM_PROCESSOR aarch64)
 
-set(CMAKE_C_COMPILER /home/ksco/llvm-mingw/bin/aarch64-w64-mingw32-clang)
-set(CMAKE_ASM_COMPILER /home/ksco/llvm-mingw/bin/aarch64-w64-mingw32-as)
+set(CMAKE_C_COMPILER aarch64-w64-mingw32-clang)
+set(CMAKE_ASM_COMPILER aarch64-w64-mingw32-as)
