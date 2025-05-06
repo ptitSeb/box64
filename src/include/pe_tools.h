a139af3510eabfaf696f4d85223fb00e9d85344b
Yang Liu <liuyang22@iscas.ac.cn>
2025-05-06 19:14:49 +0800

    Added a simple PE loaded dedicated for volatileMetadata

---
+++ src/include/pe_tools.h
@@ -0,0 +1,7 @@
+#ifndef __PE_TOOLS_H__
+#define __PE_TOOLS_H__
+
+
+void ParseVolatileMetadata(char* filename, void* addr);
+
+#endif // __PE_TOOLS_H__
--- src/tools/wine_tools.c
+++ src/tools/wine_tools.c
@@ -136,16 +136,11 @@ void dynarec_wine_prereserve()
 }
 #endif
 
-void DetectUnityPlayer(int fd)
+void DetectUnityPlayer(char* filename)
 {
     static int unityplayer_detected = 0;
-    if (fd > 0 && BOX64ENV(unityplayer) && !unityplayer_detected) {
-        char filename[4096];
-        char buf[128];
-        sprintf(buf, "/proc/self/fd/%d", fd);
-        ssize_t r = readlink(buf, filename, sizeof(filename) - 1);
-        if (r != -1) filename[r] = 0;
-        if (r > 0 && strlen(filename) > strlen("UnityPlayer.dll") && !strcasecmp(filename + strlen(filename) - strlen("UnityPlayer.dll"), "UnityPlayer.dll")) {
+    if (!filename && BOX64ENV(unityplayer) && !unityplayer_detected) {
+        if (strlen(filename) > strlen("UnityPlayer.dll") && !strcasecmp(filename + strlen(filename) - strlen("UnityPlayer.dll"), "UnityPlayer.dll")) {
             printf_log(LOG_NONE, "Detected UnityPlayer.dll\n");
 #ifdef DYNAREC
             if (!BOX64ENV(dynarec_strongmem)) {
