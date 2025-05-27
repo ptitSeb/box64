ptitSeb/box64

commit abd1ebb425d23e693847b5796ab207453e181bd6

[RV64_DYNAREC] Minor nativeflags optim to LEA and CMOVcc opcodes (#2669)

src/dynarec/rv64/dynarec_rv64_helper.c
@@ -66,7 +66,7 @@ uintptr_t geted(dynarec_rv64_t* dyn, uintptr_t addr, int ninst, uint8_t nextop, 
                     } else {
                         if (sib >> 6) {
                             SLLI(ret, TO_NAT(sib_reg), (sib >> 6));
-                            SCRATCH_USAGE(1);
+                            SCRATCH_USAGE(!IS_GPR(ret));
                         } else
                             ret = TO_NAT(sib_reg);
                         *fixaddress = tmp;
@@ -79,12 +79,24 @@ uintptr_t geted(dynarec_rv64_t* dyn, uintptr_t addr, int ninst, uint8_t nextop, 
                             break;
                     }
                     MOV64x(ret, tmp);
-                    SCRATCH_USAGE(1);
+                    SCRATCH_USAGE(!IS_GPR(ret));
                 }
             } else {
                 if (sib_reg != 4) {
-                    ADDSL(ret, TO_NAT(sib_reg2), TO_NAT(sib_reg), sib >> 6, scratch);
-                    SCRATCH_USAGE(1);
+                    if (!(sib >> 6)) {
+                        ADD(ret, TO_NAT(sib_reg2), TO_NAT(sib_reg));
+                        SCRATCH_USAGE(!IS_GPR(ret));
+                    } else if (rv64_zba) {
+                        SHxADD(ret, TO_NAT(sib_reg), sib >> 6, TO_NAT(sib_reg2));
+                        SCRATCH_USAGE(!IS_GPR(ret));
+                    } else if (rv64_xtheadba) {
+                        TH_ADDSL(ret, TO_NAT(sib_reg2), TO_NAT(sib_reg), sib >> 6);
+                        SCRATCH_USAGE(!IS_GPR(ret));
+                    } else {
+                        SLLI(scratch, TO_NAT(sib_reg), sib >> 6);
+                        ADD(ret, TO_NAT(sib_reg2), scratch);
+                        SCRATCH_USAGE(1);
+                    }
                 } else {
                     ret = TO_NAT(sib_reg2);
                 }
@@ -102,23 +114,24 @@ uintptr_t geted(dynarec_rv64_t* dyn, uintptr_t addr, int ninst, uint8_t nextop, 
                 SCRATCH_USAGE(1);
             } else if (adj && (tmp + adj >= -2048) && (tmp + adj <= maxval)) {
                 ADDI(ret, xRIP, tmp + adj);
-                SCRATCH_USAGE(1);
+                SCRATCH_USAGE(!IS_GPR(ret));
             } else if ((tmp >= -2048) && (tmp <= maxval)) {
                 GETIP(addr + delta, scratch);
                 ADDI(ret, xRIP, tmp);
                 SCRATCH_USAGE(1);
             } else if (tmp + addr + delta < 0x100000000LL) {
                 MOV64x(ret, tmp + addr + delta);
-                SCRATCH_USAGE(1);
+                SCRATCH_USAGE(!IS_GPR(ret));
             } else {
                 if (adj) {
                     MOV64x(ret, tmp + adj);
+                    SCRATCH_USAGE(!IS_GPR(ret));
                 } else {
                     MOV64x(ret, tmp);
                     GETIP(addr + delta, scratch);
+                    SCRATCH_USAGE(1);
                 }
                 ADD(ret, ret, xRIP);
-                SCRATCH_USAGE(1);
             }
             switch (lock) {
                 case 1: addLockAddress(addr + delta + tmp); break;
@@ -146,8 +159,20 @@ uintptr_t geted(dynarec_rv64_t* dyn, uintptr_t addr, int ninst, uint8_t nextop, 
             *fixaddress = i64;
             if ((nextop & 7) == 4) {
                 if (sib_reg != 4) {
-                    ADDSL(ret, TO_NAT(sib_reg2), TO_NAT(sib_reg), sib >> 6, scratch);
-                    SCRATCH_USAGE(1);
+                    if (!(sib >> 6)) {
+                        ADD(ret, TO_NAT(sib_reg2), TO_NAT(sib_reg));
+                        SCRATCH_USAGE(!IS_GPR(ret));
+                    } else if (rv64_zba) {
+                        SHxADD(ret, TO_NAT(sib_reg), sib >> 6, TO_NAT(sib_reg2));
+                        SCRATCH_USAGE(!IS_GPR(ret));
+                    } else if (rv64_xtheadba) {
+                        TH_ADDSL(ret, TO_NAT(sib_reg2), TO_NAT(sib_reg), sib >> 6);
+                        SCRATCH_USAGE(!IS_GPR(ret));
+                    } else {
+                        SLLI(scratch, TO_NAT(sib_reg), sib >> 6);
+                        ADD(ret, TO_NAT(sib_reg2), scratch);
+                        SCRATCH_USAGE(1);
+                    }
                 } else {
                     ret = TO_NAT(sib_reg2);
                 }
@@ -157,30 +182,43 @@ uintptr_t geted(dynarec_rv64_t* dyn, uintptr_t addr, int ninst, uint8_t nextop, 
             if (i64 >= -2048 && i64 <= 2047) {
                 if ((nextop & 7) == 4) {
                     if (sib_reg != 4) {
-                        ADDSL(scratch, TO_NAT(sib_reg2), TO_NAT(sib_reg), sib >> 6, scratch);
+                        if (!(sib >> 6)) {
+                            ADD(ret, TO_NAT(sib_reg2), TO_NAT(sib_reg));
+                            SCRATCH_USAGE(!IS_GPR(ret));
+                        } else if (rv64_zba) {
+                            SHxADD(ret, TO_NAT(sib_reg), sib >> 6, TO_NAT(sib_reg2));
+                            SCRATCH_USAGE(!IS_GPR(ret));
+                        } else if (rv64_xtheadba) {
+                            TH_ADDSL(ret, TO_NAT(sib_reg2), TO_NAT(sib_reg), sib >> 6);
+                            SCRATCH_USAGE(!IS_GPR(ret));
+                        } else {
+                            SLLI(scratch, TO_NAT(sib_reg), sib >> 6);
+                            ADD(ret, TO_NAT(sib_reg2), scratch);
+                            SCRATCH_USAGE(1);
+                        }
+                        ADDI(ret, ret, i64);
                     } else {
-                        scratch = TO_NAT(sib_reg2);
+                        ADDI(ret, TO_NAT(sib_reg2), i64);
+                        SCRATCH_USAGE(!IS_GPR(ret));
                     }
-                } else
-                    scratch = TO_NAT((nextop & 0x07) + (rex.b << 3));
-                ADDI(ret, scratch, i64);
-                SCRATCH_USAGE(1);
+                } else {
+                    ADDI(ret, TO_NAT((nextop & 0x07) + (rex.b << 3)), i64);
+                    SCRATCH_USAGE(!IS_GPR(ret));
+                }
             } else {
                 MOV64x(scratch, i64);
+                SCRATCH_USAGE(1);
                 if ((nextop & 7) == 4) {
                     if (sib_reg != 4) {
                         ADD(scratch, scratch, TO_NAT(sib_reg2));
                         ADDSL(ret, scratch, TO_NAT(sib_reg), sib >> 6, ret);
-                        SCRATCH_USAGE(1);
                     } else {
                         PASS3(int tmp = TO_NAT(sib_reg2));
                         ADD(ret, tmp, scratch);
-                        SCRATCH_USAGE(1);
                     }
                 } else {
                     PASS3(int tmp = TO_NAT((nextop & 0x07) + (rex.b << 3)));
                     ADD(ret, tmp, scratch);
-                    SCRATCH_USAGE(1);
                 }
             }
         }
@@ -226,7 +264,7 @@ static uintptr_t geted_32(dynarec_rv64_t* dyn, uintptr_t addr, int ninst, uint8_
                     } else {
                         if (sib >> 6) {
                             SLLI(ret, TO_NAT(sib_reg), (sib >> 6));
-                            SCRATCH_USAGE(1);
+                            SCRATCH_USAGE(!IS_GPR(ret));
                         } else
                             ret = TO_NAT(sib_reg);
                         *fixaddress = tmp;
@@ -239,18 +277,19 @@ static uintptr_t geted_32(dynarec_rv64_t* dyn, uintptr_t addr, int ninst, uint8_
                             break;
                     }
                     MOV32w(ret, tmp);
-                    SCRATCH_USAGE(1);
+                    SCRATCH_USAGE(!IS_GPR(ret));
                 }
             } else {
                 if (sib_reg != 4) {
                     if ((sib >> 6)) {
                         SLLI(scratch, TO_NAT(sib_reg), (sib >> 6));
                         ADDW(ret, scratch, TO_NAT(sib_reg2));
+                        SCRATCH_USAGE(1);
                     } else {
                         ADDW(ret, TO_NAT(sib_reg2), TO_NAT(sib_reg));
+                        SCRATCH_USAGE(!IS_GPR(ret));
                     }
                     ZEROUP(ret);
-                    SCRATCH_USAGE(1);
                 } else {
                     ret = TO_NAT(sib_reg2);
                 }
@@ -258,7 +297,7 @@ static uintptr_t geted_32(dynarec_rv64_t* dyn, uintptr_t addr, int ninst, uint8_
         } else if ((nextop & 7) == 5) {
             uint32_t tmp = F32;
             MOV32w(ret, tmp);
-            SCRATCH_USAGE(1);
+            SCRATCH_USAGE(!IS_GPR(ret));
             switch (lock) {
                 case 1: addLockAddress(tmp); break;
                 case 2:
@@ -291,11 +330,12 @@ static uintptr_t geted_32(dynarec_rv64_t* dyn, uintptr_t addr, int ninst, uint8_
                     if (sib >> 6) {
                         SLLI(scratch, TO_NAT(sib_reg), (sib >> 6));
                         ADDW(ret, scratch, TO_NAT(sib_reg2));
+                        SCRATCH_USAGE(1);
                     } else {
                         ADDW(ret, TO_NAT(sib_reg2), TO_NAT(sib_reg));
+                        SCRATCH_USAGE(!IS_GPR(ret));
                     }
                     ZEROUP(ret);
-                    SCRATCH_USAGE(1);
                 } else {
                     ret = TO_NAT(sib_reg2);
                 }
@@ -311,14 +351,17 @@ static uintptr_t geted_32(dynarec_rv64_t* dyn, uintptr_t addr, int ninst, uint8_
                             ADDW(scratch, scratch, TO_NAT(sib_reg2));
                         } else
                             ADDW(scratch, TO_NAT(sib_reg2), TO_NAT(sib_reg));
+                        SCRATCH_USAGE(1);
                     } else {
                         scratch = TO_NAT(sib_reg2);
+                        SCRATCH_USAGE(!IS_GPR(ret));
                     }
-                } else
+                } else {
                     scratch = TO_NAT(nextop & 0x07);
+                    SCRATCH_USAGE(!IS_GPR(ret));
+                }
                 ADDIW(ret, scratch, i32);
                 ZEROUP(ret);
-                SCRATCH_USAGE(1);
             } else {
                 // no need to zero up, as we did it below
                 rv64_move32(dyn, ninst, scratch, i32, 0);

src/dynarec/rv64/dynarec_rv64_helper.h
@@ -1823,6 +1823,7 @@ uintptr_t dynarec64_AVX_F3_0F(dynarec_rv64_t* dyn, uintptr_t addr, uintptr_t ip,
 #define B__safe(a, b, c) XOR(xZR, xZR, xZR)
 #define B_(a, b, c)      XOR(xZR, xZR, xZR)
 #define S_(a, b, c)      XOR(xZR, xZR, xZR)
+#define MV_(a, b, c, d)  XOR(xZR, xZR, xZR)
 
 #define NATIVEJUMP_safe(COND, val) \
     B##COND##_safe(dyn->insts[ninst].nat_flags_op1, dyn->insts[ninst].nat_flags_op2, val);
@@ -1832,6 +1833,9 @@ uintptr_t dynarec64_AVX_F3_0F(dynarec_rv64_t* dyn, uintptr_t addr, uintptr_t ip,
 
 #define NATIVESET(COND, rd) \
     S##COND(rd, dyn->insts[ninst].nat_flags_op1, dyn->insts[ninst].nat_flags_op2);
+
+#define NATIVEMV(COND, rd, rs) \
+    MV##COND(rd, rs, dyn->insts[ninst].nat_flags_op1, dyn->insts[ninst].nat_flags_op2);
 
 #define NOTEST(s1)                                     \
     if (BOX64ENV(dynarec_test)) {                      \

src/dynarec/rv64/rv64_emitter.h
@@ -251,6 +251,48 @@
 #define SGTU(rd, rs1, rs2) SLTU(rd, rs2, rs1);
 #define SLEU(rd, rs1, rs2) SGEU(rd, rs2, rs1);
 
+#define MVEQ(rd, rs1, rs2, rs3)                             \
+    if (rv64_xtheadcondmov && (rs2 == xZR || rs3 == xZR)) { \
+        TH_MVEQZ(rd, rs1, ((rs2 == xZR) ? rs3 : rs2));      \
+    } else {                                                \
+        BNE(rs2, rs3, 8);                                   \
+        MV(rd, rs1);                                        \
+    }
+#define MVNE(rd, rs1, rs2, rs3)                             \
+    if (rv64_xtheadcondmov && (rs2 == xZR || rs3 == xZR)) { \
+        TH_MVNEZ(rd, rs1, ((rs2 == xZR) ? rs3 : rs2));      \
+    } else {                                                \
+        BEQ(rs2, rs3, 8);                                   \
+        MV(rd, rs1);                                        \
+    }
+#define MVLT(rd, rs1, rs2, rs3) \
+    BGE(rs2, rs3, 8);           \
+    MV(rd, rs1);
+#define MVGE(rd, rs1, rs2, rs3) \
+    BLT(rs2, rs3, 8);           \
+    MV(rd, rs1);
+#define MVLTU(rd, rs1, rs2, rs3) \
+    BGEU(rs2, rs3, 8);           \
+    MV(rd, rs1);
+#define MVGEU(rd, rs1, rs2, rs3) \
+    BLTU(rs2, rs3, 8);           \
+    MV(rd, rs1);
+#define MVGT(rd, rs1, rs2, rs3) \
+    BGEU(rs3, rs2, 8);          \
+    MV(rd, rs1);
+#define MVLE(rd, rs1, rs2, rs3) \
+    BLT(rs3, rs2, 8);           \
+    MV(rd, rs1);
+#define MVGTU(rd, rs1, rs2, rs3) \
+    BGEU(rs3, rs2, 8);           \
+    MV(rd, rs1);
+#define MVLEU(rd, rs1, rs2, rs3) \
+    BLTU(rs3, rs2, 8);           \
+    MV(rd, rs1);
+
+#define MVEQZ(rd, rs1, rs2) MVEQ(rd, rs1, rs2, xZR)
+#define MVNEZ(rd, rs1, rs2) MVNE(rd, rs1, rs2, xZR)
+
 #define BEQ_safe(rs1, rs2, imm)              \
     if ((imm) > -0x1000 && (imm) < 0x1000) { \
         BEQ(rs1, rs2, imm);                  \

src/dynarec/rv64/dynarec_rv64_00_2.c
@@ -495,6 +495,7 @@ uintptr_t dynarec64_00_2(dynarec_rv64_t* dyn, uintptr_t addr, uintptr_t ip, int 
             if (MODREG) { // reg <= reg? that's an invalid operation
                 DEFAULT;
             } else { // mem <= reg
+                SCRATCH_USAGE(0);
                 addr = geted(dyn, addr, ninst, nextop, &ed, gd, x1, &fixedaddress, rex, NULL, 0, 0);
                 if (gd != ed) {
                     MVxw(gd, ed);

src/dynarec/rv64/dynarec_rv64_0f.c
@@ -937,11 +937,10 @@ uintptr_t dynarec64_0F(dynarec_rv64_t* dyn, uintptr_t addr, uintptr_t ip, int ni
     if (MODREG) {                                                                                \
         ed = TO_NAT((nextop & 7) + (rex.b << 3));                                                \
         if (dyn->insts[ninst].nat_flags_fusion) {                                                \
-            NATIVEJUMP(NATNO, 8);                                                                \
+            NATIVEMV(NATYES, gd, ed);                                                            \
         } else {                                                                                 \
-            B##NO(tmp1, 8);                                                                      \
+            MV##YES(gd, ed, tmp1);                                                               \
         }                                                                                        \
-        MV(gd, ed);                                                                              \
         if (!rex.w) ZEROUP(gd);                                                                  \
     } else {                                                                                     \
         addr = geted(dyn, addr, ninst, nextop, &ed, tmp2, tmp3, &fixedaddress, rex, NULL, 1, 0); \

src/dynarec/rv64/dynarec_rv64_67.c
@@ -739,6 +739,7 @@ uintptr_t dynarec64_67(dynarec_rv64_t* dyn, uintptr_t addr, uintptr_t ip, int ni
             if (MODREG) { // reg <= reg? that's an invalid operation
                 DEFAULT;
             } else { // mem <= reg
+                SCRATCH_USAGE(0);
                 addr = geted32(dyn, addr, ninst, nextop, &ed, gd, x1, &fixedaddress, rex, NULL, 0, 0);
                 ZEXTW2(gd, ed);
             }

commit c2b230759a826c6e2fc159928a6a24762a19a285

[RV64_DYNAREC] Fixed regression introduced in #2669

src/dynarec/rv64/dynarec_rv64_helper.c
@@ -66,7 +66,7 @@ uintptr_t geted(dynarec_rv64_t* dyn, uintptr_t addr, int ninst, uint8_t nextop, 
                     } else {
                         if (sib >> 6) {
                             SLLI(ret, TO_NAT(sib_reg), (sib >> 6));
-                            SCRATCH_USAGE(!IS_GPR(ret));
+                            if (!IS_GPR(ret)) SCRATCH_USAGE(1);
                         } else
                             ret = TO_NAT(sib_reg);
                         *fixaddress = tmp;
@@ -79,19 +79,19 @@ uintptr_t geted(dynarec_rv64_t* dyn, uintptr_t addr, int ninst, uint8_t nextop, 
                             break;
                     }
                     MOV64x(ret, tmp);
-                    SCRATCH_USAGE(!IS_GPR(ret));
+                    if (!IS_GPR(ret)) SCRATCH_USAGE(1);
                 }
             } else {
                 if (sib_reg != 4) {
                     if (!(sib >> 6)) {
                         ADD(ret, TO_NAT(sib_reg2), TO_NAT(sib_reg));
-                        SCRATCH_USAGE(!IS_GPR(ret));
+                        if (!IS_GPR(ret)) SCRATCH_USAGE(1);
                     } else if (rv64_zba) {
                         SHxADD(ret, TO_NAT(sib_reg), sib >> 6, TO_NAT(sib_reg2));
-                        SCRATCH_USAGE(!IS_GPR(ret));
+                        if (!IS_GPR(ret)) SCRATCH_USAGE(1);
                     } else if (rv64_xtheadba) {
                         TH_ADDSL(ret, TO_NAT(sib_reg2), TO_NAT(sib_reg), sib >> 6);
-                        SCRATCH_USAGE(!IS_GPR(ret));
+                        if (!IS_GPR(ret)) SCRATCH_USAGE(1);
                     } else {
                         SLLI(scratch, TO_NAT(sib_reg), sib >> 6);
                         ADD(ret, TO_NAT(sib_reg2), scratch);
@@ -114,18 +114,15 @@ uintptr_t geted(dynarec_rv64_t* dyn, uintptr_t addr, int ninst, uint8_t nextop, 
                 SCRATCH_USAGE(1);
             } else if (adj && (tmp + adj >= -2048) && (tmp + adj <= maxval)) {
                 ADDI(ret, xRIP, tmp + adj);
-                SCRATCH_USAGE(!IS_GPR(ret));
             } else if ((tmp >= -2048) && (tmp <= maxval)) {
                 GETIP(addr + delta, scratch);
                 ADDI(ret, xRIP, tmp);
                 SCRATCH_USAGE(1);
             } else if (tmp + addr + delta < 0x100000000LL) {
                 MOV64x(ret, tmp + addr + delta);
-                SCRATCH_USAGE(!IS_GPR(ret));
             } else {
                 if (adj) {
                     MOV64x(ret, tmp + adj);
-                    SCRATCH_USAGE(!IS_GPR(ret));
                 } else {
                     MOV64x(ret, tmp);
                     GETIP(addr + delta, scratch);
@@ -139,6 +136,7 @@ uintptr_t geted(dynarec_rv64_t* dyn, uintptr_t addr, int ninst, uint8_t nextop, 
                     if (isLockAddress(addr + delta + tmp)) *l = 1;
                     break;
             }
+            SCRATCH_USAGE(1);
         } else {
             ret = TO_NAT((nextop & 7) + (rex.b << 3));
         }
@@ -161,13 +159,13 @@ uintptr_t geted(dynarec_rv64_t* dyn, uintptr_t addr, int ninst, uint8_t nextop, 
                 if (sib_reg != 4) {
                     if (!(sib >> 6)) {
                         ADD(ret, TO_NAT(sib_reg2), TO_NAT(sib_reg));
-                        SCRATCH_USAGE(!IS_GPR(ret));
+                        if (!IS_GPR(ret)) SCRATCH_USAGE(1);
                     } else if (rv64_zba) {
                         SHxADD(ret, TO_NAT(sib_reg), sib >> 6, TO_NAT(sib_reg2));
-                        SCRATCH_USAGE(!IS_GPR(ret));
+                        if (!IS_GPR(ret)) SCRATCH_USAGE(1);
                     } else if (rv64_xtheadba) {
                         TH_ADDSL(ret, TO_NAT(sib_reg2), TO_NAT(sib_reg), sib >> 6);
-                        SCRATCH_USAGE(!IS_GPR(ret));
+                        if (!IS_GPR(ret)) SCRATCH_USAGE(1);
                     } else {
                         SLLI(scratch, TO_NAT(sib_reg), sib >> 6);
                         ADD(ret, TO_NAT(sib_reg2), scratch);
@@ -184,13 +182,13 @@ uintptr_t geted(dynarec_rv64_t* dyn, uintptr_t addr, int ninst, uint8_t nextop, 
                     if (sib_reg != 4) {
                         if (!(sib >> 6)) {
                             ADD(ret, TO_NAT(sib_reg2), TO_NAT(sib_reg));
-                            SCRATCH_USAGE(!IS_GPR(ret));
+                            if (!IS_GPR(ret)) SCRATCH_USAGE(1);
                         } else if (rv64_zba) {
                             SHxADD(ret, TO_NAT(sib_reg), sib >> 6, TO_NAT(sib_reg2));
-                            SCRATCH_USAGE(!IS_GPR(ret));
+                            if (!IS_GPR(ret)) SCRATCH_USAGE(1);
                         } else if (rv64_xtheadba) {
                             TH_ADDSL(ret, TO_NAT(sib_reg2), TO_NAT(sib_reg), sib >> 6);
-                            SCRATCH_USAGE(!IS_GPR(ret));
+                            if (!IS_GPR(ret)) SCRATCH_USAGE(1);
                         } else {
                             SLLI(scratch, TO_NAT(sib_reg), sib >> 6);
                             ADD(ret, TO_NAT(sib_reg2), scratch);
@@ -199,11 +197,11 @@ uintptr_t geted(dynarec_rv64_t* dyn, uintptr_t addr, int ninst, uint8_t nextop, 
                         ADDI(ret, ret, i64);
                     } else {
                         ADDI(ret, TO_NAT(sib_reg2), i64);
-                        SCRATCH_USAGE(!IS_GPR(ret));
+                        if (!IS_GPR(ret)) SCRATCH_USAGE(1);
                     }
                 } else {
                     ADDI(ret, TO_NAT((nextop & 0x07) + (rex.b << 3)), i64);
-                    SCRATCH_USAGE(!IS_GPR(ret));
+                    if (!IS_GPR(ret)) SCRATCH_USAGE(1);
                 }
             } else {
                 MOV64x(scratch, i64);
@@ -264,7 +262,7 @@ static uintptr_t geted_32(dynarec_rv64_t* dyn, uintptr_t addr, int ninst, uint8_
                     } else {
                         if (sib >> 6) {
                             SLLI(ret, TO_NAT(sib_reg), (sib >> 6));
-                            SCRATCH_USAGE(!IS_GPR(ret));
+                            if (!IS_GPR(ret)) SCRATCH_USAGE(1);
                         } else
                             ret = TO_NAT(sib_reg);
                         *fixaddress = tmp;
@@ -277,7 +275,7 @@ static uintptr_t geted_32(dynarec_rv64_t* dyn, uintptr_t addr, int ninst, uint8_
                             break;
                     }
                     MOV32w(ret, tmp);
-                    SCRATCH_USAGE(!IS_GPR(ret));
+                    if (!IS_GPR(ret)) SCRATCH_USAGE(1);
                 }
             } else {
                 if (sib_reg != 4) {
@@ -287,7 +285,7 @@ static uintptr_t geted_32(dynarec_rv64_t* dyn, uintptr_t addr, int ninst, uint8_
                         SCRATCH_USAGE(1);
                     } else {
                         ADDW(ret, TO_NAT(sib_reg2), TO_NAT(sib_reg));
-                        SCRATCH_USAGE(!IS_GPR(ret));
+                        if (!IS_GPR(ret)) SCRATCH_USAGE(1);
                     }
                     ZEROUP(ret);
                 } else {
@@ -297,7 +295,7 @@ static uintptr_t geted_32(dynarec_rv64_t* dyn, uintptr_t addr, int ninst, uint8_
         } else if ((nextop & 7) == 5) {
             uint32_t tmp = F32;
             MOV32w(ret, tmp);
-            SCRATCH_USAGE(!IS_GPR(ret));
+            if (!IS_GPR(ret)) SCRATCH_USAGE(1);
             switch (lock) {
                 case 1: addLockAddress(tmp); break;
                 case 2:
@@ -333,7 +331,7 @@ static uintptr_t geted_32(dynarec_rv64_t* dyn, uintptr_t addr, int ninst, uint8_
                         SCRATCH_USAGE(1);
                     } else {
                         ADDW(ret, TO_NAT(sib_reg2), TO_NAT(sib_reg));
-                        SCRATCH_USAGE(!IS_GPR(ret));
+                        if (!IS_GPR(ret)) SCRATCH_USAGE(1);
                     }
                     ZEROUP(ret);
                 } else {
@@ -354,11 +352,11 @@ static uintptr_t geted_32(dynarec_rv64_t* dyn, uintptr_t addr, int ninst, uint8_
                         SCRATCH_USAGE(1);
                     } else {
                         scratch = TO_NAT(sib_reg2);
-                        SCRATCH_USAGE(!IS_GPR(ret));
+                        if (!IS_GPR(ret)) SCRATCH_USAGE(1);
                     }
                 } else {
                     scratch = TO_NAT(nextop & 0x07);
-                    SCRATCH_USAGE(!IS_GPR(ret));
+                    if (!IS_GPR(ret)) SCRATCH_USAGE(1);
                 }
                 ADDIW(ret, scratch, i32);
                 ZEROUP(ret);

src/dynarec/rv64/dynarec_rv64_helper.h
@@ -2006,9 +2006,8 @@ uintptr_t dynarec64_AVX_F3_0F(dynarec_rv64_t* dyn, uintptr_t addr, uintptr_t ip,
 #define VECTOR_LOAD_VMASK(mask, s1, multiple) \
     vector_loadmask(dyn, ninst, VMASK, mask, s1, multiple)
 
-#define SCRATCH_USAGE(usage)                         \
-    do {                                             \
-        dyn->insts[ninst].no_scratch_usage = !usage; \
-    } while (0)
+#ifndef SCRATCH_USAGE
+#define SCRATCH_USAGE(usage)
+#endif
 
 #endif //__DYNAREC_RV64_HELPER_H__

src/dynarec/rv64/dynarec_rv64_pass0.h
@@ -111,3 +111,8 @@
     do {                                                            \
         if (dyn->need_x87check) dyn->insts[ninst].x87precision = 1; \
     } while (0)
+
+#define SCRATCH_USAGE(usage)                         \
+    do {                                             \
+        dyn->insts[ninst].no_scratch_usage = !usage; \
+    } while (0)

src/dynarec/rv64/rv64_emitter.h
@@ -278,7 +278,7 @@
     BLTU(rs2, rs3, 8);           \
     MV(rd, rs1);
 #define MVGT(rd, rs1, rs2, rs3) \
-    BGEU(rs3, rs2, 8);          \
+    BGE(rs3, rs2, 8);           \
     MV(rd, rs1);
 #define MVLE(rd, rs1, rs2, rs3) \
     BLT(rs3, rs2, 8);           \