5bae8cbf33694af7a60cb51f37a3e59c29dbbfe7
Yang Liu <numbksco@gmail.com>
2025-05-29 15:58:16 +0800

    [WOW64] Added more missing pieces and the interpreter works
    Ported from AndreRH/hangover

--- src/dynarec/arm64/dynarec_arm64_00.c
+++ src/dynarec/arm64/dynarec_arm64_00.c
@@ -2646,14 +2646,14 @@ uintptr_t dynarec64_00(dynarec_arm_t* dyn, uintptr_t addr, uintptr_t ip, int nin
             break;
         case 0xCD:
             u8 = F8;
-#ifdef _WIN32
+            #ifdef _WIN32
             NOTEST(x1);
             SMEND();
             GETIP(addr);
             STORE_XEMU_CALL(xRIP);
             MOV32w(x1, u8);
             LDRx_U12(xR8, xEmu, offsetof(x64emu_t, win64_teb));
-            CALL_S(x86Int, -1);
+            CALL_S(native_int, -1);
             LOAD_XEMU_CALL(xRIP);
             TABLE64(x3, addr); // expected return address
             CMPSx_REG(xRIP, x3);
@@ -2664,7 +2664,7 @@ uintptr_t dynarec64_00(dynarec_arm_t* dyn, uintptr_t addr, uintptr_t ip, int nin
             LOAD_XEMU_REM();
             jump_to_epilog(dyn, 0, xRIP, ninst);
             break;
-#endif
+            #endif
             if(box64_wine && (u8==0x2D || u8==0x2C || u8==0x29)) {
                 INST_NAME("INT 29/2c/2d");
                 // lets do nothing
--- wow64/include/wine/compiler.h
+++ wow64/include/wine/compiler.h
@@ -4,9 +4,9 @@
 #include <stdint.h>
 
 /* Things missing from mingw64 right now */
-#define ThreadWow64Context (29)
-#define WOW64_TLS_CPURESERVED (1)
-#define WOW64_TLS_MAX_NUMBER (19)
+#define ThreadWow64Context                 (29)
+#define WOW64_TLS_CPURESERVED              (1)
+#define WOW64_TLS_MAX_NUMBER               (19)
 #define WOW64_CPURESERVED_FLAG_RESET_STATE (1)
 
 typedef enum _MEMORY_INFORMATION_CLASS {
@@ -26,46 +26,46 @@ typedef enum _MEMORY_INFORMATION_CLASS {
     MemoryBadInformationAllProcesses,
 } MEMORY_INFORMATION_CLASS;
 
-typedef struct _WOW64_CPURESERVED
-{
+typedef struct _WOW64_CPURESERVED {
     USHORT Flags;
     USHORT Machine;
 } WOW64_CPURESERVED;
 
 typedef struct _XMM_SAVE_AREA32 {
-    WORD  ControlWord;
-    WORD  StatusWord;
-    BYTE  TagWord;
-    BYTE  Reserved1;
-    WORD  ErrorOpcode;
+    WORD ControlWord;
+    WORD StatusWord;
+    BYTE TagWord;
+    BYTE Reserved1;
+    WORD ErrorOpcode;
     DWORD ErrorOffset;
-    WORD  ErrorSelector;
-    WORD  Reserved2;
+    WORD ErrorSelector;
+    WORD Reserved2;
     DWORD DataOffset;
-    WORD  DataSelector;
-    WORD  Reserved3;
+    WORD DataSelector;
+    WORD Reserved3;
     DWORD MxCsr;
     DWORD MxCsr_Mask;
     M128A FloatRegisters[8];
     M128A XmmRegisters[16];
-    BYTE  Reserved4[96];
+    BYTE Reserved4[96];
 } XMM_SAVE_AREA32;
 
-#define NtCurrentProcess() ( (HANDLE)(LONG_PTR) -1 )
+#define NtCurrentProcess() ((HANDLE)(LONG_PTR) - 1)
 
-NTSTATUS WINAPI RtlWow64GetCurrentCpuArea(USHORT *, void **, void **);
-NTSTATUS  WINAPI Wow64SystemServiceEx(UINT, UINT*);
+NTSTATUS WINAPI RtlWow64GetCurrentCpuArea(USHORT*, void**, void**);
+NTSTATUS WINAPI Wow64SystemServiceEx(UINT, UINT*);
 NTSYSAPI NTSTATUS WINAPI LdrGetDllHandle(LPCWSTR, ULONG, const UNICODE_STRING*, HMODULE*);
 NTSYSAPI NTSTATUS WINAPI NtContinue(PCONTEXT, BOOLEAN);
 NTSYSAPI NTSTATUS WINAPI NtQueryVirtualMemory(HANDLE, LPCVOID, MEMORY_INFORMATION_CLASS, PVOID, SIZE_T, SIZE_T*);
 NTSYSAPI void* WINAPI RtlFindExportedRoutineByName(HMODULE, const char*);
 NTSYSAPI void DECLSPEC_NORETURN WINAPI RtlRaiseStatus(NTSTATUS);
+NTSYSAPI void WINAPI RtlRaiseException(EXCEPTION_RECORD*);
 
 static inline uintptr_t calculate_fs(void)
 {
-/* until mingw64 has WowTebOffset in the TEB struct */
-uint8_t* teb = (uint8_t*)NtCurrentTeb();
-return (uintptr_t)(teb + *(int32_t*)(teb + 0x180c));
+    /* until mingw64 has WowTebOffset in the TEB struct */
+    uint8_t* teb = (uint8_t*)NtCurrentTeb();
+    return (uintptr_t)(teb + *(int32_t*)(teb + 0x180c));
 }
 
 #endif //__COMPILER_H_
