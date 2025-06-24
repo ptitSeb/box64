f14fa2f2ce44a6cbee3fcef58faf742bce992a66
airidosas252 <airidaslideikis@gmail.com>
2025-06-24 21:15:21 +0300

    [WRAPPER] additional wrapped symbols (#2765)
    * [WRAPPER] Wrap libiconv library
    
    Seems to be required with newer versions of Wine for whatever reason.
    
    * [WRAPPER] Wrap _ctype_ symbol (ANDROID only)
    
    For whatever reason, this symbol is used on bionic at least. Not quite sure if it's the same on Linux.
    
    * [WRAPPER} Fix a signature typo for libiconv function

--- src/wrapped/wrappediconv_private.h
+++ src/wrapped/wrappediconv_private.h
@@ -2,3 +2,14 @@
 #error Meh...
 #endif
 
+GO(libiconv_open, pFpp)
+GO(locale_charset, pFv)
+GO(libiconv_close, iFp)
+GO(libiconvctl, iFpip)
+GO(libiconv_set_relocation_prefix, vFpp)
+GO(libiconv, LFppppp)
+GO(libiconv_open_into, iFppp)
+GO(libiconvlist, vFpp)
+GO(iconv_canonicalize, pFp)
+
+DATA(_libiconv_version, 4)
