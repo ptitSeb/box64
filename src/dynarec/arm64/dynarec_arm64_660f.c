ptitSeb/box64

commit 4903177bab1f3324a0faeedd968fed5bf4ea8772

[ARM64_DYNAREC] Minor optim to MOVNTDQA (#2568)

src/dynarec/arm64/dynarec_arm64_660f.c
@@ -625,9 +625,16 @@ uintptr_t dynarec64_660F(dynarec_arm_t* dyn, uintptr_t addr, uintptr_t ip, int n
                 case 0x2A:
                     INST_NAME("MOVNTDQA Gx, Ex");
                     nextop = F8;
-                    GETEX(q1, 0, 0);
-                    GETGX(q0, 1);
-                    VMOVQ(q0, q1);
+                    if (MODREG) {
+                        v1 = sse_get_reg(dyn, ninst, x1, (nextop & 7) + (rex.b << 3), 0);
+                        GETGX_empty(v0);
+                        VMOVQ(v0, v1);
+                    } else {
+                        GETGX_empty(v0);
+                        SMREAD();
+                        addr = geted32(dyn, addr, ninst, nextop, &ed, x1, &fixedaddress, &unscaled, 0xfff << 4, 15, rex, NULL, 0, 0);
+                        VLD128(v0, ed, fixedaddress);
+                    }
                     break;
                 case 0x2B:
                     INST_NAME("PACKUSDW Gx, Ex");  // SSE4 opcode!

commit 54ea4859ecb4b89a2097251fa9b937b6e1d5a5e3

[ARM64_DYNAREC] Fixed a regression introduced with 4903177bab1f3324a0faeedd968fed5bf4ea8772 (for ForzaHorizon4, maybe other too)

src/dynarec/arm64/dynarec_arm64_660f.c
@@ -627,7 +627,7 @@ uintptr_t dynarec64_660F(dynarec_arm_t* dyn, uintptr_t addr, uintptr_t ip, int n
                     } else {
                         GETGX_empty(v0);
                         SMREAD();
-                        addr = geted32(dyn, addr, ninst, nextop, &ed, x1, &fixedaddress, &unscaled, 0xfff << 4, 15, rex, NULL, 0, 0);
+                        addr = geted(dyn, addr, ninst, nextop, &ed, x1, &fixedaddress, &unscaled, 0xfff << 4, 15, rex, NULL, 0, 0);
                         VLD128(v0, ed, fixedaddress);
                     }
                     break;
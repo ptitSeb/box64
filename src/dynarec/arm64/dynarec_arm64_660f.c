54ea4859ecb4b89a2097251fa9b937b6e1d5a5e3
ptitSeb <sebastien.chev@gmail.com>
2025-06-30 20:35:40 +0200

    [ARM64_DYNAREC] Fixed a regression introduced with 4903177bab1f3324a0faeedd968fed5bf4ea8772 (for ForzaHorizon4, maybe other too)

--- src/dynarec/arm64/dynarec_arm64_660f.c
+++ src/dynarec/arm64/dynarec_arm64_660f.c
@@ -627,7 +627,7 @@ uintptr_t dynarec64_660F(dynarec_arm_t* dyn, uintptr_t addr, uintptr_t ip, int n
                     } else {
                         GETGX_empty(v0);
                         SMREAD();
-                        addr = geted32(dyn, addr, ninst, nextop, &ed, x1, &fixedaddress, &unscaled, 0xfff << 4, 15, rex, NULL, 0, 0);
+                        addr = geted(dyn, addr, ninst, nextop, &ed, x1, &fixedaddress, &unscaled, 0xfff << 4, 15, rex, NULL, 0, 0);
                         VLD128(v0, ed, fixedaddress);
                     }
                     break;
