2f45d731958d73091f98701fe1f2895865f92b39
Leslie Zhai <zhaixiang@loongson.cn>
2025-07-03 09:26:02 +0800

    [WRAPPER] Use find_XXX_Fct mechanism

--- src/wrapped/wrappedlibx11.c
+++ src/wrapped/wrappedlibx11.c
@@ -16,7 +16,6 @@
 #include "box64context.h"
 #include "emu/x64emu_private.h"
 #include "myalign.h"
-#include "elfloader.h"
 
 const char* libx11Name = "libX11.so.6";
 #define ALTNAME "libX11.so"
@@ -706,6 +705,30 @@ static void* find_async_handler_Fct(void* fct)
     return NULL;
 }
 
+// ResourceAlloc
+typedef unsigned long XID;
+#define GO(A)   \
+static uintptr_t my_ResourceAlloc_fct_##A = 0;                      \
+static XID my_ResourceAlloc_##A(void* dpy)                          \
+{                                                                   \
+    return (XID)RunFunctionFmt(my_ResourceAlloc_fct_##A, "p", dpy); \
+}
+SUPER()
+#undef GO
+static void* findResourceAllocFct(void* fct)
+{
+    if(!fct) return fct;
+    if(GetNativeFnc((uintptr_t)fct))  return GetNativeFnc((uintptr_t)fct);
+    #define GO(A) if(my_ResourceAlloc_fct_##A == (uintptr_t)fct) return my_ResourceAlloc_##A;
+    SUPER()
+    #undef GO
+    #define GO(A) if(my_ResourceAlloc_fct_##A == 0) {my_ResourceAlloc_fct_##A = (uintptr_t)fct; return my_ResourceAlloc_##A; }
+    SUPER()
+    #undef GO
+    printf_log(LOG_NONE, "Warning, no more slot for libX11 ResourceAlloc callback\n");
+    return NULL;
+}
+
 #undef SUPER
 
 void* my_XCreateImage(x64emu_t* emu, void* disp, void* vis, uint32_t depth, int32_t fmt, int32_t off
@@ -1391,7 +1414,6 @@ EXPORT void* my_XSynchronize(x64emu_t* emu, void* display, int onoff)
     return reverse_XSynchronizeProcFct(my_lib, my->XSynchronize(display, onoff));
 }
 
-typedef unsigned long XID;
 struct my_XFreeFuncs {
     void* atoms;
     void* modifiermap;
@@ -1617,21 +1639,9 @@ EXPORT void* my__XGetRequest(x64emu_t* emu, my_XDisplay_t* dpy, uint8_t type, si
     return my->_XGetRequest(dpy, type, len);
 }
 
-static void* x64_resource_alloc = NULL;
-static my_XDisplay_t* x64_dpy = NULL;
-static XID my_resource_alloc(void* dpy)
-{
-    XID ret = (XID)RunFunctionFmt((uintptr_t)x64_resource_alloc, "p", x64_dpy);
-    return ret;
-}
-
 EXPORT uintptr_t my_XCreateWindow(x64emu_t* emu, my_XDisplay_t* dpy, uintptr_t v2, int32_t v3, int32_t v4, uint32_t v5, uint32_t v6, uint32_t v7, int32_t v8, uint32_t v9, void* v10, uintptr_t v11, void* v12)
 {
-    if (FindElfAddress(my_context, (uintptr_t)dpy->resource_alloc)) {
-        x64_resource_alloc = dpy->resource_alloc;
-        x64_dpy = dpy;
-        dpy->resource_alloc = my_resource_alloc;
-    }
+    dpy->resource_alloc = findResourceAllocFct(dpy->resource_alloc);
     uintptr_t ret = my->XCreateWindow(dpy, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12);
     return ret;
 }
