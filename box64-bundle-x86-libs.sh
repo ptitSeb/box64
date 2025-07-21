7679b78af11146df57c08b451844413a9f639efb
Fathi Boudra <fathi.boudra@linaro.org>
2025-07-21 18:09:37 +0200

    box64-bundle-x86-libs.sh: rewrite the bundle libraries script
    Heavily based on Luke's original work, the main improvements:
    * portable:
      - it's a POSIX shell script, doesn't rely on bash anymore
      - reduce tool dependencies (use only awk, instead of awk and cut)
    * more robust:
      - clean up on exit/errors
      - retry logic with curl
      - check tools availability
      - fails on checksums errors
    * more readable
    * fixed duplicated removal libraries that can't be emulated
    * passed shellcheck analysis tool
    
    Signed-off-by: Fathi Boudra <fathi.boudra@linaro.org>

--- box64-bundle-x86-libs.sh
+++ box64-bundle-x86-libs.sh
@@ -1,121 +1,128 @@
-#!/bin/bash
+#!/bin/sh
 
-set -x
+set -ex
 
-extract_pkg_auto() {
-    # Example x86_64 package name: libcurl3-gnutls_7.74.0-1.3+deb11u7~bpo11+1_amd64.deb
-    # Example x86 package name: libcurl3-gnutls_7.74.0-1.3+deb11u7~bpo11+1_i386.deb
-    if echo "${1}" | grep -q -P ".deb$"; then
-        echo "DEB (Debian) package detected."
-        ar x "$(basename "${1}")"
-        tar --extract --file data.tar*
-    # Example x86_64 package name: curl-gnutls-8.1.2-44-1-x86_64.eopkg
-    # Example x86 package name: curl-gnutls-32bit-8.1.2-44-1-x86_64.eopkg
-    elif echo "${1}" | grep -q -P ".eopkg$"; then
-        echo "EOPKG (Solus Linux) package detected."
-        unzip -o "$(basename "${1}")"
-        tar --extract --file install.tar.xz
-    # Example x86_64 package name: bluez-libs-5.63-1.el8.x86_64.rpm
-    # Example x86 package name: bluez-libs-5.63-1.el8.i686.rpm
-    elif echo "${1}" | grep -q -P ".rpm$"; then
-        echo "RPM (Fedora) detected."
-        rpm2cpio "$(basename "${1}")" | cpio -idmv
-    # Example x86_64 package name: libjpeg-turbo-3.0.1_1.x86_64.xbps
-    # Example x86 package name: libjpeg-turbo-32bit-3.0.1_1.x86_64.xbps
-    elif echo "${1}" | grep -q -P ".xbps$"; then
-        echo "XBPS (Void Linux) package detected."
-        tar --extract --file "$(basename "${1}")"
-    else
-        echo "Unsupported package type detected."
-        exit 1
-    fi
+trap cleanup_exit INT TERM EXIT
+
+cleanup_exit()
+{
+    echo "Running cleanup_exit..."
+    rm -rf /tmp/box64-bundle.*
 }
 
-box64_dir=$(pwd)
-dir_tmp_local="$(mktemp --directory)"
-cd "${dir_tmp_local}"
+# we must have few tools
+awk=$(which awk) || { echo "E: You must have awk" && exit 1; }
+cpio=$(which cpio) || { echo "E: You must have cpio" && exit 1; }
+curl=$(which curl) || { echo "E: You must have curl" && exit 1; }
+curl_cmd="${curl} --connect-timeout 5 --retry 5 --retry-delay 1 --create-dirs -fsSLo"
+rpm2cpio=$(which rpm2cpio) || { echo "E: You must have rpm2cpio" && exit 1; }
+sha256sum=$(which sha256sum) || { echo "E: You must have sha256sum" && exit 1; }
+unzip=$(which unzip) || { echo "E: You must have unzip" && exit 1; }
 
-for line in $(cat "${box64_dir}/box64-bundle-x86-libs.csv");
-    do pkg_url="$(echo "${line}" | cut -d, -f1)"
-    pkg_checksum_expected="$(echo "${line}" | cut -d, -f2)"
-    pkg_name="$(basename "${pkg_url}")"
-    if ! curl --silent --fail --location --remote-name "${pkg_url}"
-        then echo "Failed to download ${pkg_url}"
+current_dir=$(pwd)
+dir_tmp="$(mktemp -d /tmp/box64-bundle.XXXXXX)"
+
+# download the packages
+while IFS= read -r line; do
+    # shellcheck disable=SC2016
+    pkg_url=$(echo "${line}" | ${awk} -F',' '{print$(NF-1)}')
+    # shellcheck disable=SC2016
+    pkg_checksum=$(echo "${line}" | ${awk} -F',' '{print$(NF)}')
+    # shellcheck disable=SC2016
+    pkg_name=$(echo "${pkg_url}" | ${awk} -F'/' '{print$(NF)}')
+    ${curl_cmd} "${dir_tmp}/bundle-pkgs/${pkg_name}" "${pkg_url}"
+    checksum="$(${sha256sum} "${dir_tmp}/bundle-pkgs/${pkg_name}" | awk '{print $1}')"
+    if [ "${pkg_checksum}" != "${checksum}" ]; then
+        echo "E: Invalid checksum for ${pkg_name}"
+        echo "Expected: ${pkg_checksum}"
+        echo "Computed: ${checksum}"
         exit 1
     fi
-    pkg_checksum_actual="$(sha256sum ${pkg_name} | awk '{print $1}')"
-    if [[ "${pkg_checksum_expected}" -ne "${pkg_checksum_actual}" ]];
-        then echo "Invalid checksum for ${pkg_name}"
-        echo "Expected: ${pkg_checksum_expected}"
-        echo "Actual: ${pkg_checksum_actual}"
-        exit 1
-    fi
-    extract_pkg_auto "${pkg_name}"
+done < box64-bundle-x86-libs.csv
+
+# generate the bundle packages archive
+tar -C "${dir_tmp}"/bundle-pkgs -czvf "${current_dir}"/box64-bundle-x86-pkgs.tar.gz .
+
+# extract the packages
+cd "${dir_tmp}"/bundle-pkgs
+for file in *.deb *.eopkg *.rpm *.xbps; do
+    # handle the case of no files, e.g. xbps
+    [ -e "${dir_tmp}"/bundle-pkgs/"${file}" ] || break
+    # shellcheck disable=SC2016
+    extension=$(echo "${file}" | ${awk} -F'.' '{print$(NF)}')
+    mkdir -p "${dir_tmp}"/bundle-libs
+    cd "${dir_tmp}"/bundle-libs
+    case "${extension}" in
+        deb)
+            echo "I: DEB (Debian) package detected (${file})"
+            ar x "${dir_tmp}"/bundle-pkgs/"${file}"
+            tar -xf data.tar*
+            ;;
+        eopkg)
+            echo "I: EOPKG (Solus Linux) package detected (${file})"
+            ${unzip} -o "${dir_tmp}"/bundle-pkgs/"${file}"
+            tar -xf install.tar.xz
+            ;;
+        rpm)
+            echo "I: RPM (Fedora) detected (${file})"
+            ${rpm2cpio} "${dir_tmp}"/bundle-pkgs/"${file}" | ${cpio} -idmv
+            ;;
+        xbps)
+            echo "I: XBPS (Void Linux) package detected (${file})"
+            tar -xf "${dir_tmp}"/bundle-pkgs/"${file}"
+            ;;
+    esac
 done
 
-# Start preperation of bundles.
-mkdir -p \
-  "${dir_tmp_local}/bundle-libs/usr/lib/box64-i386-linux-gnu" \
-  "${dir_tmp_local}/bundle-libs/usr/lib/box64-x86_64-linux-gnu" \
-  "${dir_tmp_local}/bundle-pkgs" \
+# reorganize the files
+cd "${dir_tmp}"/bundle-libs
+mkdir -p "${dir_tmp}"/bundle-libs/box64-i386-linux-gnu
+mv lib/*.so* usr/lib/*.so* usr/lib/i386-linux-gnu/*.so* usr/lib32/*.so* \
+    "${dir_tmp}"/bundle-libs/box64-i386-linux-gnu
+mkdir -p "${dir_tmp}"/bundle-libs/box64-x86_64-linux-gnu
+mv lib64/*.so* usr/lib/x86_64-linux-gnu/*.so* usr/lib64/*.so* \
+    "${dir_tmp}"/bundle-libs/box64-x86_64-linux-gnu
+rm -f ./*.tar.* debian-binary files.xml metadata.xml
+rm -rf ./lib* etc run sbin usr var
+mkdir -p usr/lib
+mv box64-*-linux-gnu usr/lib
 
-# Copy Debian family libraries.
-cp --archive "${dir_tmp_local}"/usr/lib/i386-linux-gnu/*.so* "${dir_tmp_local}/bundle-libs/usr/lib/box64-i386-linux-gnu"
-cp --archive "${dir_tmp_local}"/usr/lib/x86_64-linux-gnu/*.so* "${dir_tmp_local}/bundle-libs/usr/lib/box64-x86_64-linux-gnu"
-# Copy Fedora family libraries.
-cp --archive "${dir_tmp_local}"/usr/lib/*.so* "${dir_tmp_local}/bundle-libs/usr/lib/box64-i386-linux-gnu"
-cp --archive "${dir_tmp_local}"/usr/lib64/*.so* "${dir_tmp_local}/bundle-libs/usr/lib/box64-x86_64-linux-gnu"
-## This location is used by a few special packages such as "glibc".
-cp --archive "${dir_tmp_local}"/lib/*.so* "${dir_tmp_local}/bundle-libs/usr/lib/box64-i386-linux-gnu"
-cp --archive "${dir_tmp_local}"/lib64/*.so* "${dir_tmp_local}/bundle-libs/usr/lib/box64-x86_64-linux-gnu"
-# Copy Solus and Void Linux family libraries.
-cp --archive "${dir_tmp_local}"/usr/lib32/*.so* "${dir_tmp_local}/bundle-libs/usr/lib/box64-i386-linux-gnu"
+# remove libraries that cannot be emulated
+# https://github.com/ptitSeb/box64/blob/v0.3.6/src/librarian/library.c#L433
+set -- libc libpthread librt libGL libX11 libasound libdl libm libbsd libutil \
+  libresolv libXxf86vm libXinerama libXrandr libXext libXfixes libXcursor \
+  libXrender libXft libXi libXss libXpm libXau libXdmcp libX11-xcb libxcb \
+  libxcb-xfixes libxcb-shape libxcb-shm libxcb-randr libxcb-image \
+  libxcb-keysyms libxcb-xtest libxcb-glx libxcb-dri2 libxcb-dri3 libXtst libXt \
+  libXcomposite libXdamage libXmu libxkbcommon libxkbcommon-x11 \
+  libpulse-simple libpulse libvulkan ld-linux-x86-64 crashhandler \
+  libtcmalloc_minimal libanl ld-linux libthread_db
+for file in "$@"; do
+    rm -f "${dir_tmp}"/bundle-libs/usr/lib/box64-*-linux-gnu/"${file}".so*
+done
 
-# Create "<LIBRARY_FILE>.so" symlinks.
-for dir_lib in \
-  "${dir_tmp_local}/bundle-libs/usr/lib/box64-i386-linux-gnu" \
-  "${dir_tmp_local}/bundle-libs/usr/lib/box64-x86_64-linux-gnu"
-    # If two or more libraries of the same name exist, this will symlink the oldest version.
-    # Pipe this into a reverse sort `| sort -r` to symlink the newest version.
-    do for lib in $(ls -1 "${dir_lib}")
-        # Example output: libbluetooth.so
-        do lib_base="$(echo "${lib}" | cut -d. -f1,2)"
-        if ! ls "${dir_lib}/${lib_base}" 2> /dev/null
-            then ln -s "${lib}" "${dir_lib}/${lib_base}"
+# check broken symlinks
+if find "${dir_tmp}"/bundle-libs -type l ! -exec test -e {} \; -print | grep bundle-libs; then
+    echo "E: Broken symlinks found"
+    exit 1
+fi
+
+# create shared libraries (.so) symlinks
+# if multiple libraries of the same name exist, it will symlink the oldest version
+# e.g. libcurl-gnutls soname with versioned symbols (CURL_GNUTLS_3, CURL_GNUTLS_4)
+# libcurl-gnutls.so -> libcurl-gnutls.so.3
+# libcurl-gnutls.so.3 -> libcurl-gnutls.so.4
+# libcurl-gnutls.so.4 -> libcurl-gnutls.so.4.7.0
+# libcurl-gnutls.so.4.7.0
+for dir_lib in "${dir_tmp}"/bundle-libs/usr/lib/box64-*-linux-gnu; do
+    cd "${dir_lib}"
+    for lib in lib*.so*; do
+        lib_base="$(echo "${lib}" | awk -F'.' '{print $1"."$2}')"
+        if ! ls "${dir_lib}/${lib_base}" 2> /dev/null; then
+            ln -s "${lib}" "${lib_base}"
         fi
     done
 done
 
-# Remove libraries that cannot be emulated.
-# https://github.com/ptitSeb/box64/blob/v0.3.6/src/librarian/library.c#L433
-for lib in libc.so libpthread.so librt.so libGL.so libGL.so libX11.so \
-  libasound.so libdl.so libm.so libbsd.so libutil.so libresolv.so \
-  libXxf86vm.so libXinerama.so libXrandr.so libXext.so libXfixes.so libXcursor.so \
-  libXrender.so  libXft.so libXi.so libXss.so libXpm.so libXau.so libXdmcp.so \
-  libX11-xcb.so libxcb.so libxcb-xfixes.so libxcb-shape.so libxcb-shm.so libxcb-randr.so \
-  libxcb-image.so libxcb-keysyms.so libxcb-xtest.so libxcb-glx.so libxcb-dri2.so libxcb-dri3.so \
-  libXtst.so libXt.so libXcomposite.so libXdamage.so libXmu.so libxkbcommon.so \
-  libxkbcommon-x11.so libpulse-simple.so libpulse.so libvulkan.so libvulkan.so \
-  ld-linux-x86-64.so crashhandler.so libtcmalloc_minimal.so libtcmalloc_minimal.so libanl.so \
-  ld-linux.so ld-linux.so libthread_db.so
-    do find "${dir_tmp_local}" -name "${lib}"* -delete
-done
-
-if find "${dir_tmp_local}"/bundle-libs/ -type l ! -exec test -e {} \; -print | grep bundle-libs
-    then echo "Broken symlinks found."
-    cd -
-    echo rm -r -f "${dir_tmp_local}"
-    exit 1
-fi
-
-mv "${dir_tmp_local}"/*.deb "${dir_tmp_local}/bundle-pkgs/"
-mv "${dir_tmp_local}"/*.eopkg "${dir_tmp_local}/bundle-pkgs/"
-mv "${dir_tmp_local}"/*.rpm "${dir_tmp_local}/bundle-pkgs/"
-mv "${dir_tmp_local}"/*.xbps "${dir_tmp_local}/bundle-pkgs/"
-
-tar --directory "${dir_tmp_local}/bundle-libs" --create --file "${box64_dir}/box64-bundle-x86-libs.tar.gz" .
-tar --directory "${dir_tmp_local}/bundle-pkgs" --create --file "${box64_dir}/box64-bundle-x86-pkgs.tar.gz" .
-
-# Clean up.
-cd -
-rm -r -f "${dir_tmp_local}"
+# generate the bundle libraries archive
+tar -C "${dir_tmp}"/bundle-libs -czvf "${current_dir}"/box64-bundle-x86-libs.tar.gz .
