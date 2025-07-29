9a6cab91b02ebaa06cb3dbaa4ee21d296d901921
Yang Liu <numbksco@gmail.com>
2025-07-29 22:46:38 +0800

    more

--- src/dynarec/la64/dynarec_la64_0f.c
+++ src/dynarec/la64/dynarec_la64_0f.c
@@ -1518,24 +1518,14 @@ uintptr_t dynarec64_0F(dynarec_la64_t* dyn, uintptr_t addr, uintptr_t ip, int ni
                             XOR(x4, x4, x4);
                             BSTRPICK_W(x3, ed, 5, 5);
                             BSTRINS_W(x4, x3, 16, 16);
-                            BSTRINS_W(x4, x3, 24, 24);
-
                             BSTRPICK_W(x3, ed, 4, 4);
                             BSTRINS_W(x4, x3, 17, 17);
-                            BSTRINS_W(x4, x3, 25, 25);
-
                             BSTRPICK_W(x3, ed, 3, 3);
                             BSTRINS_W(x4, x3, 18, 18);
-                            BSTRINS_W(x4, x3, 26, 26);
-
                             BSTRPICK_W(x3, ed, 2, 2);
                             BSTRINS_W(x4, x3, 19, 19);
-                            BSTRINS_W(x4, x3, 27, 27);
-
                             BSTRPICK_W(x3, ed, 0, 0);
                             BSTRINS_W(x4, x3, 20, 20);
-                            BSTRINS_W(x4, x3, 28, 28);
-
                             MOVGR2FCSR(FCSR2, x4);
                         }
                         break;
