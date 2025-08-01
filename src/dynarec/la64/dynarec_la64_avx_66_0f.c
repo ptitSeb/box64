6da5698c020dd0facf0995f811e230f387ea607e
phorcys <phorcys@126.com>
2025-08-01 14:17:23 +0800

    [LA64_DYNAREC] Add la64 avx cvt ops, part2. (#2866)
    Integer <=> Float convert.
    VCVT{DQ2PS, PS2DQ, TPS2DQ}
    VCVT{SI2SS, SS2SI, TSS2SI}

--- src/dynarec/la64/dynarec_la64_avx_66_0f.c
+++ src/dynarec/la64/dynarec_la64_avx_66_0f.c
@@ -378,6 +378,38 @@ uintptr_t dynarec64_AVX_66_0F(dynarec_la64_t* dyn, uintptr_t addr, uintptr_t ip,
                 x87_restoreround(dyn, ninst, u8);
             }
             break;
+        case 0x5B:
+            INST_NAME("VCVTPS2DQ Gx, Ex");
+            nextop = F8;
+            GETGY_empty_EY_xy(v0, v1, 0);
+            u8 = sse_setround(dyn, ninst, x6, x4);
+            if(vex.l){
+                d1 = fpu_get_scratch(dyn);
+                XVFTINT_W_S(d1, v1);
+                if (!BOX64ENV(dynarec_fastround)) {
+                    q0 = fpu_get_scratch(dyn);
+                    q1 = fpu_get_scratch(dyn); // mask
+                    d0 = fpu_get_scratch(dyn);
+                    XVLDI(q0, 0b1001110000000); // broadcast 0x80000000 to all
+                    XVLDI(d0, (0b10011 << 8) | 0x4f);
+                    XVFCMP_S(q1, d0, v1, cULE); // get Nan,+overflow mark
+                    XVBITSEL_V(v0, d1, q0, q1);
+                }
+            } else {
+                d1 = fpu_get_scratch(dyn);
+                VFTINT_W_S(d1, v1);
+                if (!BOX64ENV(dynarec_fastround)) {
+                    q0 = fpu_get_scratch(dyn);
+                    q1 = fpu_get_scratch(dyn); // mask
+                    d0 = fpu_get_scratch(dyn);
+                    VLDI(q0, 0b1001110000000); // broadcast 0x80000000 to all
+                    VLDI(d0, (0b10011 << 8) | 0x4f);
+                    VFCMP_S(q1, d0, v1, cULE); // get Nan,+overflow mark
+                    VBITSEL_V(v0, d1, q0, q1);
+                }
+            }
+            x87_restoreround(dyn, ninst, u8);
+            break;
         case 0x5C:
             INST_NAME("VSUBPD Gx, Vx, Ex");
             nextop = F8;
