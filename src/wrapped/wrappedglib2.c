f82c5440b7b01fcc31254a76941e338a47dc5ace
Leslie Zhai <zhaixiang@loongson.cn>
2025-08-05 20:33:45 +0800

    [WRAPPER] Wrapped g_thread_pool some functions (#2900)

--- src/wrapped/wrappedglib2.c
+++ src/wrapped/wrappedglib2.c
@@ -1556,6 +1556,11 @@ EXPORT void my_g_async_queue_push_sorted(x64emu_t* emu, void* queue, void* data,
     my->g_async_queue_push_sorted(queue, data, findGCompareDataFuncFct(func), user_data);
 }
 
+EXPORT void my_g_thread_pool_set_sort_function(x64emu_t* emu, void* pool, void* func, void* user_data)
+{
+    my->g_thread_pool_set_sort_function(pool, findGCompareDataFuncFct(func), user_data);
+}
+
 #define PRE_INIT    \
     if(BOX64ENV(nogtk)) \
         return -1;
