167217c76b9fc226c7da1b4482a39b90b63b4bd9
Yang Liu <liuyang22@iscas.ac.cn>
2025-08-16 03:09:44 +0800

    [LA64_DYNAREC] Added more scalar AVX opcodes (#2943)

--- src/dynarec/rv64/dynarec_rv64_avx_66_0f.c
+++ src/dynarec/rv64/dynarec_rv64_avx_66_0f.c
@@ -46,6 +46,27 @@ uintptr_t dynarec64_AVX_66_0F(dynarec_rv64_t* dyn, uintptr_t addr, uintptr_t ip,
     rex_t rex = vex.rex;
 
     switch (opcode) {
+        case 0x28:
+            INST_NAME("VMOVAPD Gx, Ex");
+            nextop = F8;
+            GETEX(x2, 0, vex.l ? 24 : 8);
+            GETGX();
+            GETGY();
+            LD(x3, wback, fixedaddress);
+            SD(x3, gback, gdoffset);
+            LD(x3, wback, fixedaddress + 8);
+            SD(x3, gback, gdoffset + 8);
+            if (vex.l) {
+                GETEY();
+                LD(x3, wback, fixedaddress);
+                SD(x3, gyback, gyoffset);
+                LD(x3, wback, fixedaddress + 8);
+                SD(x3, gyback, gyoffset + 8);
+            } else {
+                SD(xZR, gyback, gyoffset);
+                SD(xZR, gyback, gyoffset + 8);
+            }
+            break;
         case 0x6E:
             INST_NAME("VMOVD Gx, Ed");
             nextop = F8;
@@ -56,6 +77,31 @@ uintptr_t dynarec64_AVX_66_0F(dynarec_rv64_t* dyn, uintptr_t addr, uintptr_t ip,
             SD(xZR, gback, gdoffset + 8);
             SD(xZR, gyback, gyoffset);
             SD(xZR, gyback, gyoffset + 8);
+            break;
+        case 0x7E:
+            INST_NAME("VMOVD Ed, Gx");
+            nextop = F8;
+            GETGX();
+            ed = TO_NAT((nextop & 7) + (rex.b << 3));
+            if (rex.w) {
+                if (MODREG) {
+                    LD(ed, gback, gdoffset);
+                } else {
+                    addr = geted(dyn, addr, ninst, nextop, &wback, x2, x3, &fixedaddress, rex, NULL, 1, 0);
+                    LD(x3, gback, gdoffset);
+                    SD(x3, wback, fixedaddress);
+                    SMWRITE2();
+                }
+            } else {
+                if (MODREG) {
+                    LWU(ed, gback, gdoffset);
+                } else {
+                    addr = geted(dyn, addr, ninst, nextop, &wback, x2, x3, &fixedaddress, rex, NULL, 1, 0);
+                    LWU(x3, gback, gdoffset);
+                    SW(x3, wback, fixedaddress);
+                    SMWRITE2();
+                }
+            }
             break;
         default:
             DEFAULT;
