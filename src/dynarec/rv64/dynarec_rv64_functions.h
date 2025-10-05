e8ed70905c73a7d4c1c0ef6f099f6b1c4e208e96
xctan <xctan@cirno.icu>
2025-10-05 15:56:07 +0800

    [DYNAREC] Optimized scalar AES impl in DynaRec (#3041)
    * [DYNAREC] Optimized scalar AES impl
    
    * [RV64_DYNAREC] Optimized AES with RVV
    
    * [CI] Bump RISC-V toolchains
    
    * [RV64_DYNAREC] Switch to scalar impl when xtheadvector is present
    
    * [RV64_DYNAREC] Try to disable rvv aes kernels
    
    * [RV64_DYNAREC] Reverted assembly aes functions

--- src/dynarec/rv64/dynarec_rv64_functions.h
+++ src/dynarec/rv64/dynarec_rv64_functions.h
@@ -70,4 +70,5 @@ int fpu_is_st_freed(dynarec_native_t* dyn, int ninst, int st);
 
 void updateNativeFlags(dynarec_rv64_t* dyn);
 void get_free_scratch(dynarec_rv64_t* dyn, int ninst, uint8_t* tmp1, uint8_t* tmp2, uint8_t* tmp3, uint8_t s1, uint8_t s2, uint8_t s3, uint8_t s4, uint8_t s5);
+
 #endif //__DYNAREC_RV64_FUNCTIONS_H__
