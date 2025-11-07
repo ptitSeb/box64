a96f2dcda9dbdf48e2a33cf6139089f85e3a3646
phorcys <phorcys@126.com>
2025-11-07 23:12:21 +0800

    [LA64_DYNAREC] Add 8bits lock op macros. (#3127)
    *  Add LOCK_8_ALIGNED_4BYTE, LOCK_8_IN_4BYTE macro.
      *  F0 00 LOCK ADD Eb, Gb

--- src/dynarec/la64/dynarec_la64_f0.c
+++ src/dynarec/la64/dynarec_la64_f0.c
@@ -52,6 +52,33 @@ uintptr_t dynarec64_F0(dynarec_la64_t* dyn, uintptr_t addr, uintptr_t ip, int ni
     GETREX();
 
     switch (opcode) {
+        case 0x00:
+            INST_NAME("LOCK ADD Eb, Gb");
+            SETFLAGS(X_ALL, SF_SET_PENDING, NAT_FLAGS_FUSION);
+            nextop = F8;
+            GETGD;
+            if (MODREG) {
+                ed = TO_NAT((nextop & 7) + (rex.b << 3));
+                emit_add8(dyn, ninst, ed, gd, x3, x4);
+            } else {
+                addr = geted(dyn, addr, ninst, nextop, &wback, x2, x1, &fixedaddress, rex, LOCK_LOCK, 0, 0);
+                if (cpuext.lam_bh) {
+                    AMADD_DB_B(x1, gd, wback);
+                } else {
+                    ANDI(x3, wback, 0b11);
+                    BNEZ_MARK(x3);
+                    LOCK_8_ALIGNED_4BYTE(ADD_W(x4, x1, gd), x1, wback, x3, x4, x5, x6, x7);
+                    B_MARK3_nocond;
+                    MARK;
+                    LOCK_8_IN_4BYTE(ADD_W(x4, x1, gd), x1, wback, x3, x4, x5, x6, x7);
+                    MARK3;
+                }
+                IFXORNAT (X_ALL | X_PEND) {
+                    emit_add8(dyn, ninst, x1, gd, x3, x4);
+                }
+            }
+            SMDMB();
+            break;
         case 0x01:
             INST_NAME("LOCK ADD Ed, Gd");
             SETFLAGS(X_ALL, SF_SET_PENDING, NAT_FLAGS_FUSION);
@@ -1319,7 +1346,7 @@ uintptr_t dynarec64_F0(dynarec_la64_t* dyn, uintptr_t addr, uintptr_t ip, int ni
                         BNEZ_MARK2(x3);
                     }
                     /* LoongArch Reference Manual Vol1 2.2.7.1
-                        If the AM* atomic memory access instruction has the same register number as rd and rk, 
+                        If the AM* atomic memory access instruction has the same register number as rd and rk,
                         the execution result is uncertain. Please software to avoid this situation.
                     */
                     AMSWAP_DB_D(x1, gd, wback);
