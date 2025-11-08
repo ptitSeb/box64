4095d12c2dd7e88b40ef8280098afcc0871ba2b2
Yang Liu <numbksco@gmail.com>
2025-11-09 05:09:02 +0800

    [LA64_DYNAREC] Fixed more regressions introduced in 14e3d05 and 44822bb

--- src/dynarec/la64/dynarec_la64_helper.h
+++ src/dynarec/la64/dynarec_la64_helper.h
@@ -1833,9 +1833,8 @@ uintptr_t dynarec64_DF(dynarec_la64_t* dyn, uintptr_t addr, uintptr_t ip, int ni
     MV(s6, wback);                                       \
     BSTRINS_D(s6, xZR, 2, 0);                            \
     MARKLOCK2;                                           \
-    LL_D(s5, s6, 0);                                     \
-    DBAR(0b00101);                                       \
     LDxw(s1, wback, 0);                                  \
+    LL_D(s5, s6, 0);                                     \
     op;                                                  \
     SC_D(s5, s6, 0);                                     \
     BEQZ_MARKLOCK2(s5);                                  \
@@ -1850,39 +1849,39 @@ uintptr_t dynarec64_DF(dynarec_la64_t* dyn, uintptr_t addr, uintptr_t ip, int ni
     s4 = result
     s5 = 8 byte block
     s6 = mask, amcas orignal val
-    s7 = imm if inst use imm op
 */
-#define LOCK_32_IN_8BYTE(op, s1, wback, s3, s4, s5, s6, s7) \
-    if (wback != x2) {                                      \
-        MV(x2, wback);                                      \
-        wback = x2;                                         \
-    }                                                       \
-    BSTRINS_D(wback, xZR, 2, 0);                            \
-    SLLI_W(s3, s3, 3);                                      \
-    if (cpuext.lamcas) {                                    \
-        LD_D(s5, wback, 0);                                 \
-        MARKLOCK;                                           \
-    } else {                                                \
-        MARKLOCK;                                           \
-        LL_D(s5, wback, 0);                                 \
-    }                                                       \
-    SRL_D(s1, s5, s3);                                      \
-    BSTRPICK_D(s1, s1, 31, 0);                              \
-    op;                                                     \
-    BSTRPICK_D(s4, s4, 31, 0);                              \
-    SLL_D(s4, s4, s3);                                      \
-    ADDI_D(s6, xZR, -1);                                    \
-    BSTRINS_D(s6, xZR, 63, 32);                             \
-    SLL_D(s6, s6, s3);                                      \
-    ANDN(s6, s5, s6);                                       \
-    OR(s4, s6, s4);                                         \
-    if (cpuext.lamcas) {                                    \
-        MV(s6, s5);                                         \
-        AMCAS_DB_D(s5, s4, wback);                          \
-        BNE_MARKLOCK(s5, s6);                               \
-    } else {                                                \
-        SC_D(s4, wback, 0);                                 \
-        BEQZ_MARKLOCK(s4);                                  \
+#define LOCK_32_IN_8BYTE(op, s1, wback, s3, s4, s5, s6) \
+    ed = wback;                                         \
+    if (wback != x2) {                                  \
+        ed = x2;                                        \
+        MV(ed, wback);                                  \
+    }                                                   \
+    BSTRINS_D(ed, xZR, 2, 0);                           \
+    SLLI_W(s3, s3, 3);                                  \
+    if (cpuext.lamcas) {                                \
+        LD_D(s5, ed, 0);                                \
+        MARKLOCK;                                       \
+    } else {                                            \
+        MARKLOCK;                                       \
+        LL_D(s5, ed, 0);                                \
+    }                                                   \
+    SRL_D(s1, s5, s3);                                  \
+    BSTRPICK_D(s1, s1, 31, 0);                          \
+    op;                                                 \
+    BSTRPICK_D(s4, s4, 31, 0);                          \
+    SLL_D(s4, s4, s3);                                  \
+    ADDI_D(s6, xZR, -1);                                \
+    BSTRINS_D(s6, xZR, 63, 32);                         \
+    SLL_D(s6, s6, s3);                                  \
+    ANDN(s6, s5, s6);                                   \
+    OR(s4, s6, s4);                                     \
+    if (cpuext.lamcas) {                                \
+        MV(s6, s5);                                     \
+        AMCAS_DB_D(s5, s4, ed);                         \
+        BNE_MARKLOCK(s5, s6);                           \
+    } else {                                            \
+        SC_D(s4, ed, 0);                                \
+        BEQZ_MARKLOCK(s4);                              \
     }
 
 /*
@@ -1894,25 +1893,24 @@ uintptr_t dynarec64_DF(dynarec_la64_t* dyn, uintptr_t addr, uintptr_t ip, int ni
     s4 = result
     s5 = 4 byte block
     s6 = temp use
-    s7 = imm if inst use imm op
 */
-#define LOCK_8_ALIGNED_4BYTE(op, s1, wback, s3, s4, s5, s6, s7) \
-    if (cpuext.lamcas) {                                        \
-        LD_B(s1, wback, 0); /* amcas.b rd is sign extended */   \
-        MARKLOCK;                                               \
-        op;                                                     \
-        MV(s6, s1);                                             \
-        AMCAS_DB_B(s1, s4, wback);                              \
-        BNE_MARKLOCK(s1, s6);                                   \
-    } else {                                                    \
-        MARKLOCK;                                               \
-        LL_W(s5, wback, 0);                                     \
-        BSTRPICK_D(s1, s5, 7, 0);                               \
-        EXT_W_B(s6, s1);                                        \
-        op;                                                     \
-        BSTRINS_W(s5, s4, 7, 0);                                \
-        SC_W(s5, wback, 0);                                     \
-        BEQZ_MARKLOCK(s5);                                      \
+#define LOCK_8_ALIGNED_4BYTE(op, s1, wback, s3, s4, s5, s6)   \
+    if (cpuext.lamcas) {                                      \
+        LD_B(s1, wback, 0); /* amcas.b rd is sign extended */ \
+        MARKLOCK;                                             \
+        op;                                                   \
+        MV(s6, s1);                                           \
+        AMCAS_DB_B(s1, s4, wback);                            \
+        BNE_MARKLOCK(s1, s6);                                 \
+    } else {                                                  \
+        MARKLOCK;                                             \
+        LL_W(s5, wback, 0);                                   \
+        BSTRPICK_D(s1, s5, 7, 0);                             \
+        EXT_W_B(s6, s1);                                      \
+        op;                                                   \
+        BSTRINS_W(s5, s4, 7, 0);                              \
+        SC_W(s5, wback, 0);                                   \
+        BEQZ_MARKLOCK(s5);                                    \
     }
 
 /*
@@ -1924,7 +1922,7 @@ uintptr_t dynarec64_DF(dynarec_la64_t* dyn, uintptr_t addr, uintptr_t ip, int ni
     s4 = result
     s5 = 4 byte block
     s6 = mask
-    s7 = imm if inst use imm op
+    s7 = wback tmp
 */
 #define LOCK_8_IN_4BYTE(op, s1, wback, s3, s4, s5, s6, s7) \
     if (cpuext.lamcas) {                                   \
@@ -1935,14 +1933,11 @@ uintptr_t dynarec64_DF(dynarec_la64_t* dyn, uintptr_t addr, uintptr_t ip, int ni
         AMCAS_DB_W(s1, s4, wback);                         \
         BNE_MARKLOCK2(s1, s6);                             \
     } else {                                               \
-        if (wback != x2) {                                 \
-            MV(x2, wback);                                 \
-            wback = x2;                                    \
-        }                                                  \
-        BSTRINS_D(wback, xZR, 1, 0);                       \
+        MV(s7, wback);                                     \
+        BSTRINS_D(s7, xZR, 1, 0);                          \
         SLLI_W(s3, s3, 3);                                 \
         MARKLOCK2;                                         \
-        LL_W(s5, wback, 0);                                \
+        LL_W(s5, s7, 0);                                   \
         SRL_W(s1, s5, s3);                                 \
         BSTRPICK_W(s1, s1, 7, 0);                          \
         EXT_W_B(s6, s1);                                   \
@@ -1954,7 +1949,7 @@ uintptr_t dynarec64_DF(dynarec_la64_t* dyn, uintptr_t addr, uintptr_t ip, int ni
         SLL_W(s6, s6, s3);                                 \
         ANDN(s6, s5, s6);                                  \
         OR(s4, s6, s4);                                    \
-        SC_W(s4, wback, 0);                                \
+        SC_W(s4, s7, 0);                                   \
         BEQZ_MARKLOCK2(s4);                                \
     }
 
