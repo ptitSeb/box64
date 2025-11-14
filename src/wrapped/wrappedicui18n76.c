adef24894259ac054b48832b692ce12fcfe9b75f
a9ix <neonItem+2g@protonmail.com>
2025-11-14 09:22:12 +0200

    [WRAPPER] Wrap libicu76 (#3172)

---
+++ src/wrapped/wrappedicui18n76.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define _GNU_SOURCE         /* See feature_test_macros(7) */
+#include <dlfcn.h>
+
+#include "wrappedlibs.h"
+
+#include "debug.h"
+#include "wrapper.h"
+#include "bridge.h"
+#include "librarian/library_private.h"
+#include "x64emu.h"
+#include "emu/x64emu_private.h"
+#include "callback.h"
+#include "librarian.h"
+#include "box64context.h"
+#include "emu/x64emu_private.h"
+#include "myalign.h"
+
+const char* icui18n76Name = "libicui18n.so.76";
+#define LIBNAME icui18n76
+
+#define ALTMY my76_
+
+#define NEEDED_LIBS "libicuuc.so.76"
+
+#include "wrappedlib_init.h"
