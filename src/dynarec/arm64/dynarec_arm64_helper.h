039c6f557efebb5f750bda5f44219436f88b3ba0
Yang Liu <numbksco@gmail.com>
2025-11-30 04:14:41 +0800

    [ARM_DYNAREC] Fixed a regression introduced in commit c64a431e (#3241)

--- src/dynarec/arm64/dynarec_arm64_helper.h
+++ src/dynarec/arm64/dynarec_arm64_helper.h
@@ -1180,7 +1180,7 @@
     && (dyn->insts[ninst].x64.gen_flags&(~(A))))                                                \
         READFLAGS(((dyn->insts[ninst].x64.gen_flags&X_PEND)?X_ALL:dyn->insts[ninst].x64.gen_flags)&(~(A)));\
     if(dyn->insts[ninst].x64.gen_flags) switch(B) {                                             \
-        case SF_SUBSET: dyn->f.pending = SF_SET; break;                                         \
+        case SF_SUBSET: SET_DFNONE(); dyn->f.pending = SF_SET; break;                                         \
         case SF_SET: dyn->f.pending = SF_SET; break;                                            \
         case SF_SET_DF: dyn->f.pending = SF_SET; dyn->f.dfnone = 1; break;                      \
         case SF_SET_NODF: dyn->f.pending = SF_SET; dyn->f.dfnone = 0; break;                    \
