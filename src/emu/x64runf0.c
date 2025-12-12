ptitSeb/box64

commit 33c9d6349838f986d6e820d7f65b82012ae9f04f

[INTERP] Small improvment on handling edge case for CMPXCHG8B/16B

src/emu/x64runf0.c
@@ -19,6 +19,8 @@
 #include "box64context.h"
 #include "my_cpuid.h"
 #include "bridge.h"
+#include "x64_signals.h"
+#include "emit_signals.h"
 #ifdef DYNAREC
 #include "../dynarec/native_lock.h"
 #endif
@@ -780,6 +782,9 @@ uintptr_t RunF0(x64emu_t *emu, rex_t rex, uintptr_t addr)
                     GETE8xw(0);
                     switch((nextop>>3)&7) {
                         case 1:
+                            if(((uintptr_t)ED)&0xf) {
+                                EmitSignal(emu, X64_SIGSEGV, (void*)R_RIP, 0xbad0); // GPF
+                            }
                             CHECK_FLAGS(emu);
                             GETGD;
 #if defined(DYNAREC) && !defined(TEST_INTERPRETER)

src/emu/x64run0f.c
@@ -1781,10 +1781,16 @@ uintptr_t Run0F(x64emu_t *emu, rex_t rex, uintptr_t addr, int *step)
         case 0xC7:
             CHECK_FLAGS(emu);
             nextop = F8;
+            if(MODREG) {
+                return 0;   // register mode is Undefined Instruction
+            }
             GETE8xw(0);
             switch((nextop>>3)&7) {
-                case 1:     /* CMPXCHG8B Eq */
+                case 1:     /* CMPXCHG8B Eq / CMPXCHG16B Eq */
                     if(rex.w) {
+                        if(((uintptr_t)ED)&0xf) {
+                            EmitSignal(emu, X64_SIGSEGV, (void*)R_RIP, 0xbad0); // GPF
+                        }
                         tmp64u = ED->q[0];
                         tmp64u2= ED->q[1];
                         if(R_RAX == tmp64u && R_RDX == tmp64u2) {

commit ac08b010d533c6e2de0368322e213add8efe5125

[INTERP] Fixed a regression introduced with 33c9d6349838f986d6e820d7f65b82012ae9f04f

src/emu/x64runf0.c
@@ -782,7 +782,7 @@ uintptr_t RunF0(x64emu_t *emu, rex_t rex, uintptr_t addr)
                     GETE8xw(0);
                     switch((nextop>>3)&7) {
                         case 1:
-                            if(((uintptr_t)ED)&0xf) {
+                            if(rex.w && ((uintptr_t)ED)&0xf) {
                                 EmitSignal(emu, X64_SIGSEGV, (void*)R_RIP, 0xbad0); // GPF
                             }
                             CHECK_FLAGS(emu);