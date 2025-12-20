a983cbe67b5f0736a81be41e2d4a75ae0b30845a
ptitSeb <sebastien.chev@gmail.com>
2025-12-20 15:00:28 +0100

    [WRAPPER] Reworked libidn2 wrapping with wrapperhelper

--- src/wrapped/wrappedidn2_private.h
+++ src/wrapped/wrappedidn2_private.h
@@ -1,5 +1,5 @@
 #if !(defined(GO) && defined(GOM) && defined(GO2) && defined(DATA))
-#error Meh....
+#error Meh...
 #endif
 
 GO(idn2_check_version, pFp)
@@ -8,18 +8,18 @@ GO(idn2_lookup_u8, iFppi)
 GO(idn2_lookup_ul, iFppi)
 //GO(_idn2_punycode_decode, 
 //GO(_idn2_punycode_encode, 
-//GO(idn2_register_u8, 
-//GO(idn2_register_ul, 
+GO(idn2_register_u8, iFpppi)
+GO(idn2_register_ul, iFpppi)
 GO(idn2_strerror, pFi)
-//GO(idn2_strerror_name, 
-//GO(idn2_to_ascii_4i, 
-//GO(idn2_to_ascii_4i2, 
-//GO(idn2_to_ascii_4z, 
+GO(idn2_strerror_name, pFi)
+GO(idn2_to_ascii_4i, iFpLpi)
+GO(idn2_to_ascii_4i2, iFpLpi)
+GO(idn2_to_ascii_4z, iFppi)
 GO(idn2_to_ascii_8z, iFppi)
-//GO(idn2_to_ascii_lz, 
-//GO(idn2_to_unicode_44i, 
-//GO(idn2_to_unicode_4z4z, 
-//GO(idn2_to_unicode_8z4z, 
+GO(idn2_to_ascii_lz, iFppi)
+GO(idn2_to_unicode_44i, iFpLppi)
+GO(idn2_to_unicode_4z4z, iFppi)
+GO(idn2_to_unicode_8z4z, iFppi)
 GO(idn2_to_unicode_8z8z, iFppi)
-//GO(idn2_to_unicode_8zlz, 
-//GO(idn2_to_unicode_lzlz, 
+GO(idn2_to_unicode_8zlz, iFppi)
+GO(idn2_to_unicode_lzlz, iFppi)
