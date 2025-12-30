ptitSeb/box64

commit 2b070086d12a1b7ea6439023143dc0c9999eea79

Added FSGSBASE cpu extension

src/dynarec/rv64/dynarec_rv64_00_2.c
@@ -514,6 +514,12 @@ uintptr_t dynarec64_00_2(dynarec_rv64_t* dyn, uintptr_t addr, uintptr_t ip, int 
                 ed = x1;
             }
             SH(ed, xEmu, offsetof(x64emu_t, segs[(nextop & 0x38) >> 3]));
+            if((u8==_FS) || (u8==_GS)) {
+                // refresh offset if needed
+                CBZ_NEXT(ed);
+                MOV32w(x1, u8);
+                CALL(const_getsegmentbase, -1, x1, x2);
+            }
             break;
         case 0x8F:
             INST_NAME("POP Ed");

src/dynarec/arm64/arm64_epilog.S
@@ -20,7 +20,7 @@ arm64_epilog:
     stp     x26, x27, [x0, (8 * 16)] // put back reg value in emu, including EIP (so x27 must be EIP now)
     //restore all used register
 #ifdef _WIN32
-    ldr     x18, [x0, 3104]
+    ldr     x18, [x0, 3088]
 #endif
     add     sp, x28, 0
     //vpop     {d8-d15}

src/dynarec/arm64/arm64_next.S
@@ -23,7 +23,7 @@ arm64_next:
     stp     x18, x27, [sp, (8 * 10)]    // also save x27(rip) to allow change in LinkNext
 
 #ifdef _WIN32
-    ldr     x18, [x0, 3104]
+    ldr     x18, [x0, 3088]
 #endif
     mov     x1, x27     // xRIP, should not be needed most of the time
     mov     x2, x30      // "from" is in lr, so put in x2

src/dynarec/arm64/dynarec_arm64_00.c
@@ -1689,6 +1689,12 @@ uintptr_t dynarec64_00(dynarec_arm_t* dyn, uintptr_t addr, uintptr_t ip, int nin
                     ed = x1;
                 }
                 STRH_U12(ed, xEmu, offsetof(x64emu_t, segs[u8]));
+                if((u8==_FS) || (u8==_GS)) {
+                    // refresh offset if needed
+                    CBZw_NEXT(ed);
+                    MOV32w(x1, u8);
+                    CALL(const_getsegmentbase, -1);
+                }
             }
             break;
         case 0x8F:

src/dynarec/arm64/dynarec_arm64_0f.c
@@ -1774,6 +1774,9 @@ uintptr_t dynarec64_0F(dynarec_arm_t* dyn, uintptr_t addr, uintptr_t ip, int nin
             INST_NAME("POP FS");
             POP1z(x2);
             STRH_U12(x2, xEmu, offsetof(x64emu_t, segs[_FS]));
+            CBZw_NEXT(x2);
+            MOV32w(x1, _FS);
+            CALL(const_getsegmentbase, -1);
             break;
         case 0xA2:
             INST_NAME("CPUID");
@@ -1857,6 +1860,9 @@ uintptr_t dynarec64_0F(dynarec_arm_t* dyn, uintptr_t addr, uintptr_t ip, int nin
             INST_NAME("POP GS");
             POP1z(x2);
             STRH_U12(x2, xEmu, offsetof(x64emu_t, segs[_GS]));
+            CBZw_NEXT(x2);
+            MOV32w(x1, _GS);
+            CALL(const_getsegmentbase, -1);
             break;
 
         case 0xAB:

src/dynarec/arm64/dynarec_arm64_64.c
@@ -1006,6 +1006,12 @@ uintptr_t dynarec64_64(dynarec_arm_t* dyn, uintptr_t addr, uintptr_t ip, int nin
                     ed = x1;
                 }
                 STRH_U12(ed, xEmu, offsetof(x64emu_t, segs[u8]));
+                if((u8==_FS) || (u8==_GS)) {
+                    // refresh offset if needed
+                    CBZw_NEXT(ed);
+                    MOV32w(x1, u8);
+                    CALL(const_getsegmentbase, -1);
+                }
             }
             break;
         case 0x8F:

src/dynarec/arm64/dynarec_arm64_66.c
@@ -730,6 +730,12 @@ uintptr_t dynarec64_66(dynarec_arm_t* dyn, uintptr_t addr, uintptr_t ip, int nin
                     ed = x1;
                 }
                 STRH_U12(ed, xEmu, offsetof(x64emu_t, segs[u8]));
+                if((u8==_FS) || (u8==_GS)) {
+                    // refresh offset if needed
+                    CBZw_NEXT(ed);
+                    MOV32w(x1, u8);
+                    CALL(const_getsegmentbase, -1);
+                }
             }
             break;
         case 0x8F:

src/dynarec/arm64/dynarec_arm64_f30f.c
@@ -503,15 +503,32 @@ uintptr_t dynarec64_F30F(dynarec_arm_t* dyn, uintptr_t addr, uintptr_t ip, int n
         case 0xAE:
             nextop = F8;
             switch((nextop>>3)&7) {
+                case 0:
+                case 1:
+                    if(rex.is32bits || !MODREG) {
+                        INST_NAME("Illegal AE");
+                        FAKEED;
+                        UDF(0);
+                    } else {
+                        if(((nextop>>3)&7)==1) {INST_NAME("RDGSBASE");} else {INST_NAME("RDFSBASE");}
+                        ed = TO_NAT((nextop & 7) + (rex.b << 3));
+                        int seg = _FS + ((nextop>>3)&7);
+                        grab_segdata(dyn, addr, ninst, x4, seg, (MODREG));
+                        MOVxw_REG(ed, x4);
+                    }
+                     break;
                 case 2:
-                    INST_NAME("(unsupported) WRFSBASE Ed");
-                    FAKEED;
-                    UDF(0);
-                    break;
                 case 3:
-                    INST_NAME("(unsupported) WRGSBASE Ed");
-                    FAKEED;
-                    UDF(0);
+                    if(rex.is32bits || !MODREG) {
+                        INST_NAME("Illegal AE");
+                        FAKEED;
+                        UDF(0);
+                    } else {
+                        if(((nextop>>3)&7)==3) {INST_NAME("WRGSBASE");} else {INST_NAME("WRFSBASE");}
+                        ed = TO_NAT((nextop & 7) + (rex.b << 3));
+                        int seg = _FS + ((nextop>>3)&7)-2;
+                        STRx_U12(ed, xEmu, offsetof(x64emu_t, segs_offs[seg]));
+                    }
                     break;
                 case 5:
                     INST_NAME("(unsupported) INCSSPD/INCSSPQ Ed");

commit 4759e2d20d221354c935cc9e94fac35e5e1ad9e4

[RV64_DYNAREC] Fixed a regression introduced with 2b070086d12a1b7ea6439023143dc0c9999eea79

src/dynarec/rv64/dynarec_rv64_00_2.c
@@ -516,6 +516,7 @@ uintptr_t dynarec64_00_2(dynarec_rv64_t* dyn, uintptr_t addr, uintptr_t ip, int 
         case 0x8E:
             INST_NAME("MOV Seg,Ew");
             nextop = F8;
+            u8 = (nextop & 0x38) >> 3;
             if (MODREG) {
                 ed = TO_NAT((nextop & 7) + (rex.b << 3));
             } else {
@@ -524,7 +525,7 @@ uintptr_t dynarec64_00_2(dynarec_rv64_t* dyn, uintptr_t addr, uintptr_t ip, int 
                 LHU(x1, ed, fixedaddress);
                 ed = x1;
             }
-            SH(ed, xEmu, offsetof(x64emu_t, segs[(nextop & 0x38) >> 3]));
+            SH(ed, xEmu, offsetof(x64emu_t, segs[u8]));
             if((u8==_FS) || (u8==_GS)) {
                 // refresh offset if needed
                 CBZ_NEXT(ed);