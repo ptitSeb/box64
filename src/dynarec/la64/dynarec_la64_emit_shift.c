ptitSeb/box64

commit b5c07726a7fbfc9cde181728581a730b24571406

[LA64_DYNAREC] Refined RCR32 opcode (#3343)

src/dynarec/la64/dynarec_la64_emit_shift.c
@@ -1858,6 +1858,42 @@ void emit_rcr16c(dynarec_la64_t* dyn, int ninst, int s1, uint32_t c, int s3, int
     if (dyn->insts[ninst].nat_flags_fusion) NAT_FLAGS_OPS(s1, xZR, s3, xZR);
 }
 
+// emit RCR32 instruction, from s1, s2, store result in s1 using s3, s4 and s5 as scratch
+void emit_rcr32(dynarec_la64_t* dyn, int ninst, rex_t rex, int s1, int s2, int s3, int s4, int s5)
+{
+    IFXORNAT (X_ALL) SET_DFNONE();
+    RESTORE_EFLAGS(s3);
+
+    IFX (X_OF) {
+        SRLI_D(s3, s1, rex.w ? 63 : 31);
+        XOR(s3, s3, xFlags);
+        BSTRINS_D(xFlags, s3, F_OF, F_OF);
+    }
+    ADDI_D(s5, s2, -1);
+    IFX (X_CF) SRL_D(s3, s1, s5); // for later
+
+    SRLI_D(s4, s1, 1);
+    BSTRINS_D(s4, xFlags, rex.w ? 63 : 31, rex.w ? 63 : 31); // insert cf
+    BEQZ(s5, 4 + 4 * 5);                                     // goto label
+    SRL_D(s4, s4, s5);
+    ADDI_D(s5, xZR, rex.w ? 65 : 33);
+    SUB_D(s2, s5, s2);
+    if (rex.w)
+        SLL_D(s1, s1, s2);
+    else
+        SLL_W(s1, s1, s2);
+    OR(s4, s1, s4);
+    // label
+    if (rex.w)
+        MV(s1, s4);
+    else
+        ZEROUP2(s1, s4);
+
+    IFX (X_CF) BSTRINS_D(xFlags, s3, F_CF, F_CF);
+    IFXA (X_ALL, cpuext.lbt) SPILL_EFLAGS();
+    if (dyn->insts[ninst].nat_flags_fusion) NAT_FLAGS_OPS(s1, xZR, s3, xZR);
+}
+
 // emit RCR32 instruction, from s1 , constant c, store result in s1 using s3, s4 and s5 as scratch
 void emit_rcr32c(dynarec_la64_t* dyn, int ninst, rex_t rex, int s1, uint32_t c, int s3, int s4, int s5)
 {

src/dynarec/la64/dynarec_la64_00.c
@@ -2992,17 +2992,24 @@ uintptr_t dynarec64_00(dynarec_la64_t* dyn, uintptr_t addr, uintptr_t ip, int ni
                     break;
                 case 3:
                     INST_NAME("RCR Ed, CL");
-                    MESSAGE(LOG_DUMP, "Need Optimization\n");
                     if (BOX64DRENV(dynarec_safeflags) > 1) {
                         READFLAGS(X_OF | X_CF);
                     } else {
                         READFLAGS(X_CF);
                     }
-                    ANDI(x2, xRCX, rex.w ? 0x3f : 0x1f);
-                    GETEDW(x4, x1, 0);
-                    CALL_(rex.w ? (const_rcr64) : (const_rcr32), ed, x4, x1, x2);
+                    SETFLAGS(X_OF | X_CF, SF_SUBSET, NAT_FLAGS_FUSION);
+                    if (rex.w) {
+                        ANDI(x3, xRCX, 0x3f);
+                    } else {
+                        ANDI(x3, xRCX, 0x1f);
+                    }
+                    GETED(0);
+                    UFLAG_IF {
+                        if (!rex.w && !rex.is32bits && MODREG) { ZEROUP(ed); }
+                    }
+                    CBZ_NEXT(x3);
+                    emit_rcr32(dyn, ninst, rex, ed, x3, x5, x4, x6);
                     WBACK;
-                    if (!wback && !rex.w) ZEROUP(ed);
                     break;
                 case 4:
                 case 6:

src/dynarec/la64/dynarec_la64_helper.h
@@ -1184,6 +1184,7 @@
 #define emit_rcl8           STEPNAME(emit_rcl8)
 #define emit_rcl8c          STEPNAME(emit_rcl8c)
 #define emit_rcr16c         STEPNAME(emit_rcr16c)
+#define emit_rcr32          STEPNAME(emit_rcr32)
 #define emit_rcr32c         STEPNAME(emit_rcr32c)
 #define emit_rcr8           STEPNAME(emit_rcr8)
 #define emit_rcr8c          STEPNAME(emit_rcr8c)
@@ -1350,6 +1351,7 @@ void emit_rcl32c(dynarec_la64_t* dyn, int ninst, rex_t rex, int s1, uint32_t c, 
 void emit_rcl8(dynarec_la64_t* dyn, int ninst, int s1, int s2, int s3, int s4, int s5);
 void emit_rcl8c(dynarec_la64_t* dyn, int ninst, int s1, uint32_t c, int s3, int s4);
 void emit_rcr16c(dynarec_la64_t* dyn, int ninst, int s1, uint32_t c, int s3, int s4);
+void emit_rcr32(dynarec_la64_t* dyn, int ninst, rex_t rex, int s1, int s2, int s3, int s4, int s5);
 void emit_rcr32c(dynarec_la64_t* dyn, int ninst, rex_t rex, int s1, uint32_t c, int s3, int s4, int s5);
 void emit_rcr8(dynarec_la64_t* dyn, int ninst, int s1, int s2, int s3, int s4, int s5);
 void emit_rcr8c(dynarec_la64_t* dyn, int ninst, int s1, uint32_t c, int s3, int s4);

commit 3044ac02d0027fb2b02ce84f99389f54a8909222

[LA64_DYNAREC] Fixed a regression introduced in commit b5c07726

src/dynarec/la64/dynarec_la64_emit_shift.c
@@ -1876,7 +1876,7 @@ void emit_rcr32(dynarec_la64_t* dyn, int ninst, rex_t rex, int s1, int s2, int s
     BSTRINS_D(s4, xFlags, rex.w ? 63 : 31, rex.w ? 63 : 31); // insert cf
     BEQZ(s5, 4 + 4 * 5);                                     // goto label
     SRL_D(s4, s4, s5);
-    ADDI_D(s5, xZR, rex.w ? 65 : 33);
+    ADDI_D(s5, xZR, rex.w ? 64 : 32);
     SUB_D(s2, s5, s2);
     if (rex.w)
         SLL_D(s1, s1, s2);