0414fadaf0ee810ed448f25d9ab48098c6ab0923
Yang Liu <numbksco@gmail.com>
2026-01-16 16:39:14 +0800

    [INTERP] Fixed subnormal handling of f16 to f32 (#3382)
    * [INTERP] Fixed subnormal handling of f16 to f32
    
    * Fixed wow64 build

--- src/include/bitutils.h
+++ src/include/bitutils.h
@@ -4,7 +4,9 @@
 
 extern const uint8_t deBruijn64tab[64];
 extern const uint8_t lead0tab[16];
+extern const uint8_t lead0tab8[256];
 
 int TrailingZeros64(uint64_t x);
+int LeadingZeros16(uint16_t x);
 
 #endif //__BITUTILS_H_
