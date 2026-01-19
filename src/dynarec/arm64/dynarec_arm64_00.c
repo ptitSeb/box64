ptitSeb/box64

commit eb743a13075a562891dbc2b7585f597200c97e43

[ARM64_DYNAREC] Refacfored the handling of deffered flags

src/dynarec/arm64/dynarec_arm64_00.c
@@ -3011,7 +3011,7 @@ uintptr_t dynarec64_00(dynarec_arm_t* dyn, uintptr_t addr, uintptr_t ip, int nin
             break;
         case 0xCF:
             INST_NAME("IRET");
-            SETFLAGS(X_ALL, SF_SET_NODF);    // Not a hack, EFLAGS are restored
+            SETFLAGS(X_ALL, SF_SET_DF);    // Not a hack, EFLAGS are restored
             BARRIER(BARRIER_FLOAT);
             iret_to_epilog(dyn, ip, ninst, rex.is32bits, rex.w);
             *need_epilog = 0;
@@ -3835,7 +3835,6 @@ uintptr_t dynarec64_00(dynarec_arm_t* dyn, uintptr_t addr, uintptr_t ip, int nin
                 JUMP(j64, 0);
                 if(dyn->insts[ninst].x64.jmp_insts==-1) {
                     // out of the block
-                    SET_NODF();
                     BARRIER(BARRIER_FLOAT);
                     jump_to_next(dyn, j64, 0, ninst, rex.is32bits);
                 } else {
@@ -4517,7 +4516,6 @@ uintptr_t dynarec64_00(dynarec_arm_t* dyn, uintptr_t addr, uintptr_t ip, int nin
                 case 4: // JMP Ed
                     INST_NAME("JMP Ed");
                     READFLAGS(X_PEND);
-                    SET_NODF();
                     BARRIER(BARRIER_FLOAT);
                     GETEDz(0);
                     jump_to_next(dyn, 0, ed, ninst, rex.is32bits);
@@ -4530,7 +4528,6 @@ uintptr_t dynarec64_00(dynarec_arm_t* dyn, uintptr_t addr, uintptr_t ip, int nin
                     } else {
                         INST_NAME("JMP FAR Ed");
                         READFLAGS(X_PEND);
-                        SET_NODF();
                         BARRIER(BARRIER_FLOAT);
                         SMREAD();
                         addr = geted(dyn, addr, ninst, nextop, &wback, x2, &fixedaddress, &unscaled, 0, 0, rex, NULL, 0, 0);

src/dynarec/arm64/dynarec_arm64_helper.h
@@ -1001,70 +1001,48 @@
 #endif
 
 #define FORCE_DFNONE()  STRw_U12(wZR, xEmu, offsetof(x64emu_t, df))
+#define CHECK_DFNONE(N)  do {if(dyn->f==status_none_pending) {FORCE_DFNONE(); if(N) dyn->f = status_none;}} while(0)
 
-#define SET_DFNONE()                                        \
-    do {                                                    \
-        if (!dyn->f.dfnone) {                               \
-            FORCE_DFNONE();                                 \
-        }                                                   \
-        if(!dyn->insts[ninst].x64.may_set) {                \
-            dyn->f.dfnone = 1;                              \
-        }                                                   \
+#define SET_DFNONE()                                                    \
+    do {                                                                \
+        if(!dyn->insts[ninst].x64.may_set && (dyn->f!=status_none)) {   \
+            dyn->f = status_none_pending;                               \
+        }                                                               \
     } while(0)
 
 #define SET_DF(S, N)                                                                                                            \
     if ((N) != d_none) {                                                                                                        \
         MOVZw(S, (N));                                                                                                          \
         STRw_U12(S, xEmu, offsetof(x64emu_t, df));                                                                              \
-        if (dyn->f.pending == SF_PENDING && dyn->insts[ninst].x64.need_after && !(dyn->insts[ninst].x64.need_after & X_PEND)) { \
-            TABLE64C(x6, const_updateflags_arm64);                                                                              \
-            BLR(x6);                                                                                                            \
-            dyn->f.pending = SF_SET;                                                                                            \
-            SET_NODF();                                                                                                         \
-        }                                                                                                                       \
-        dyn->f.dfnone = 0;                                                                                                      \
+        dyn->f = status_set;                                                                                                    \
     } else                                                                                                                      \
         SET_DFNONE()
 
-#ifndef SET_NODF
-#define SET_NODF()          dyn->f.dfnone = 0
-#endif
-#define SET_DFOK()          dyn->f.dfnone = 1
-
 #ifndef READFLAGS
 #define READFLAGS(A) \
-    if(((A)!=X_PEND && dyn->f.pending!=SF_SET)          \
-    && (dyn->f.pending!=SF_SET_PENDING)) {              \
-        if(dyn->f.pending!=SF_PENDING) {                \
-            LDRw_U12(x3, xEmu, offsetof(x64emu_t, df)); \
-            j64 = (GETMARKF)-(dyn->native_size);        \
-            CBZw(x3, j64);                              \
-        }                                               \
-        TABLE64C(x6, const_updateflags_arm64);          \
-        BLR(x6);                                        \
-        MARKF;                                          \
-        dyn->f.pending = SF_SET;                        \
-        SET_DFOK();                                     \
+    if((A)!=X_PEND                                          \
+    && (dyn->f==status_unk)) {                              \
+        TABLE64C(x6, const_updateflags_arm64);              \
+        BLR(x6);                                            \
+        dyn->f = status_none;                               \
     }
 #endif
 
 #ifndef SETFLAGS
 #define SETFLAGS(A, B)                                                                          \
-    if(dyn->f.pending!=SF_SET                                                                   \
-    && ((B)&SF_SUB)                                                                             \
+    if (((B)&SF_SUB)                                                                            \
     && (dyn->insts[ninst].x64.gen_flags&(~(A))))                                                \
         READFLAGS(((dyn->insts[ninst].x64.gen_flags&X_PEND)?X_ALL:dyn->insts[ninst].x64.gen_flags)&(~(A)));\
     if(dyn->insts[ninst].x64.gen_flags) switch(B) {                                             \
-        case SF_SUBSET: SET_DFNONE(); dyn->f.pending = SF_SET; break;                           \
-        case SF_SET: dyn->f.pending = SF_SET; break;                                            \
-        case SF_SET_DF: dyn->f.pending = SF_SET; dyn->f.dfnone = 1; break;                      \
-        case SF_SET_NODF: dyn->f.pending = SF_SET; dyn->f.dfnone = 0; break;                    \
-        case SF_PENDING: dyn->f.pending = SF_PENDING; break;                                    \
+        case SF_SET_DF: dyn->f = status_set; break;                                             \
+        case SF_SET_NODF: SET_DFNONE(); break;                                                  \
+        case SF_SUBSET:                                                                         \
         case SF_SUBSET_PENDING:                                                                 \
+        case SF_SET:                                                                            \
+        case SF_PENDING:                                                                        \
         case SF_SET_PENDING:                                                                    \
-            dyn->f.pending = (dyn->insts[ninst].x64.gen_flags&X_PEND)?SF_SET_PENDING:SF_SET;    \
-            break;                                                                              \
-    } else dyn->f.pending = SF_SET
+            SET_DFNONE(); break;                                                                \
+    } else SET_DFNONE()
 #endif
 #ifndef JUMP
 #define JUMP(A, C) SMEND()

src/dynarec/arm64/dynarec_arm64_0f.c
@@ -570,7 +570,7 @@ uintptr_t dynarec64_0F(dynarec_arm_t* dyn, uintptr_t addr, uintptr_t ip, int nin
             // no special check...
         case 0x2F:
             if(opcode==0x2F) {INST_NAME("COMISS Gx, Ex");} else {INST_NAME("UCOMISS Gx, Ex");}
-            SETFLAGS(X_ALL, SF_SET_NODF);
+            SETFLAGS(X_ALL, SF_SET_DF);
             nextop = F8;
             GETGX(v0, 0);
             GETEXSS(s0, 0, 0);

src/dynarec/arm64/dynarec_arm64_arch.c
@@ -86,9 +86,9 @@ static int arch_build(dynarec_arm_t* dyn, int ninst, arch_build_t* arch, int noa
 {
     memset(arch, 0, sizeof(arch_build_t));
     // flags
-    if((dyn->insts[ninst].f_entry.dfnone==0) || dyn->insts[ninst].need_nat_flags || dyn->insts[ninst].before_nat_flags) {
+    if(((dyn->insts[ninst].f_entry!=status_none && dyn->insts[ninst].f_entry!=status_none_pending)) || dyn->insts[ninst].need_nat_flags || dyn->insts[ninst].before_nat_flags) {
         arch->flags = 1;
-        arch->flags_.defered = dyn->insts[ninst].f_entry.dfnone==0;
+        arch->flags_.defered = (dyn->insts[ninst].f_entry!=status_none && dyn->insts[ninst].f_entry!=status_none_pending);
         uint8_t flags = dyn->insts[ninst].need_nat_flags | dyn->insts[ninst].before_nat_flags;
         arch->flags_.vf = flags&NF_VF;
         arch->flags_.nf = flags&NF_SF;

src/dynarec/arm64/dynarec_arm64_functions.c
@@ -771,6 +771,8 @@ static register_mapping_t register_mappings[] = {
     { "rip", "x27" },
 };
 
+static const char* df_status[] = {"unknown", "set", "none_pending", "none"};
+
 void printf_x64_instruction(dynarec_native_t* dyn, zydis_dec_t* dec, instruction_x64_t* inst, const char* name);
 void inst_name_pass3(dynarec_native_t* dyn, int ninst, const char* name, rex_t rex)
 {
@@ -784,15 +786,13 @@ void inst_name_pass3(dynarec_native_t* dyn, int ninst, const char* name, rex_t r
     }
 
     static char buf[4096];
-    int length = sprintf(buf, "barrier=%d state=%d/%d/%d(%d:%d->%d:%d), %s=%X/%X, use=%X, need=%X/%X, sm=%d(%d/%d/%d)",
+    int length = sprintf(buf, "barrier=%d state=%d/%s(%s->%s)/%d, %s=%X/%X, use=%X, need=%X/%X, sm=%d(%d/%d/%d)",
         dyn->insts[ninst].x64.barrier,
         dyn->insts[ninst].x64.state_flags,
-        dyn->f.pending,
-        dyn->f.dfnone,
-        dyn->insts[ninst].f_entry.pending,
-        dyn->insts[ninst].f_entry.dfnone,
-        dyn->insts[ninst].f_exit.pending,
-        dyn->insts[ninst].f_exit.dfnone,
+        df_status[dyn->f],
+        df_status[dyn->insts[ninst].f_entry],
+        df_status[dyn->insts[ninst].f_exit],
+        dyn->insts[ninst].df_notneeded,
         dyn->insts[ninst].x64.may_set ? "may" : "set",
         dyn->insts[ninst].x64.set_flags,
         dyn->insts[ninst].x64.gen_flags,
@@ -1249,6 +1249,8 @@ void updateUneeded(dynarec_arm_t* dyn)
     for(int ninst=0; ninst<dyn->size; ++ninst) {
         if(dyn->insts[ninst].n.xmm_unneeded || dyn->insts[ninst].n.ymm_unneeded)
             propagateXYMMUneeded(dyn, ninst, dyn->insts[ninst].n.xmm_unneeded, dyn->insts[ninst].n.ymm_unneeded);
+        if(dyn->insts[ninst].df_notneeded)
+            propagate_nodf(dyn, ninst);
     }
     // try to add some preload of XYMM on jump were it would make sense
     for(int ninst=0; ninst<dyn->size; ++ninst)

src/dynarec/arm64/dynarec_arm64_helper.c
@@ -343,6 +343,7 @@ void jump_to_epilog(dynarec_arm_t* dyn, uintptr_t ip, int reg, int ninst)
     NOTEST(x2);
     TABLE64C(x2, const_epilog);
     SMEND();
+    CHECK_DFNONE(0);
     if(dyn->have_purge)
         doLeaveBlock(dyn, ninst, x4, x5, x6);
     BR(x2);
@@ -352,7 +353,6 @@ void jump_to_epilog(dynarec_arm_t* dyn, uintptr_t ip, int reg, int ninst)
 static int indirect_lookup(dynarec_arm_t* dyn, int ninst, int is32bits, int s1, int s2)
 {
     MAYUSE(dyn);
-
     if (!is32bits) {
         // check higher 48bits
         LSRx_IMM(s1, xRIP, 48);
@@ -399,7 +399,7 @@ void jump_to_next(dynarec_arm_t* dyn, uintptr_t ip, int reg, int ninst, int is32
         ip &= 0xffffffffLL;
 
     SMEND();
-
+    CHECK_DFNONE(0);
     int dest;
     if (reg) {
         if (reg != xRIP) {
@@ -438,6 +438,7 @@ void ret_to_epilog(dynarec_arm_t* dyn, uintptr_t ip, int ninst, rex_t rex)
 {
     MAYUSE(dyn); MAYUSE(ninst);
     MESSAGE(LOG_DUMP, "Ret to epilog\n");
+    CHECK_DFNONE(0);
     POP1z(xRIP);
     MOVz_REG(x1, xRIP);
     SMEND();
@@ -468,6 +469,7 @@ void retn_to_epilog(dynarec_arm_t* dyn, uintptr_t ip, int ninst, rex_t rex, int 
 {
     MAYUSE(dyn); MAYUSE(ninst);
     MESSAGE(LOG_DUMP, "Retn to epilog\n");
+    CHECK_DFNONE(0);
     POP1z(xRIP);
     if(n>0xfff) {
         MOV32w(w1, n);
@@ -526,7 +528,7 @@ void iret_to_epilog(dynarec_arm_t* dyn, uintptr_t ip, int ninst, int is32bits, i
     MOV32w(x4, 0x3E7FD7);   // also mask RF, because it's not really handled
     ANDx_REG(x3, x3, x4);
     ORRx_mask(x3, x3, 1, 0b111111, 0); // xFlags | 0b10
-    SET_DFNONE();
+    FORCE_DFNONE();
     if(is32bits) {
         ANDw_mask(x4, x2, 0, 7);   // mask 0xff
         // check if return segment is 64bits, then restore rsp too
@@ -578,6 +580,7 @@ void iret_to_epilog(dynarec_arm_t* dyn, uintptr_t ip, int ninst, int is32bits, i
 void call_c(dynarec_arm_t* dyn, int ninst, arm64_consts_t fnc, int reg, int ret, int saveflags, int savereg)
 {
     MAYUSE(fnc);
+    CHECK_DFNONE(1);
     #if STEP == 0
     dyn->insts[ninst].nat_flags_op = NAT_FLAG_OP_UNUSABLE;
     #endif
@@ -632,6 +635,7 @@ void call_c(dynarec_arm_t* dyn, int ninst, arm64_consts_t fnc, int reg, int ret,
 void call_d(dynarec_arm_t* dyn, int ninst, arm64_consts_t fnc, int ret, int arg1, int arg2, int sav1, int sav2)
 {
     MAYUSE(fnc);
+    CHECK_DFNONE(1);
     #if STEP == 0
     dyn->insts[ninst].nat_flags_op = NAT_FLAG_OP_UNUSABLE;
     #endif
@@ -677,6 +681,7 @@ void call_d(dynarec_arm_t* dyn, int ninst, arm64_consts_t fnc, int ret, int arg1
 void call_n(dynarec_arm_t* dyn, int ninst, void* fnc, int w)
 {
     MAYUSE(fnc);
+    CHECK_DFNONE(1);
     #if STEP == 0
     dyn->insts[ninst].nat_flags_op = NAT_FLAG_OP_UNUSABLE;
     #endif
@@ -2293,24 +2298,38 @@ static void flagsCacheTransform(dynarec_arm_t* dyn, int ninst)
     int jmp = dyn->insts[ninst].x64.jmp_insts;
     if(jmp<0)
         return;
-    if(dyn->f.dfnone || ((dyn->insts[jmp].f_exit.dfnone && !dyn->insts[jmp].f_entry.dfnone) && !dyn->insts[jmp].x64.use_flags))  // flags are fully known, nothing we can do more
+    if(dyn->insts[jmp].f_exit==dyn->insts[jmp].f_entry)  // flags will be fully known, nothing we can do more
+        return;
+    if(dyn->insts[jmp].df_notneeded)
         return;
     MESSAGE(LOG_DUMP, "\tFlags fetch ---- ninst=%d -> %d\n", ninst, jmp);
-    int go = (dyn->insts[jmp].f_entry.dfnone && !dyn->f.dfnone && !dyn->insts[jmp].df_notneeded)?1:0;
-    switch (dyn->insts[jmp].f_entry.pending) {
-        case SF_UNKNOWN: 
-            go = 0;
-            break;
-        default:
-            if(go && !(dyn->insts[jmp].x64.need_before&X_PEND) && (dyn->f.pending!=SF_UNKNOWN)) {
-                // just clear df flags
-                go = 0;
-                STRw_U12(xZR, xEmu, offsetof(x64emu_t, df));
+    int go_fetch = 0;
+    switch(dyn->insts[jmp].f_entry) {
+        case status_unk:
+            if(dyn->insts[ninst].f_exit==status_none_pending) {
+                FORCE_DFNONE();
             }
             break;
+        case status_set:
+            if(dyn->insts[ninst].f_exit==status_none_pending) {
+                FORCE_DFNONE();
+            }
+            if(dyn->insts[ninst].f_exit==status_unk)
+                go_fetch = 1;
+            break;
+        case status_none_pending:
+            if(dyn->insts[ninst].f_exit!=status_none)
+                go_fetch = 1;
+            break;
+        case status_none:
+            if(dyn->insts[ninst].f_exit==status_none_pending) {
+                FORCE_DFNONE();
+            } else
+                go_fetch = 1;
+            break;
     }
-    if(go) {
-        if(dyn->f.pending!=SF_PENDING) {
+    if(go_fetch) {
+        if(dyn->f==status_unk) {
             LDRw_U12(x1, xEmu, offsetof(x64emu_t, df));
             j64 = (GETMARKF2)-(dyn->native_size);
             CBZw(x1, j64);
@@ -2323,6 +2342,7 @@ static void flagsCacheTransform(dynarec_arm_t* dyn, int ninst)
             MSR_nzcv(x6);
         MARKF2;
     }
+    MESSAGE(LOG_DUMP, "\t---- Flags fetch\n");
 }
 
 static void nativeFlagsTransform(dynarec_arm_t* dyn, int ninst, int s1, int s2)

src/dynarec/arm64/dynarec_arm64_pass0.h
@@ -7,15 +7,14 @@
 
 #define MESSAGE(A, ...) do {} while (0)
 #define READFLAGS(A)    \
-        dyn->insts[ninst].x64.use_flags = A; dyn->f.dfnone = 1;\
+        dyn->insts[ninst].x64.use_flags = A; if(dyn->f!=status_none_pending) dyn->f = status_none;\
         if(!BOX64ENV(dynarec_df) && (A)&X_PEND) dyn->insts[ninst].x64.use_flags = X_ALL; \
-        dyn->f.pending=SF_SET
+        dyn->f = status_none
 #define SETFLAGS(A,B)   \
         dyn->insts[ninst].x64.set_flags = A;    \
         dyn->insts[ninst].x64.state_flags = (B)&~SF_DF;  \
-        dyn->f.pending=(B)&SF_SET_PENDING;      \
-        dyn->f.dfnone=((B)&SF_SET)?(((B)==SF_SET_NODF)?0:1):0;  \
-        if(!BOX64ENV(dynarec_df)) {dyn->f.dfnone=1; dyn->f.pending=0; if((A)==SF_PENDING){printf_log(LOG_INFO, "Warning, some opcode use SF_PENDING, forcing deferedflags ON\n"); SET_BOX64ENV(dynarec_df, 1); }}
+        dyn->f=((B)&SF_SET)?(((B)==SF_SET_NODF)?status_none:status_none_pending):(((B)&SF_SET_PENDING)?status_set:status_none_pending);  \
+        if(!BOX64ENV(dynarec_df)) {dyn->f=status_none; if((A)==SF_PENDING){printf_log(LOG_INFO, "Warning, some opcode use SF_PENDING, forcing deferedflags ON\n"); SET_BOX64ENV(dynarec_df, 1); }}
 #define EMIT(A)         dyn->native_size+=4
 #define JUMP(A, C)         add_jump(dyn, ninst); add_next(dyn, (uintptr_t)A); SMEND(); dyn->insts[ninst].x64.jmp = A; dyn->insts[ninst].x64.jmp_cond = C; dyn->insts[ninst].x64.jmp_insts = 0
 #define BARRIER(A)      if(A!=BARRIER_MAYBE) {fpu_purgecache(dyn, ninst, 0, x1, x2, x3, 0); dyn->insts[ninst].x64.barrier = A;} else dyn->insts[ninst].barrier_maybe = 1

src/dynarec/arm64/dynarec_arm64_pass1.h
@@ -2,16 +2,12 @@
 #define FINI
 #define MESSAGE(A, ...) do {} while (0)
 #define EMIT(A) do {} while (0)
-#define SET_NODF()      \
-        if(!dyn->insts[ninst].x64.gen_flags && !dyn->insts[ninst].x64.use_flags)   \
-                propagate_nodf(dyn, ninst);     \
-        dyn->f.dfnone = 0
+
 #define SETFLAGS(A,B)                                                   \
         dyn->insts[ninst].x64.set_flags = A;                            \
         dyn->insts[ninst].x64.state_flags = (B)&~SF_DF;                 \
-        dyn->f.pending=(B)&SF_SET_PENDING;                              \
-        dyn->f.dfnone=((B)&SF_SET)?(((B)==SF_SET_NODF)?0:1):0;          \
-        if(!BOX64ENV(dynarec_df)) {dyn->f.dfnone=1; dyn->f.pending=0; }
+        dyn->f=((B)&SF_SET)?(((B)==SF_SET_NODF)?status_none:status_none_pending):(((B)&SF_SET_PENDING)?status_set:status_none_pending);  \
+        if(!BOX64ENV(dynarec_df)) {dyn->f = status_none; }
 #define NEW_INST                                \
         dyn->insts[ninst].f_entry = dyn->f;     \
         dyn->n.combined1 = dyn->n.combined2 = 0;\

commit 9f501924eec93f8772db4f7da4619cca45b952dc

[ARM64_DYNAREC] Fixed some regression introduced with eb743a13075a562891dbc2b7585f597200c97e43

src/dynarec/arm64/dynarec_arm64_00.c
@@ -4067,7 +4067,7 @@ uintptr_t dynarec64_00(dynarec_arm_t* dyn, uintptr_t addr, uintptr_t ip, int nin
                     MSUBw(x4, x3, ed, x2);  // x4 = x2 mod ed (i.e. x2 - x3*ed)
                     BFIx(xRAX, x3, 0, 8);
                     BFIx(xRAX, x4, 8, 8);
-                    SET_DFNONE();
+                    FORCE_DFNONE();
                     IFX(X_OF)                         {BFCw(xFlags, F_OF, 1);}
                     IFX(X_CF)                         {BFCw(xFlags, F_CF, 1);}
                     IFX2(X_AF, && !BOX64ENV(cputype)) {BFCw(xFlags, F_AF, 1);}
@@ -4102,10 +4102,7 @@ uintptr_t dynarec64_00(dynarec_arm_t* dyn, uintptr_t addr, uintptr_t ip, int nin
                     MSUBw(x4, x3, ed, x2);  // x4 = x2 mod ed (i.e. x2 - x3*ed)
                     BFIx(xRAX, x3, 0, 8);
                     BFIx(xRAX, x4, 8, 8);
-                    if (!BOX64DRENV(dynarec_safeflags)) {
-                        SET_DFNONE();
-                        FORCE_DFNONE();
-                    }
+                    FORCE_DFNONE();
                     IFX2(X_AF, && BOX64ENV(cputype))  {ORRw_mask(xFlags, xFlags, 28, 0);}   //mask=0x10
                     IFX2(X_ZF, && BOX64ENV(cputype))  {BFCw(xFlags, F_ZF, 1);}
                     IFX2(X_SF, && BOX64ENV(cputype))  {BFCw(xFlags, F_SF, 1);}
@@ -4291,6 +4288,7 @@ uintptr_t dynarec64_00(dynarec_arm_t* dyn, uintptr_t addr, uintptr_t ip, int nin
                             MOVx_REG(xRAX, x2);
                         }
                     }
+                    FORCE_DFNONE();
                     IFX(X_OF)                         {BFCw(xFlags, F_OF, 1);}
                     IFX(X_CF)                         {BFCw(xFlags, F_CF, 1);}
                     IFX2(X_AF, && !BOX64ENV(cputype)) {BFCw(xFlags, F_AF, 1);}
@@ -4376,10 +4374,7 @@ uintptr_t dynarec64_00(dynarec_arm_t* dyn, uintptr_t addr, uintptr_t ip, int nin
                             MOVx_REG(xRAX, x2);
                         }
                     }
-                    if (!BOX64DRENV(dynarec_safeflags)) {
-                        SET_DFNONE();
-                        FORCE_DFNONE();
-                    }
+                    FORCE_DFNONE();
                     IFX2(X_AF, && BOX64ENV(cputype))  {ORRw_mask(xFlags, xFlags, 28, 0);}   //mask=0x10
                     IFX2(X_ZF, && BOX64ENV(cputype))  {BFCw(xFlags, F_ZF, 1);}
                     IFX2(X_SF, && BOX64ENV(cputype))  {BFCw(xFlags, F_SF, 1);}

src/dynarec/arm64/dynarec_arm64_helper.h
@@ -1028,11 +1028,19 @@
     }
 #endif
 
+#define GRABFLAGS(A) \
+    if((A)!=X_PEND                                          \
+    && ((dyn->f==status_unk) || (dyn->f==status_set))) {    \
+        TABLE64C(x6, const_updateflags_arm64);              \
+        BLR(x6);                                            \
+        dyn->f = status_none;                               \
+    }
+
 #ifndef SETFLAGS
 #define SETFLAGS(A, B) do {                                                                     \
     if (((B)&SF_SUB)                                                                            \
     && (dyn->insts[ninst].x64.gen_flags&(~(A))))                                                \
-        { READFLAGS(((dyn->insts[ninst].x64.gen_flags&X_PEND)?X_ALL:dyn->insts[ninst].x64.gen_flags)&(~(A))); }\
+        { GRABFLAGS(((dyn->insts[ninst].x64.gen_flags&X_PEND)?X_ALL:dyn->insts[ninst].x64.gen_flags)&(~(A))); }\
     if(dyn->insts[ninst].x64.gen_flags) switch(B) {                                             \
         case SF_SET_DF: dyn->f = status_set; break;                                             \
         case SF_SET_NODF: break;                                                                \