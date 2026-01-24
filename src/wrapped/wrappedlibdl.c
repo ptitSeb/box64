c5f81e8a056c8e721887073f47e8aec8dc9667cd
ptitSeb <sebastien.chev@gmail.com>
2026-01-24 10:27:08 +0100

    [DEBUG] Added disabled debug code to get potentially missing wrapped symbol for complex libs like X11 and friends

--- src/wrapped/wrappedlibdl.c
+++ src/wrapped/wrappedlibdl.c
@@ -448,8 +448,26 @@ void* my_dlsym_internal(x64emu_t* emu, void *handle, void *symbol, int version, 
 void* my_dlsym(x64emu_t* emu, void *handle, void *symbol)
 {
     printf_dlsym(LOG_DEBUG, "%04d|Call to dlsym(%p, \"%s\")%s", GetTID(), handle, symbol, BOX64ENV(dlsym_error)?"":"\n");
-    return my_dlsym_internal(emu, handle, symbol, -1, NULL);
+    void* ret = my_dlsym_internal(emu, handle, symbol, -1, NULL);
+    #if 0
+    char* symb = symbol;
+    static char* previous = NULL;
+    static char previous_storage[128];
+    if(previous && ret && (!strcmp(previous, symb) || (previous[0]=='_' && !strcmp(&previous[1], symb)) || (symb[0]=='_' && !strcmp(previous, &symb[1]))))
+        previous = NULL;
+    else if(previous && ret) {
+        printf_log(LOG_INFO, "Warning, symbol %s might be missing\n", previous);
+        previous = NULL;
+    } else if(!ret) {
+        if(previous && !(!strcmp(previous, symb) || (previous[0]=='_' && !strcmp(&previous[1], symb)) || (symb[0]=='_' && !strcmp(previous, &symb[1]))))
+            printf_log(LOG_INFO, "Warning, symbol %s might be missing\n", previous);
+        previous = previous_storage;
+        strcpy(previous, symb);
+    }
+    #endif
+    return ret;
 }
+
 void* my_dlvsym(x64emu_t* emu, void *handle, void *symbol, const char *vername)
 {
     int version = (vername)?2:-1;
