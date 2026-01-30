ptitSeb/box64

commit bae577f9158e0901516566711bede89711a0d669

[SYSINFO] Fixed initialization errors in sysinfo (#3434)

src/core.c
@@ -223,7 +223,6 @@ static void displayMiscInfo()
     }
 #endif
 
-    InitializeSystemInfo();
     printf_log(LOG_INFO, "Running on %s with %d core%s, pagesize: %zd", box64_sysinfo.cpuname, box64_sysinfo.ncpu, box64_sysinfo.ncpu > 1 ? "s" : "", box64_pagesize);
     if (BOX64ENV(maxcpu))
         printf_log_prefix(0, LOG_INFO, ", emulating %d core%s\n", BOX64ENV(maxcpu), BOX64ENV(maxcpu) > 1 ? "s" : "");
@@ -794,6 +793,7 @@ int initialize(int argc, const char **argv, char** env, x64emu_t** emulator, elf
 
     if (!BOX64ENV(nobanner)) PrintBox64Version(1);
 
+    InitializeSystemInfo();
     displayMiscInfo();
 
     hookMangoHud();
@@ -1047,15 +1047,17 @@ int initialize(int argc, const char **argv, char** env, x64emu_t** emulator, elf
     if(pythonpath)
         my_context->pythonpath = box_strdup(pythonpath);
 
-    ApplyEnvFileEntry(box64_guest_name);
+    int applied = ApplyEnvFileEntry(box64_guest_name);
     if (box64_wine && box64_wine_guest_name) {
-        ApplyEnvFileEntry(box64_wine_guest_name);
+        applied |= ApplyEnvFileEntry(box64_wine_guest_name);
         box64_wine_guest_name = NULL;
     }
-    // Try to open ftrace again after applying rcfile.
-    displayMiscInfo();
-    setupZydis(my_context);
-    PrintEnvVariables(&box64env, LOG_INFO);
+    if (applied) {
+        printf_log(LOG_INFO, "Applied settings from rcfile\n");
+        displayMiscInfo();
+        setupZydis(my_context);
+        PrintEnvVariables(&box64env, LOG_INFO);
+    }
 
     for(int i=1; i<my_context->argc; ++i) {
         my_context->argv[i] = box_strdup(argv[i+nextarg]);

src/include/env.h
@@ -230,7 +230,7 @@ typedef struct DynaCacheBlock_s {
 #endif
 
 void InitializeEnvFiles();
-void ApplyEnvFileEntry(const char* name);
+int ApplyEnvFileEntry(const char* name);
 const char* GetLastApplyEntryName();
 void InitializeEnv();
 void LoadEnvVariables();

src/os/hostext_linux.c
@@ -137,6 +137,7 @@ void rv64Detect(void)
 #ifdef DYNAREC
 int DetectHostCpuFeatures(void)
 {
+    memset(&cpuext, 0, sizeof(cpu_ext_t));
     char* p = BOX64ENV(dynarec_nohostext);
 #ifdef ARM64 
     unsigned long hwcap = real_getauxval(AT_HWCAP);

src/os/sysinfo.c
@@ -15,6 +15,7 @@
 #ifndef _WIN32
 static void readCpuinfo(sysinfo_t* info)
 {
+    memset(info, 0, sizeof(sysinfo_t));
     info->emulated_frequency = 1;
     int cpucore = 0;
     while (1) {
@@ -50,7 +51,9 @@ static void readCpuinfo(sysinfo_t* info)
         } else if (info->ncpu <= 1 && strstr(line, "Model Name") != NULL) { // TODO: big.LITTLE handling?
             char* colon = strchr(line, ':');
             if (colon != NULL) {
-                size_t len = strlen(colon + 2);
+                while (colon[1] == ' ' || colon[1] == '\t')
+                    ++colon;
+                size_t len = strlen(colon);
                 info->cpuname = (char*)malloc(len + 1);
                 strcpy(info->cpuname, colon + 2);
                 char* newline = strchr(info->cpuname, '\n');
@@ -95,7 +98,9 @@ lscpu:
             } else if (!info->read_cpuname && strstr(line, "Model name:") != NULL) {
                 char* colon = strchr(line, ':');
                 if (colon != NULL) {
-                    size_t len = strlen(colon + 2);
+                    while (colon[1] == ' ' || colon[1] == '\t')
+                        ++colon;
+                    size_t len = strlen(colon);
                     info->cpuname = (char*)malloc(len + 1);
                     strcpy(info->cpuname, colon + 2);
                     char* newline = strchr(info->cpuname, '\n');

src/tools/env.c
@@ -593,11 +593,12 @@ static void internalApplyEnvFileEntry(const char* entryname, const box64env_t* e
 }
 
 static char old_entryname[256] = "";
-void ApplyEnvFileEntry(const char* entryname)
+int ApplyEnvFileEntry(const char* entryname)
 {
-    if (!entryname || !box64env_entries) return;
-    if (!strcasecmp(entryname, old_entryname)) return;
+    if (!entryname || !box64env_entries) return 0;
+    if (!strcasecmp(entryname, old_entryname)) return 0;
 
+    int ret = 0;
     strncpy(old_entryname, entryname, 255);
     khint_t k1;
     {
@@ -605,18 +606,23 @@ void ApplyEnvFileEntry(const char* entryname)
         k1 = kh_get(box64env_entry, box64env_entries, lowercase_entryname);
         box64env_t* env;
         const char* k2;
+        // clang-format off
         kh_foreach_ref(box64env_entries_wildcard, k2, env,
-            if (strstr(lowercase_entryname, k2))
+            if (strstr(lowercase_entryname, k2)) {
                 internalApplyEnvFileEntry(entryname, env);
-            applyCustomRules();
+                applyCustomRules();
+                ret = 1;
+            }
         )
         box_free(lowercase_entryname);
+        // clang-format on
     }
-    if (k1 == kh_end(box64env_entries)) return;
+    if (k1 == kh_end(box64env_entries)) return ret;
 
     box64env_t* env = &kh_value(box64env_entries, k1);
     internalApplyEnvFileEntry(entryname, env);
     applyCustomRules();
+    return 1;
 }
 
 void LoadEnvVariables()

commit 48da1f4283574e392036131e082d38913a8f72b3

[CORE] Fixed zydis initialization regression introduced in #3434

src/core.c
@@ -1055,9 +1055,9 @@ int initialize(int argc, const char **argv, char** env, x64emu_t** emulator, elf
     if (applied) {
         printf_log(LOG_INFO, "Applied settings from rcfile\n");
         displayMiscInfo();
-        setupZydis(my_context);
         PrintEnvVariables(&box64env, LOG_INFO);
     }
+    setupZydis(my_context);
 
     for(int i=1; i<my_context->argc; ++i) {
         my_context->argv[i] = box_strdup(argv[i+nextarg]);