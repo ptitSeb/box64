2c9b92e2d60a59400548851a195a5329cefef37d
Zewei Yang <yangzewei@loongson.cn>
2026-02-07 15:07:10 +0800

    [LA64_DYNAREC] Fix call_c arg setup clobbering A0-mapped regs
    Signed-off-by: Zewei Yang <yangzewei@loongson.cn>
    Co-authored-by: Leslie Zhai <xiangzhai83@gmail.com>

--- src/dynarec/rv64/dynarec_rv64_helper.c
+++ src/dynarec/rv64/dynarec_rv64_helper.c
@@ -612,13 +612,13 @@ void call_c(dynarec_rv64_t* dyn, int ninst, rv64_consts_t fnc, int reg, int ret,
         SD(xRIP, xEmu, offsetof(x64emu_t, ip));
     }
     TABLE64C(reg, fnc);
-    MV(A0, xEmu);
     if (arg1) MV(A1, arg1);
     if (arg2) MV(A2, arg2);
     if (arg3) MV(A3, arg3);
     if (arg4) MV(A4, arg4);
     if (arg5) MV(A5, arg5);
     if (arg6) MV(A6, arg6);
+    MV(A0, xEmu);
     JALR(xRA, reg);
     if (ret >= 0) {
         MV(ret, A0);
