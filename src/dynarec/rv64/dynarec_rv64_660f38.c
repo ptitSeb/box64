481f2ed644a79e76a2300f9671fcd596b4c7ecfe
Yang Liu <numbksco@gmail.com>
2026-03-12 09:40:22 +0800

    [LA64_DYNAREC] Fixed scratch register conflict for 66 0F 38 63 PCMPISTRI opcode ([RV64_DYNAREC] too)

--- src/dynarec/rv64/dynarec_rv64_660f38.c
+++ src/dynarec/rv64/dynarec_rv64_660f38.c
@@ -1323,7 +1323,7 @@ uintptr_t dynarec64_660F38(dynarec_rv64_t* dyn, uintptr_t addr, uint8_t opcode, 
                         ADDI(x1, xEmu, offsetof(x64emu_t, xmm[ed]));
                         ed = x1;
                     } else {
-                        addr = geted(dyn, addr, ninst, nextop, &ed, x1, x2, &fixedaddress, rex, NULL, 0, 1);
+                        addr = geted(dyn, addr, ninst, nextop, &ed, x1, x5, &fixedaddress, rex, NULL, 0, 1);
                     }
                     u8 = F8;
                     MOV32w(x3, u8);
