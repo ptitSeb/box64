38d793e0b561b5e1fbfa51c48ff6401a995b7b1b
ptitSeb <sebastien.chev@gmail.com>
2026-03-24 18:01:08 +0100

    [PERFMAP] Changed the function used in perf map generation to gather function name

--- src/os/perfmap.c
+++ src/os/perfmap.c
@@ -17,9 +17,7 @@
 void writePerfMap(uintptr_t func_addr, uintptr_t code_addr, size_t code_size, const char* inst_name)
 {
     char pbuf[128];
-    uint64_t sz = 0;
-    uintptr_t start = 0;
-    const char* symbname = FindNearestSymbolName(FindElfAddress(my_context, func_addr), (void*)func_addr, &start, &sz);
+    const char* symbname = getAddrFunctionName(func_addr);
     if(!symbname || !strcmp(symbname, "???"))
         snprintf(pbuf, sizeof(pbuf), "0x%" PRIx64 " %" PRId64 " 0x%" PRIx64 ":%s\n", code_addr, code_size, func_addr, inst_name);
     else
