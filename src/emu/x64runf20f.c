ptitSeb/box64

commit 761b2df41551715c77a262175e2a394f46f5c31c

[INTERP][DYNAREC] Fixed CRC32 register preservation (#3719)

src/emu/x64runf20f.c
@@ -173,7 +173,8 @@ uintptr_t RunF20F(x64emu_t *emu, rex_t rex, uintptr_t addr, int *step)
                     nextop = F8;
                     _GETEB(0);
                     GETGD;
-                    GD->dword[0] ^=  EB->byte[0];
+                    tmp8u = EB->byte[0];
+                    GD->dword[0] ^= tmp8u;
                     for (int i = 0; i < 8; i++) {
                         if (GD->dword[0] & 1)
                             GD->dword[0] = (GD->dword[0] >> 1) ^ 0x82f63b78;
@@ -186,8 +187,9 @@ uintptr_t RunF20F(x64emu_t *emu, rex_t rex, uintptr_t addr, int *step)
                     nextop = F8;
                     _GETED(0);
                     GETGD;
+                    tmp64u = ED->q[0];
                     for(int j=0; j<4*(rex.w+1); ++j) {
-                        GD->dword[0] ^=  ED->byte[j];
+                        GD->dword[0] ^= (tmp64u >> (j * 8)) & 0xff;
                         for (int i = 0; i < 8; i++) {
                             if (GD->dword[0] & 1)
                                 GD->dword[0] = (GD->dword[0] >> 1) ^ 0x82f63b78;

src/dynarec/arm64/dynarec_arm64_f20f.c
@@ -207,6 +207,10 @@ uintptr_t dynarec64_F20F(dynarec_arm_t* dyn, uintptr_t addr, uintptr_t ip, int n
                     if(cpuext.crc32) {
                         CRC32Cxw(gd, gd, ed);
                     } else {
+                        if (ed == gd) {
+                            MOVxw_REG(x1, ed);
+                            ed = x1;
+                        }
                         MOV32w(x2, 0x82f63b78);
                         for(int j=0; j<4*(1+rex.w); ++j) {
                             UBFXxw(x3, ed, 8*j, 8);

src/dynarec/rv64/dynarec_rv64_66f20f.c
@@ -58,10 +58,11 @@ uintptr_t dynarec64_66F20F(dynarec_rv64_t* dyn, uintptr_t addr, uintptr_t ip, in
                     nextop = F8;
                     GETGD;
                     GETEW(x1, 0);
+                    MV(x5, ed);
                     MOV32w(x2, 0x82f63b78);
                     for (int j = 0; j < 2; ++j) {
-                        SRLI(x5, ed, 8 * j);
-                        ANDI(x6, x5, 0xFF);
+                        SRLI(x6, x5, 8 * j);
+                        ANDI(x6, x6, 0xFF);
                         XOR(gd, gd, x6);
                         for (int i = 0; i < 8; i++) {
                             SRLI((i & 1) ? gd : x4, (i & 1) ? x4 : gd, 1);

src/dynarec/rv64/dynarec_rv64_f20f.c
@@ -151,7 +151,8 @@ uintptr_t dynarec64_F20F(dynarec_rv64_t* dyn, uintptr_t addr, uintptr_t ip, int 
                     nextop = F8;
                     GETEB(x1, 0);
                     GETGD;
-                    XOR(gd, gd, ed);
+                    MV(x5, ed);
+                    XOR(gd, gd, x5);
                     MOV32w(x2, 0x82f63b78);
                     for (int i = 0; i < 8; i++) {
                         SRLI((i & 1) ? gd : x4, (i & 1) ? x4 : gd, 1);
@@ -165,10 +166,11 @@ uintptr_t dynarec64_F20F(dynarec_rv64_t* dyn, uintptr_t addr, uintptr_t ip, int 
                     nextop = F8;
                     GETGD;
                     GETED(0);
+                    MV(x5, ed);
                     MOV32w(x2, 0x82f63b78);
                     for (int j = 0; j < 4 * (rex.w + 1); ++j) {
-                        SRLI(x5, ed, 8 * j);
-                        ANDI(x3, x5, 0xFF);
+                        SRLI(x3, x5, 8 * j);
+                        ANDI(x3, x3, 0xFF);
                         XOR(gd, gd, x3);
                         for (int i = 0; i < 8; i++) {
                             SRLI((i & 1) ? gd : x4, (i & 1) ? x4 : gd, 1);

src/emu/x64run66f20f.c
@@ -64,8 +64,9 @@ uintptr_t Run66F20F(x64emu_t *emu, rex_t rex, uintptr_t addr, int *step)
                     nextop = F8;
                     GETEW(0);
                     GETGD;
+                    tmp32u = EW->word[0];
                     for(int j=0; j<2; ++j) {
-                        GD->dword[0] ^=  EW->byte[j];
+                        GD->dword[0] ^= (tmp32u >> (j * 8)) & 0xff;
                         for (int i = 0; i < 8; i++) {
                             if (GD->dword[0] & 1)
                                 GD->dword[0] = (GD->dword[0] >> 1) ^ 0x82f63b78;

commit dc35bec2e93716938dbe2dd97c8853af8d70a456

[INTERP] Fixed a regression introduced in #3719

src/emu/x64runf20f.c
@@ -187,7 +187,7 @@ uintptr_t RunF20F(x64emu_t *emu, rex_t rex, uintptr_t addr, int *step)
                     nextop = F8;
                     _GETED(0);
                     GETGD;
-                    tmp64u = ED->q[0];
+                    tmp64u = rex.w ? ED->q[0] : ED->dword[0];
                     for(int j=0; j<4*(rex.w+1); ++j) {
                         GD->dword[0] ^= (tmp64u >> (j * 8)) & 0xff;
                         for (int i = 0; i < 8; i++) {