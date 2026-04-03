ptitSeb/box64

commit a613ff82777c8d9c72b053c0aaeb9af2989400e4

[ARM64_DYNAREC] Reworked how df_unneeded is propagated

src/dynarec/dynarec_native_functions.c
@@ -856,19 +856,55 @@ uint8_t geted_ib(dynarec_native_t* dyn, uintptr_t addr, int ninst, uint8_t nexto
 }
 #undef F8
 
-void propagate_nodf(dynarec_native_t* dyn, int ninst)
+static void propagate_dfneeded_internal(dynarec_native_t* dyn, int ninst)
 {
     while(ninst>=0) {
+        if(dyn->insts[ninst].df_needed)
+            return; // already flagged
+        if(dyn->insts[ninst].x64.gen_flags || (dyn->insts[ninst].x64.use_flags&X_PEND))
+            return; // flags are use, we can stop propagate there
+        dyn->insts[ninst].df_needed = 1;
+        if(!dyn->insts[ninst].pred_sz)
+            return;
+        for(int i=1; i<dyn->insts[ninst].pred_sz; ++i)
+            propagate_dfneeded_internal(dyn, dyn->insts[ninst].pred[i]);
+        ninst = dyn->insts[ninst].pred[0];
+    }
+}
+
+static void propagate_nodf_internal(dynarec_native_t* dyn, int ninst)
+{
+    while(ninst>=0) {
+        if(dyn->insts[ninst].df_needed)
+            return; // flag are needed, stop
         if(dyn->insts[ninst].df_notneeded)
             return; // already flagged
-        if(dyn->insts[ninst].x64.gen_flags || dyn->insts[ninst].x64.use_flags)
-            return; // flags are use, so maybe it's needed
+        if(dyn->insts[ninst].x64.has_callret || (dyn->insts[ninst].x64.barrier&BARRIER_FLAGS))
+            return; // stop propagate
         dyn->insts[ninst].df_notneeded = 1;
         if(!dyn->insts[ninst].pred_sz)
             return;
         for(int i=1; i<dyn->insts[ninst].pred_sz; ++i)
-            propagate_nodf(dyn, dyn->insts[ninst].pred[i]);
+            propagate_nodf_internal(dyn, dyn->insts[ninst].pred[i]);
         ninst = dyn->insts[ninst].pred[0];
+    }
+}
+
+void propagate_nodf(dynarec_native_t* dyn)
+{
+    // first propagate the df_needed flag
+    for(int ninst=dyn->size-1; ninst>=0; --ninst) {
+        if((dyn->insts[ninst].x64.jmp && dyn->insts[ninst].x64.jmp_insts==-1)
+         || (dyn->insts[ninst].x64.barrier&BARRIER_FLAGS) || (dyn->insts[ninst].x64.use_flags)
+         || dyn->insts[ninst].x64.has_callret
+         || ((dyn->insts[ninst].x64.state_flags&SF_SUB) && dyn->insts[ninst].x64.need_before)
+        )
+            propagate_dfneeded_internal(dyn, ninst);
+    }
+    // and now propagete df_unneeded
+    for(int ninst=dyn->size-1; ninst>=0; --ninst) {
+        if(dyn->insts[ninst].f_exit!=status_unk && dyn->insts[ninst].x64.gen_flags)
+            propagate_nodf_internal(dyn, ninst);
     }
 }
 

src/dynarec/arm64/dynarec_arm64_functions.c
@@ -786,13 +786,13 @@ void inst_name_pass3(dynarec_native_t* dyn, int ninst, const char* name, rex_t r
     }
 
     static char buf[4096];
-    int length = sprintf(buf, "barrier=%d state=%d/%s(%s->%s)/%d, set=%X/%X, use=%X, need=%X/%X, sm=%d(%d/%d/%d)",
+    int length = sprintf(buf, "barrier=%d state=%d/%s(%s->%s)/%c, set=%X/%X, use=%X, need=%X/%X, sm=%d(%d/%d/%d)",
         dyn->insts[ninst].x64.barrier,
         dyn->insts[ninst].x64.state_flags,
         df_status[dyn->f],
         df_status[dyn->insts[ninst].f_entry],
         df_status[dyn->insts[ninst].f_exit],
-        dyn->insts[ninst].df_notneeded,
+        dyn->insts[ninst].df_needed?'N':(dyn->insts[ninst].df_notneeded?'U':'-'),
         dyn->insts[ninst].x64.set_flags,
         dyn->insts[ninst].x64.gen_flags,
         dyn->insts[ninst].x64.use_flags,
@@ -1282,6 +1282,8 @@ static void propagateXYMMUneeded(dynarec_arm_t* dyn, int ninst, uint16_t mask_x,
 
 void updateUneeded(dynarec_arm_t* dyn)
 {
+    propagate_nodf(dyn);
+
     if(!dyn->use_xmm && !dyn->use_ymm)
         return;
     // first propagate the needed regs: those which are used and are not unneeded
@@ -1303,8 +1305,6 @@ void updateUneeded(dynarec_arm_t* dyn)
             for(int i=0; i<dyn->insts[ninst].pred_sz; ++i)
                 propagateXYMMUneeded(dyn, dyn->insts[ninst].pred[i], dyn->insts[ninst].n.xmm_unneeded, dyn->insts[ninst].n.ymm_unneeded);
         }
-        if(dyn->insts[ninst].df_notneeded)
-            propagate_nodf(dyn, ninst);
     }
     // try to add some preload of XYMM on jump were it would make sense
     for(int ninst=0; ninst<dyn->size; ++ninst)

src/dynarec/arm64/dynarec_arm64_private.h
@@ -137,6 +137,7 @@ typedef struct instruction_arm64_s {
     unsigned            normal_carry:1;
     unsigned            normal_carry_before:1;
     unsigned            invert_carry:1; // this opcode force an inverted carry
+    unsigned            df_needed:1;
     unsigned            df_notneeded:1;
     unsigned            unaligned:1;    // this opcode can be re-generated for unaligned special case
     unsigned            x87precision:1; // this opcode can handle x87pc

src/dynarec/dynarec_native_functions.h
@@ -71,7 +71,7 @@ void native_div0(x64emu_t* emu);
 // Caches transformation (for loops) // Specific, need to be written par backend
 int CacheNeedsTransform(dynarec_native_t* dyn, int i1);
 // propagete defererd to unknow, as state is not needed
-void propagate_nodf(dynarec_native_t* dyn, int ninst);
+void propagate_nodf(dynarec_native_t* dyn);
 
 // predecessor access
 int isPred(dynarec_native_t* dyn, int ninst, int pred);

src/dynarec/la64/dynarec_la64_private.h
@@ -110,7 +110,8 @@ typedef struct instruction_la64_s {
     uint8_t             will_read:1;     // [strongmem] will read from memory
     uint8_t             last_write:1;    // [strongmem] the last write in a SEQ
     uint8_t             lock:1;          // [strongmem] lock semantic
-    uint8_t             df_notneeded;
+    uint8_t             df_needed:1;
+    uint8_t             df_notneeded:1;
     uint8_t             nat_flags_fusion:1;
     uint8_t             nat_flags_nofusion:1;
     uint8_t             nat_flags_carry:1;

src/dynarec/ppc64le/dynarec_ppc64le_private.h
@@ -124,6 +124,7 @@ typedef struct instruction_ppc64le_s {
     uint8_t             will_read:1;     // [strongmem] will read from memory
     uint8_t             last_write:1;    // [strongmem] the last write in a SEQ
     uint8_t             lock:1;          // [strongmem] lock semantic
+    uint8_t             df_needed:1;
     uint8_t             df_notneeded:1;
     uint8_t             nat_flags_fusion:1;
     uint8_t             nat_flags_nofusion:1;

src/dynarec/rv64/dynarec_rv64_private.h
@@ -121,7 +121,8 @@ typedef struct instruction_rv64_s {
     uint8_t             will_read:1;     // [strongmem] will read from memory
     uint8_t             last_write:1;    // [strongmem] the last write in a SEQ
     uint8_t             lock:1;          // [strongmem] lock semantic
-    uint8_t             df_notneeded;
+    uint8_t             df_needed:1;
+    uint8_t             df_notneeded:1;
     uint8_t             nat_flags_fusion:1;
     uint8_t             nat_flags_nofusion:1;
     uint8_t             nat_flags_carry:1;

commit c843a2df6ad3e687d9dcefdbecbc0a5d23d4f403

[ARM64_DYNAREC] Fixed a regression introduced with a613ff82777c8d9c72b053c0aaeb9af2989400e4

src/dynarec/dynarec_native_functions.c
@@ -862,9 +862,9 @@ static void propagate_dfneeded_internal(dynarec_native_t* dyn, int ninst)
     while(ninst>=0) {
         if(dyn->insts[ninst].df_needed)
             return; // already flagged
-        if(dyn->insts[ninst].x64.gen_flags || (dyn->insts[ninst].x64.use_flags&X_PEND))
-            return; // flags are use, we can stop propagate there
         dyn->insts[ninst].df_needed = 1;
+        if(dyn->insts[ninst].x64.gen_flags || (dyn->insts[ninst].x64.use_flags&X_PEND) || (dyn->insts[ninst].x64.need_before))
+            return; // flags are use, we can stop propagate there
         if(!dyn->insts[ninst].pred_sz)
             return;
         for(int i=1; i<dyn->insts[ninst].pred_sz; ++i)