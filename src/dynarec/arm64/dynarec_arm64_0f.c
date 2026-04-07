ptitSeb/box64

commit 0874bb4d017277adbb106982fae332f308fbe7ad

[DYNAREC] Fixed XADD where source register overlapping memory base (#3766)

src/dynarec/arm64/dynarec_arm64_0f.c
@@ -2527,7 +2527,7 @@ uintptr_t dynarec64_0F(dynarec_arm_t* dyn, uintptr_t addr, uintptr_t ip, int nin
             }
             break;
         case 0xC0:
-            INST_NAME("XADD Gb, Eb");
+            INST_NAME("XADD Eb, Gb");
             SETFLAGS(X_ALL, SF_SET_PENDING);
             nextop = F8;
             GETGB(x1);
@@ -2538,16 +2538,15 @@ uintptr_t dynarec64_0F(dynarec_arm_t* dyn, uintptr_t addr, uintptr_t ip, int nin
             EBBACK; // eb gets x1 (sum)
             break;
         case 0xC1:
-            INST_NAME("XADD Gd, Ed");
+            INST_NAME("XADD Ed, Gd");
             SETFLAGS(X_ALL, SF_SET_PENDING);
             nextop = F8;
             GETGD;
             GETED(0);
             MOVxw_REG(x3, ed);
-            MOVxw_REG(ed, gd);
-            MOVxw_REG(gd, x3);
             emit_add32(dyn, ninst, rex, ed, gd, x4, x5);
             WBACK;
+            MOVxw_REG(gd, x3);
             break;
         case 0xC2:
             INST_NAME("CMPPS Gx, Ex, Ib");

src/dynarec/arm64/dynarec_arm64_660f.c
@@ -2918,7 +2918,7 @@ uintptr_t dynarec64_660F(dynarec_arm_t* dyn, uintptr_t addr, uintptr_t ip, int n
 
         case 0xC1:
             if(rex.w) return dynarec64_00(dyn, addr-1, ip, ninst, rex, ok, need_epilog);
-            INST_NAME("XADD Gw, Ew");
+            INST_NAME("XADD Ew, Gw");
             SETFLAGS(X_ALL, SF_SET_PENDING);
             nextop = F8;
             GETGW(x1);

src/dynarec/la64/dynarec_la64_0f.c
@@ -2096,8 +2096,8 @@ uintptr_t dynarec64_0F(dynarec_la64_t* dyn, uintptr_t addr, uintptr_t ip, int ni
             GETED(0);
             if (ed != gd) MV(x7, ed);
             emit_add32(dyn, ninst, rex, ed, gd, x4, x5, x6);
-            if (ed != gd) MVxw(gd, x7);
             WBACK;
+            if (ed != gd) MVxw(gd, x7);
             break;
         case 0xC2:
             INST_NAME("CMPPS Gx, Ex, Ib");

src/dynarec/rv64/dynarec_rv64_0f.c
@@ -2460,9 +2460,8 @@ uintptr_t dynarec64_0F(dynarec_rv64_t* dyn, uintptr_t addr, uintptr_t ip, int ni
             if (ed != gd)
                 MV(x7, ed);
             emit_add32(dyn, ninst, rex, ed, gd, x4, x5, x6);
-            if (ed != gd)
-                MVxw(gd, x7);
             WBACK;
+            if (ed != gd) MVxw(gd, x7);
             break;
         case 0xC2:
             INST_NAME("CMPPS Gx, Ex, Ib");

commit 6132b603639aedf292caaee5d47aa53a94217341

[ARM64_DYNAREC] Fixed a regression introduced in #3766

src/dynarec/arm64/dynarec_arm64_0f.c
@@ -2543,10 +2543,10 @@ uintptr_t dynarec64_0F(dynarec_arm_t* dyn, uintptr_t addr, uintptr_t ip, int nin
             nextop = F8;
             GETGD;
             GETED(0);
-            MOVxw_REG(x3, ed);
+            if (ed != gd) MOVx_REG(x3, ed);
             emit_add32(dyn, ninst, rex, ed, gd, x4, x5);
             WBACK;
-            MOVxw_REG(gd, x3);
+            if (ed != gd) MOVxw_REG(gd, x3);
             break;
         case 0xC2:
             INST_NAME("CMPPS Gx, Ex, Ib");