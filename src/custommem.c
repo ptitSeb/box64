ptitSeb/box64

commit 174a9496fa949c62b9079d8881b0835363118a71

[DYNAREC] Added deferred dynablock support for FreeDynablock (#3898)

src/dynarec/dynablock.c
@@ -75,6 +75,18 @@ dynablock_t* InvalidDynablock(dynablock_t* db, int need_lock)
     return db;
 }
 
+static void DeferFreeDynablockMap(dynablock_t* db)
+{
+    // enq for a deferred free so any threads still running in this block has a better chance to finish.
+    if (my_context->db_zombie_count == DB_ZOMBIE_SIZE) {
+        FreeDynarecMap((uintptr_t)my_context->db_zombie[my_context->db_zombie_head]->actual_block);
+    } else {
+        my_context->db_zombie_count++;
+    }
+    my_context->db_zombie[my_context->db_zombie_head] = db;
+    my_context->db_zombie_head = (my_context->db_zombie_head + 1) % DB_ZOMBIE_SIZE;
+}
+
 void FreeInvalidDynablock(dynablock_t* db, int need_lock)
 {
     if(db) {
@@ -92,14 +104,7 @@ void FreeInvalidDynablock(dynablock_t* db, int need_lock)
                 dynarec_log(LOG_INFO, "BOX64 Dynarec: lower max_db=%d\n", my_context->max_db_size);
             }
         }
-        // enq for a deferred free so any threads still running in this block has a better chance to finish.
-        if (my_context->db_zombie_count == DB_ZOMBIE_SIZE) {
-            FreeDynarecMap((uintptr_t)my_context->db_zombie[my_context->db_zombie_head]->actual_block);
-        } else {
-            my_context->db_zombie_count++;
-        }
-        my_context->db_zombie[my_context->db_zombie_head] = db;
-        my_context->db_zombie_head = (my_context->db_zombie_head + 1) % DB_ZOMBIE_SIZE;
+        DeferFreeDynablockMap(db);
         if(need_lock)
             mutex_unlock(&my_context->mutex_dyndump);
     }
@@ -131,7 +136,7 @@ void FreeDynablock(dynablock_t* db, int need_lock, int need_remove)
                 dynarec_log(LOG_INFO, "BOX64 Dynarec: lower max_db=%d\n", my_context->max_db_size);
             }
         }
-        FreeDynarecMap((uintptr_t)db->actual_block);    // will also free db
+        DeferFreeDynablockMap(db);
         if(need_lock)
             mutex_unlock(&my_context->mutex_dyndump);
     }

commit 113b8b427efd02a43f414a5e9dca048911ba1cbe

[DYNAREC] Fixed a regression introduced with #3898 when Defered block are part of a lib that is unloaded

src/custommem.c
@@ -1623,6 +1623,7 @@ void DelMmaplist(mmaplist_t* list)
     for(int i=0; i<list->size; ++i)
         if(list->chunks[i]->size) {
             cleanDBFromAddressRange((uintptr_t)list->chunks[i]->block, list->chunks[i]->size, 1);
+            DeferFreeDynablockClearRange(list->chunks[i]->block, list->chunks[i]->size);
             rb_unset(rbt_dynmem, (uintptr_t)list->chunks[i]->block, (uintptr_t)list->chunks[i]->block+list->chunks[i]->size);
             // the blocklist_t "chunk" structure is port of the memory map, so grab info before freing the memory
             // also need to include back the blocklist_t that is excluded from the blocklist tracking

src/dynarec/dynablock.c
@@ -79,12 +79,20 @@ static void DeferFreeDynablockMap(dynablock_t* db)
 {
     // enq for a deferred free so any threads still running in this block has a better chance to finish.
     if (my_context->db_zombie_count == DB_ZOMBIE_SIZE) {
-        FreeDynarecMap((uintptr_t)my_context->db_zombie[my_context->db_zombie_head]->actual_block);
+        if(my_context->db_zombie[my_context->db_zombie_head])
+            FreeDynarecMap((uintptr_t)my_context->db_zombie[my_context->db_zombie_head]->actual_block);
     } else {
         my_context->db_zombie_count++;
     }
     my_context->db_zombie[my_context->db_zombie_head] = db;
     my_context->db_zombie_head = (my_context->db_zombie_head + 1) % DB_ZOMBIE_SIZE;
+}
+
+void DeferFreeDynablockClearRange(void* addr, size_t sz)
+{
+    for(int i=0; i<my_context->db_zombie_count; ++i)
+        if(my_context->db_zombie[i] && ((void*)my_context->db_zombie[i]>=addr) && ((void*)my_context->db_zombie[i]<(addr+sz)))
+            my_context->db_zombie[i] = NULL;
 }
 
 void FreeInvalidDynablock(dynablock_t* db, int need_lock)

src/include/dynablock.h
@@ -12,6 +12,7 @@ void MarkCRCRangeDynablock(dynablock_t* db, uintptr_t addr, uintptr_t size);
 int FreeRangeDynablock(dynablock_t* db, uintptr_t addr, uintptr_t size);
 void FreeInvalidDynablock(dynablock_t* db, int need_lock);
 dynablock_t* InvalidDynablock(dynablock_t* db, int need_lock);
+void DeferFreeDynablockClearRange(void* addr, size_t sz);
 
 dynablock_t* FindDynablockFromNativeAddress(void* addr);    // defined in box64context.h
 