ptitSeb/box64

commit d39062d3d768fcf020d8e795c2fad818f5f55375

[WRAPPER] Fixed gstreamer not always finding the custom gstreamer folder of steam runtime

src/core.c
@@ -936,24 +936,6 @@ int initialize(int argc, const char **argv, char** env, x64emu_t** emulator, elf
             }
         }
         box64_wine = 1;
-        // check if it's proton, with it's custom gstreamer build, to disable gtk3 loading
-        char tmp[strlen(prog)+100];
-        strcpy(tmp, prog);
-        char* pp = strrchr(tmp, '/');
-        if(pp) {
-            *pp = '\0'; // remove the wine binary call
-            strcat(tmp, "/../lib64/gstreamer-1.0");
-            // check if it exist
-            if(FileExist(tmp, 0)) {
-                box64_custom_gstreamer = box_strdup(tmp);
-            } else {
-                *pp = '\0';
-                strcat(tmp, "/../lib/x86_64-linux-gnu/gstreamer-1.0");
-                if(FileExist(tmp, 0)) {
-                   box64_custom_gstreamer = box_strdup(tmp);
-                }
-            }
-        }
         // Try to get the name of the exe being run, to ApplyEnvFileEntry laters
         if(argv[nextarg+1] && argv[nextarg+1][0]!='-' && strlen(argv[nextarg+1])>4 /*&& !strcasecmp(argv[nextarg+1]+strlen(argv[nextarg+1])-4, ".exe")*/) {
             const char* pp = strrchr(argv[nextarg+1], '/');
@@ -997,6 +979,28 @@ int initialize(int argc, const char **argv, char** env, x64emu_t** emulator, elf
     my_context = NewBox64Context(argc - nextarg);
 
     addLibPaths(my_context);
+    if(box64_wine) {
+        // check if it's proton, with it's custom gstreamer build, to disable gtk3 loading
+        char* tmp_ = ResolveFile(prog_, &my_context->box64_path);
+        char tmp[strlen(tmp_)+100];
+        strcpy(tmp, tmp_);
+        box_free(tmp_);
+        char* pp = strrchr(tmp, '/');
+        if(pp) {
+            *pp = '\0'; // remove the wine binary call
+            strcat(tmp, "/../lib64/gstreamer-1.0");
+            // check if it exist
+            if(FileExist(tmp, 0)) {
+                box64_custom_gstreamer = box_strdup(tmp);
+            } else {
+                *pp = '\0';
+                strcat(tmp, "/../lib/x86_64-linux-gnu/gstreamer-1.0");
+                if(FileExist(tmp, 0)) {
+                   box64_custom_gstreamer = box_strdup(tmp);
+                }
+            }
+        }
+    }
 
     // Append ld_list if it exist
     if(ld_libs_args!=-1)

commit 5c84eb6f3a76e2e771d358a3e76417026c26e2fc

Fixed a regression introduced with d39062d3d768fcf020d8e795c2fad818f5f55375

src/core.c
@@ -982,21 +982,23 @@ int initialize(int argc, const char **argv, char** env, x64emu_t** emulator, elf
     if(box64_wine) {
         // check if it's proton, with it's custom gstreamer build, to disable gtk3 loading
         char* tmp_ = ResolveFile(prog_, &my_context->box64_path);
-        char tmp[strlen(tmp_)+100];
-        strcpy(tmp, tmp_);
-        box_free(tmp_);
-        char* pp = strrchr(tmp, '/');
-        if(pp) {
-            *pp = '\0'; // remove the wine binary call
-            strcat(tmp, "/../lib64/gstreamer-1.0");
-            // check if it exist
-            if(FileExist(tmp, 0)) {
-                box64_custom_gstreamer = box_strdup(tmp);
-            } else {
-                *pp = '\0';
-                strcat(tmp, "/../lib/x86_64-linux-gnu/gstreamer-1.0");
+        if(tmp_) {
+            char tmp[strlen(tmp_)+100];
+            strcpy(tmp, tmp_);
+            box_free(tmp_);
+            char* pp = strrchr(tmp, '/');
+            if(pp) {
+                *pp = '\0'; // remove the wine binary call
+                strcat(tmp, "/../lib64/gstreamer-1.0");
+                // check if it exist
                 if(FileExist(tmp, 0)) {
-                   box64_custom_gstreamer = box_strdup(tmp);
+                    box64_custom_gstreamer = box_strdup(tmp);
+                } else {
+                    *pp = '\0';
+                    strcat(tmp, "/../lib/x86_64-linux-gnu/gstreamer-1.0");
+                    if(FileExist(tmp, 0)) {
+                    box64_custom_gstreamer = box_strdup(tmp);
+                    }
                 }
             }
         }

src/tools/fileutils.c
@@ -45,7 +45,7 @@ static char* ResolveFileInner(const char* filename, path_collection_t* paths, in
         } else
             strcpy(p, paths->paths[i]);
         strcat(p, filename);
-        if(FileExist(p, IS_FILE)) {
+        if(FileExist(p, 0)) {
             return ResolvePathInner(p, resolve_symlink);
         }
     }