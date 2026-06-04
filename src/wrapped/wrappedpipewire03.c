94f97c96e0261e3d8498bf031f70f7044b312375
Yang Liu <numbksco@gmail.com>
2026-06-04 18:22:33 +0800

    [WRAPPER] Wrapped more symbol for pipewire

--- src/wrapped/wrappedpipewire03.c
+++ src/wrapped/wrappedpipewire03.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #define _GNU_SOURCE         /* See feature_test_macros(7) */
 #include <dlfcn.h>
+#include <stdarg.h>
 
 #include "wrappedlibs.h"
 
@@ -20,5 +21,39 @@
 const char* pipewire03Name = "libpipewire-0.3.so";
 
 #define LIBNAME pipewire03
+
+typedef int (*iFppp_t)(void*, void*, void*);
+typedef int (*iFpppA_t)(void*, void*, void*, va_list);
+
+#define ADDED_FUNCTIONS()                   \
+    GO(pw_properties_set, iFppp_t)          \
+    GO(pw_properties_setva, iFpppA_t)       \
+
+#include "generated/wrappedpipewire03types.h"
+
+#include "wrappercallback.h"
+
+EXPORT void* my_pw_properties_new(x64emu_t* emu, void* key, uintptr_t* b)
+{
+    void* props = my->pw_properties_new(NULL, NULL);
+    if(!props || !key)
+        return props;
+
+    int n = 0;
+    while(key) {
+        void* value = (void*)getVArgs(emu, 1, b, n++);
+        my->pw_properties_set(props, key, value);
+        key = (void*)getVArgs(emu, 1, b, n++);
+    }
+
+    return props;
+}
+
+EXPORT int my_pw_properties_setf(x64emu_t* emu, void* props, void* key, void* fmt, uintptr_t* b)
+{
+    myStackAlign(emu, (const char*)fmt, b, emu->scratch, R_EAX, 3);
+    PREPARE_VALIST;
+    return my->pw_properties_setva(props, key, fmt, VARARGS);
+}
 
 #include "wrappedlib_init.h"
