dcf179d9f8539d9afbb427a6a6241c8bfee5343d
Yang Liu <numbksco@gmail.com>
2026-07-15 17:46:58 +0800

    [DYNAREC][ENV] Disabled SEP by default
    Now that we have callret enabled by default, I noticed SEP causes the branch misprediction by an order of magnitude in coremark, while the memory-saving effect is almost imperceptible.
    
    ```
    ksco@steel [ coremark@main ] $ BOX64_LOG=0 BOX64_NOBANNER=1 BOX64_DYNACACHE=0 BOX64_DYNAREC_SEP=0 perf stat -e task-clock,cycles,instructions,branches,branch-misses -- taskset -c 2 box64 ./coremark.exe_x64 0 0 0x66 200000 7 1 >/dev/null
    
     Performance counter stats for 'taskset -c 2 box64 ./coremark.exe_x64 0 0 0x66 200000 7 1':
    
             16,339.65 msec task-clock:u
        35,828,022,082      cycles:u
       118,528,644,489      instructions:u
        12,994,274,457      branches:u
            36,068,961      branch-misses:u
    
          16.348269274 seconds time elapsed
    
          16.331188000 seconds user
           0.009995000 seconds sys
    
    ksco@steel [ coremark@main ] $ BOX64_LOG=0 BOX64_NOBANNER=1 BOX64_DYNACACHE=0 BOX64_DYNAREC_SEP=1 perf stat -e task-clock,cycles,instructions,branches,branch-misses -- taskset -c 2 box64 ./coremark.exe_x64 0 0 0x66 200000 7 1 >/dev/null
    
     Performance counter stats for 'taskset -c 2 box64 ./coremark.exe_x64 0 0 0x66 200000 7 1':
    
             18,571.57 msec task-clock:u
        40,721,222,650      cycles:u
       118,523,943,716      instructions:u
        12,993,208,645      branches:u
           383,363,320      branch-misses:u
    
          18.581470637 seconds time elapsed
    
          18.564061000 seconds user
           0.008996000 seconds sys
    
    ksco@steel [ coremark@main ] $ perf stat -e task-clock,cycles,instructions,branches,branch-misses -- taskset -c 2 ./coremark.exe 0 0 0x66 200000 7 1 >/dev/null
    
     Performance counter stats for 'taskset -c 2 ./coremark.exe 0 0 0x66 200000 7 1':
    
              9,757.79 msec task-clock:u
        21,407,080,976      cycles:u
        62,785,977,365      instructions:u
        11,852,476,157      branches:u
            18,851,352      branch-misses:u
    
           9.762550998 seconds time elapsed
    
           9.757495000 seconds user
           0.000999000 seconds sys
    
    ```

--- src/include/env.h
+++ src/include/env.h
@@ -51,7 +51,7 @@ extern char* ftrace_name;
     BOOLEAN(BOX64_DYNAREC_ALIGNED_ATOMICS, dynarec_aligned_atomics, 0, 1, 1)     \
     INTEGER(BOX64_DYNAREC_BIGBLOCK, dynarec_bigblock, 2, 0, 3, 1, 2)             \
     BOOLEAN(BOX64_DYNAREC_BLEEDING_EDGE, dynarec_bleeding_edge, 1, 0, 0)         \
-    INTEGER(BOX64_DYNAREC_SEP, dynarec_sep, 1, 0, 2, 1, 2)                       \
+    INTEGER(BOX64_DYNAREC_SEP, dynarec_sep, 0, 0, 2, 1, 2)                       \
     BOOLEAN(BOX64_DYNAREC_DF, dynarec_df, 1, 1, 1)                               \
     INTEGER(BOX64_DYNAREC_DIRTY, dynarec_dirty, 0, 0, 2, 0, 2)                   \
     BOOLEAN(BOX64_DYNAREC_NOHOTPAGE, dynarec_nohotpage, 0, 0, 1)                 \
