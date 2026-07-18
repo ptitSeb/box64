2544543b3ace019bf2c9073a3afdb49b68d28bb1
Ryan <hi@sped0n.com>
2026-07-19 04:00:19 +0800

    [CORE] Don't consume guest t/test arguments (#4062)
    Signed-off-by: Sped0n <hi@sped0n.com>

--- src/core.c
+++ src/core.c
@@ -837,14 +837,7 @@ int initialize(int argc, const char **argv, char** env, x64emu_t** emulator, elf
         exit(0);
     }
 
-    int is_test = 0;
-    for (int i = 1; i < argc; i++) {
-        if (!strcmp(argv[i], "--test") || !strcmp(argv[i], "-t")) {
-            is_test = 1;
-            break;
-        }
-    }
-    if (is_test) {
+    if (argc >= 3 && (!strcmp(argv[1], "--test") || !strcmp(argv[1], "-t"))) {
         box64_unittest_mode = 1;
         exit(unittest(argc, argv));
     }
