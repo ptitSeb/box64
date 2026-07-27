9e4de53e2d0506c0139d4d62acf5d8ff1ef12501
Yang Liu <numbksco@gmail.com>
2026-07-27 22:18:58 +0800

    [RV64_DYNAREC] Fixed VSQRTPD opcode

--- src/dynarec/rv64/dynarec_rv64_avx_66_0f.c
+++ src/dynarec/rv64/dynarec_rv64_avx_66_0f.c
@@ -250,6 +250,7 @@ uintptr_t dynarec64_AVX_66_0F(dynarec_rv64_t* dyn, uintptr_t addr, uintptr_t ip,
             INST_NAME("VSQRTPD Gx, Ex");
             nextop = F8;
             GETGX();
+            GETGY();
             GETEX(x2, 0, 8);
             d0 = fpu_get_scratch(dyn);
             if (!BOX64ENV(dynarec_fastnan)) {
