01d7c21a539732fbcc28ee1d572b681f09419265
Yang Liu <numbksco@gmail.com>
2026-07-28 00:42:13 +0800

    [RV64_DYNAREC] Fixed VPSUBQ opcode

--- src/dynarec/rv64/dynarec_rv64_avx_66_0f.c
+++ src/dynarec/rv64/dynarec_rv64_avx_66_0f.c
@@ -2870,7 +2870,7 @@ uintptr_t dynarec64_AVX_66_0F(dynarec_rv64_t* dyn, uintptr_t addr, uintptr_t ip,
         case 0xFB:
             INST_NAME("VPSUBQ Gx, Vx, Ex");
             nextop = F8;
-            GETEX(x2, 0, 8);
+            GETEX(x2, 0, vex.l ? 24 : 8);
             GETGX();
             GETGY();
             GETVX();
