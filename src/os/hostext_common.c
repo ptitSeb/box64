64a882ab3840cb08219da9a9ae81cc7979f7dbb4
Yang Liu <numbksco@gmail.com>
2026-09-14 15:17:06 +0800

    [RV64_DYNAREC] Added non-zvfh fallback path for VCVTPH2PS opcode (#4386)

--- src/os/hostext_common.c
+++ src/os/hostext_common.c
@@ -72,6 +72,7 @@ void PrintHostCpuFeatures(void)
     if (cpuext.zvknha) printf_log_prefix(0, LOG_INFO, "_zvknha");
     if (cpuext.zvknhb) printf_log_prefix(0, LOG_INFO, "_zvknhb");
     if (cpuext.zvbb) printf_log_prefix(0, LOG_INFO, "_zvbb");
+    if (cpuext.zvfh) printf_log_prefix(0, LOG_INFO, "_zvfh");
     printf_log_prefix(0, LOG_INFO, "\n");
 #elif defined(PPC64LE)
     printf_log(LOG_INFO, "Dynarec for PPC64LE (POWER9+, ISA 3.0)");
