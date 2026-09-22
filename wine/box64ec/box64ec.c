65db023e45fba0ea0671a201e5e0a6d127f5e1e1
airidosas252 <airidaslideikis@gmail.com>
2026-09-22 14:08:56 +0300

    [BOX64EC] Move process initialization into its own box64ec_init.c module

--- wine/box64ec/box64ec.c
+++ wine/box64ec/box64ec.c
@@ -1,8 +1,5 @@
 // SPDX-License-Identifier: MIT
-#define WIN32_NO_STATUS
 #include <windows.h>
-#undef WIN32_NO_STATUS
-#include <ntstatus.h>
 #include <winternl.h>
 
 NTSYSAPI NTSTATUS NTAPI LdrDisableThreadCalloutsForDll(HMODULE);
@@ -13,10 +10,4 @@ BOOL WINAPI DllMainCRTStartup(HINSTANCE inst, DWORD reason, void* reserved)
     if (reason == DLL_PROCESS_ATTACH)
         LdrDisableThreadCalloutsForDll(inst);
     return TRUE;
-}
-
-NTSTATUS WINAPI ProcessInit(void)
-{
-    /* The CPU runtime will be introduced separately. */
-    return STATUS_NOT_IMPLEMENTED;
 }
