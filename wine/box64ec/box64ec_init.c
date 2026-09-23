c3b52820500c28e0dbae5b357f271c2a34d6a116
airidosas252 <airidaslideikis@gmail.com>
2026-09-23 16:45:33 +0300

    [BOX64EC] Add support for loading .box64rc file (#4459)

--- wine/box64ec/box64ec_init.c
+++ wine/box64ec/box64ec_init.c
@@ -3,10 +3,12 @@
 #include <windows.h>
 #undef WIN32_NO_STATUS
 #include <ntstatus.h>
+#include <string.h>
 
 #include "env.h"
 #include "sysinfo.h"
 #include "box64context.h"
+#include "custommem.h"
 
 uintptr_t box64_pagesize = 4096;
 int box64_is32bits = 0;
@@ -17,7 +19,18 @@ box64context_t* my_context = &Box64EC_Context;
 
 NTSTATUS WINAPI ProcessInit(void)
 {
+    char filename[MAX_PATH];
+
     LoadEnvVariables();
+    init_custommem_helper(&Box64EC_Context);
+    InitializeEnvFiles();
+
+    DWORD length = GetModuleFileNameA(NULL, filename, sizeof(filename));
+    if (length && length < sizeof(filename)) {
+        const char* profile = strrchr(filename, '\\');
+        ApplyEnvFileEntry(profile ? profile + 1 : filename);
+    }
+
     /* The CPU runtime will be introduced separately. */
     return STATUS_NOT_IMPLEMENTED;
 }
