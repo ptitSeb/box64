a3124f300148f120cc615681aa74f8254e02b901
airidosas252 <airidaslideikis@gmail.com>
2026-09-23 07:40:43 +0300

    [BOX64EC] Add CPU feature reporting and env var initialization (#4454)
    * [BOX64EC] Introduce CPU feature enumeration
    
    * [BOX64EC] Implement basic env var initialization
    
    * [BOX64EC] Move process initialization into its own box64ec_init.c module
    
    * [BOX64EC][CMAKE] Build with shared Box64 and Wine helpers

---
+++ wine/box64ec/box64ec_init.c
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+#define WIN32_NO_STATUS
+#include <windows.h>
+#undef WIN32_NO_STATUS
+#include <ntstatus.h>
+
+#include "env.h"
+#include "sysinfo.h"
+#include "box64context.h"
+
+uintptr_t box64_pagesize = 4096;
+int box64_is32bits = 0;
+int box64_wine = 1;
+sysinfo_t box64_sysinfo = {0};
+box64context_t Box64EC_Context;
+box64context_t* my_context = &Box64EC_Context;
+
+NTSTATUS WINAPI ProcessInit(void)
+{
+    LoadEnvVariables();
+    /* The CPU runtime will be introduced separately. */
+    return STATUS_NOT_IMPLEMENTED;
+}
