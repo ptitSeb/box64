5697f8c4e488d64a3ecfa4890d5b89670484090a
airidosas252 <airidaslideikis@gmail.com>
2026-09-24 18:14:30 +0300

    [BOX64EC] Avoid x86 intrinsic headers with ARM64EC compilation

--- src/emu/x64emu.c
+++ src/emu/x64emu.c
@@ -23,9 +23,9 @@
 #endif
 #include "custommem.h"
 // for the applyFlushTo0
-#ifdef __x86_64__
+#if defined(__x86_64__) && !defined(__arm64ec__) && !defined(_M_ARM64EC)
 #include <immintrin.h>
-#elif defined(__aarch64__)
+#elif defined(__aarch64__) || defined(__arm64ec__) || defined(_M_ARM64EC)
 #else
 #warning Architecture cannot follow SSE Flush to 0 flag
 #endif
@@ -643,9 +643,9 @@ void EmuCall(x64emu_t* emu, uintptr_t addr)
 
 void applyFlushTo0(x64emu_t* emu)
 {
-    #ifdef __x86_64__
+    #if defined(__x86_64__) && !defined(__arm64ec__) && !defined(_M_ARM64EC)
     _mm_setcsr(_mm_getcsr() | (emu->mxcsr.x32&0x8040));
-    #elif defined(__aarch64__)
+    #elif defined(__aarch64__) || defined(__arm64ec__) || defined(_M_ARM64EC)
     #if defined(__ANDROID__) || defined(__clang__)
     uint64_t fpcr;
     __asm__ __volatile__ ("mrs    %0, fpcr":"=r"(fpcr));
